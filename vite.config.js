575d09bdce158dfe8636864e5f4d15479477de5b
Daustinn Dev <57356474+daustinn@users.noreply.github.com>
2024-01-05 17:17:46 -0500

    vite config 2

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    build: {
-        outDir: 'public/build',
-        assetsDir: 'assets',
-        rollupOptions: {
+    plugins: [
+        laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-        },
-    },
-    plugins: [
-        laravel({
             refresh: true,
         }),
     ],
