5ee3d7169665f8d272cb654f8b24ab73844e7107
Daustinn Dev <57356474+daustinn@users.noreply.github.com>
2024-01-26 18:54:43 -0500

    query input colab,

--- resources/js/colaboradores.js
+++ resources/js/colaboradores.js
@@ -67,3 +67,131 @@ selects.forEach(select => {
         }
     });
 });
+
+
+/// add providers
+
+
+const btnToggleEstado = document.querySelectorAll('.toggle-estado')
+const changePasswordForm = document.querySelectorAll('.change-password')
+// STORE 
+
+changePasswordForm.forEach(form => {
+    form.addEventListener('submit', async function (event) {
+        event.preventDefault();
+        try {
+            const formData = new FormData(this);
+            const response = await axios.post(this.action, formData);
+            Swal.fire({
+                icon: 'success',
+                title: 'Contraseña actualizado correctamente!',
+            }).then(() => {
+                location.reload()
+            });
+        } catch (err) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error al actualizar lac contraseña',
+                text: err.message,
+            });
+        }
+
+    });
+})
+
+btnToggleEstado.forEach((btn) => {
+    btn.addEventListener('click', async function () {
+        const id = btn.dataset.id
+        try {
+            await axios.post(`/colaboradores/cambiar-estado/${id}`)
+            Swal.fire({
+                icon: 'success',
+                title: 'Estado cambiado correctamente',
+            }).then(() => {
+                location.reload()
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    })
+})
+
+const busquedaInputs = document.querySelectorAll('.query-colab');
+const ul_results = document.querySelectorAll('.colabs-q');
+const btnSupers = document.querySelectorAll('.btn-super-colab');
+const initialHtml = `<div class="h-[100px] grid place-content-center">
+                                <h2 class="text-center text-neutral-500 text-lg">Busca un colaborador y luego asigna
+                                    como supervisor
+                                </h2>
+                            </div>`
+
+
+
+busquedaInputs.forEach((input) => {
+    input.addEventListener('input', async function () {
+        const q = this.value;
+        const id_colab = this.getAttribute('data-id');
+        if (q === '') {
+            ul_results.forEach((ul) => {
+                ul.innerHTML = initialHtml;
+            });
+            return;
+        }
+        const response = await axios.get(`/search-colaboradores?q=${q}`)
+
+        ul_results.forEach((ul) => {
+            ul.innerHTML = '';
+        });
+
+        response.data.forEach(function (colaborador) {
+            const li = document.createElement('li');
+            li.innerHTML =
+                `<div class='flex item-center gap-2'>
+                     <span class="w-10 h-10 rounded-full overflow-hidden">
+                         <img class="w-full h-full object-cover" src="${colaborador.perfil ?? '/default-user.webp'}" alt="Jese image">
+                     </span>
+                     <h4>${colaborador.apellidos},${colaborador.nombres}</h4>
+                     <span class="font-semibold">${colaborador.dni}</span>
+                     <button id_colab='${id_colab}' id_super='${colaborador.id}' type="button"
+                         class="btn-super-colab text-gray-900 ml-auto bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center :focus:ring-gray-600 :bg-gray-800 :border-gray-700 :text-white :hover:bg-gray-700">
+                         <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="none"
+                             stroke="currentColor" viewBox="0 0 24 24" xmlns="http:www.w3.org/2000/svg">
+                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                 d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1">
+                             </path>
+                         </svg>
+                         Asignar
+                     </button>
+                  </div>
+                 `;
+
+            ul_results.forEach((ul) => {
+                const data_id = ul.getAttribute('data-id');
+                if (data_id === id_colab) {
+                    ul.appendChild(li);
+                }
+            });
+
+            const btnSupers = li.querySelector('.btn-super-colab');
+            btnSupers.addEventListener('click', async function () {
+                const idColab = this.getAttribute("id_colab");
+                const idSuper = this.getAttribute("id_super");
+                try {
+                    await axios.post(
+                        '/colaboradores/update-supervisor', {
+                        id_colab: idColab,
+                        id_super: idSuper
+                    })
+                    location.reload()
+                } catch (error) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error al crear el objetivo',
+                        text: error.message,
+                    });
+                }
+            });
+        });
+    });
+});
--- resources/views/colaboradore/item.blade.php
+++ resources/views/colaboradore/item.blade.php
@@ -150,198 +150,73 @@
 
             @include('colaboradore.contraseña', ['colaborador' => $colaborador])
         </div>
+        
+        @if ($hasAssignSuper)
+            <div id="modal-add-supervisor-{{ $colaborador->id }}" tabindex="-1" aria-hidden="true"
+                class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative w-full max-w-lg max-h-full">
+                    <div class="relative bg-white rounded-2xl shadow :bg-gray-700">
+                        <button type="button"
+                            class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center :hover:bg-gray-600 :hover:text-white"
+                            data-modal-hide="modal-add-supervisor-{{ $colaborador->id }}">
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                            </svg>
+                        </button>
+                        <div class="px-6 py-4 border-b rounded-t :border-gray-600">
+                            <h3 class="text-base font-semibold text-gray-900 lg:text-xl :text-white">
+                                Asignar supervisor
+                            </h3>
+                        </div>
+                        <div class="p-4">
+                            @if ($colaborador->id_supervisor)
+                                <div class='flex items-center gap-2 p-2 bg-neutral-100 rounded-xl'>
+                                    <img class="w-10 h-10 rounded-full" src="/default-user.webp" alt="Jese image">
+                                    <div>
+                                        <h4>{{ $colaborador->supervisor->nombres }}
+                                            {{ $colaborador->supervisor->apellidos }}
+                                        </h4>
+                                        <span class="font-semibold">{{ $colaborador->supervisor->dni }}</span>
+                                    </div>
+                                    <div class="ml-auto">
+                                        <span
+                                            class="bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded :bg-green-900 :text-green-300">Supervisor
+                                            actual</span>
+                                    </div>
+                                </div>
+                            @endif
+                            <p class="text-sm mb-4 font-normal text-gray-500 :text-gray-400">
+                                Asigna o actualiza el supervisor de este colaborador
+                            </p>
+        
+                            <div class="relative w-full block">
+                                <span class="absolute top-[50%] left-2 translate-y-[-50%]">
+                                    <svg class="w-6 h-6 text-gray-500" aria-hidden="true" fill="none"
+                                        viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                    </svg>
+                                </span>
+                                <input type="text" data-id='{{ $colaborador->id }}'
+                                    class="w-full block query-colab rounded-xl outline-none border-neutral-200 bg-gray-100 border p-2 px-4 pl-10"
+                                    placeholder="Buscar colaborador por nombres o DNI">
+                            </div>
+        
+                            <ul data-id='{{ $colaborador->id }}' class="flex colabs-q flex-col gap-1 pt-2">
+                                <div class="h-[100px] grid place-content-center">
+                                    <h2 class="text-center text-neutral-500 text-lg">Busca un colaborador y
+                                        luego asigna
+                                        como supervisor
+                                    </h2>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
     </td>
 </tr>
 
-@if ($hasAssignSuper)
-    <div id="modal-add-supervisor-{{ $colaborador->id }}" tabindex="-1" aria-hidden="true"
-        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative w-full max-w-lg max-h-full">
-            <div class="relative bg-white rounded-2xl shadow :bg-gray-700">
-                <button type="button"
-                    class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center :hover:bg-gray-600 :hover:text-white"
-                    data-modal-hide="modal-add-supervisor-{{ $colaborador->id }}">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                    </svg>
-                </button>
-                <div class="px-6 py-4 border-b rounded-t :border-gray-600">
-                    <h3 class="text-base font-semibold text-gray-900 lg:text-xl :text-white">
-                        Asignar supervisor
-                    </h3>
-                </div>
-                <div class="p-4">
-                    @if ($colaborador->id_supervisor)
-                        <div class='flex items-center gap-2 p-2 bg-neutral-100 rounded-xl'>
-                            <img class="w-10 h-10 rounded-full" src="/default-user.webp" alt="Jese image">
-                            <div>
-                                <h4>{{ $colaborador->supervisor->nombres }}
-                                    {{ $colaborador->supervisor->apellidos }}
-                                </h4>
-                                <span class="font-semibold">{{ $colaborador->supervisor->dni }}</span>
-                            </div>
-                            <div class="ml-auto">
-                                <span
-                                    class="bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded :bg-green-900 :text-green-300">Supervisor
-                                    actual</span>
-                            </div>
-                        </div>
-                    @endif
-                    <p class="text-sm mb-4 font-normal text-gray-500 :text-gray-400">
-                        Asigna o actualiza el supervisor de este colaborador
-                    </p>
-
-                    <div class="relative w-full block">
-                        <span class="absolute top-[50%] left-2 translate-y-[-50%]">
-                            <svg class="w-6 h-6 text-gray-500" aria-hidden="true" fill="none"
-                                viewBox="0 0 20 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                            </svg>
-                        </span>
-                        <input type="text" data-id='{{ $colaborador->id }}'
-                            class="w-full block query-colab rounded-xl outline-none border-neutral-200 bg-gray-100 border p-2 px-4 pl-10"
-                            placeholder="Buscar colaborador por nombres o DNI">
-                    </div>
-
-                    <ul data-id='{{ $colaborador->id }}' class="flex colabs-q flex-col gap-1 pt-2">
-                        <div class="h-[100px] grid place-content-center">
-                            <h2 class="text-center text-neutral-500 text-lg">Busca un colaborador y
-                                luego asigna
-                                como supervisor
-                            </h2>
-                        </div>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-@endif
-
-    <script>
-        const btnToggleEstado = document.querySelectorAll('.toggle-estado')
-        const changePasswordForm = document.querySelectorAll('.change-password')
-        // STORE 
-
-        changePasswordForm.forEach(form => {
-            form.addEventListener('submit', async function(event) {
-                event.preventDefault();
-                try {
-                    const formData = new FormData(this);
-                    const response = await axios.post(this.action, formData);
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Contraseña actualizado correctamente!',
-                    }).then(() => {
-                        location.reload()
-                    });
-                } catch (err) {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error al actualizar lac contraseña',
-                        text: err.message,
-                    });
-                }
-
-            });
-        })
-
-        btnToggleEstado.forEach((btn) => {
-            btn.addEventListener('click', async function() {
-                const id = btn.dataset.id
-                try {
-                    await axios.post(`/colaboradores/cambiar-estado/${id}`)
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Estado cambiado correctamente',
-                    }).then(() => {
-                        location.reload()
-                    });
-
-                } catch (error) {
-                    console.log(error);
-                }
-            })
-        })
-
-        const busquedaInputs = document.querySelectorAll('.query-colab');
-        const ul_results = document.querySelectorAll('.colabs-q');
-        const btnSupers = document.querySelectorAll('.btn-super-colab');
-        const initialHtml = `<div class="h-[100px] grid place-content-center">
-                                <h2 class="text-center text-neutral-500 text-lg">Busca un colaborador y luego asigna
-                                    como supervisor
-                                </h2>
-                            </div>`
-
-
-
-        busquedaInputs.forEach((input) => {
-            input.addEventListener('input', async function() {
-                const q = this.value;
-                const id_colab = this.getAttribute('data-id');
-                if (q === '') {
-                    ul_results.forEach((ul) => {
-                        ul.innerHTML = initialHtml;
-                    });
-                    return;
-                }
-                const response = await axios.get(`/search-colaboradores?q=${q}`)
-
-                ul_results.forEach((ul) => {
-                    ul.innerHTML = '';
-                });
-
-                response.data.forEach(function(colaborador) {
-                    const li = document.createElement('li');
-                    li.innerHTML =
-                        `<div class='flex item-center gap-2'>
-                            <span class="w-10 h-10 rounded-full overflow-hidden">
-                                <img class="w-full h-full object-cover" src="${colaborador.perfil ?? '/default-user.webp'}" alt="Jese image">
-                            </span>
-                            <h4>${colaborador.apellidos},${colaborador.nombres}</h4>
-                            <span class="font-semibold">${colaborador.dni}</span>
-                            <button id_colab='${id_colab}' id_super='${colaborador.id}' type="button"
-                                class="btn-super-colab text-gray-900 ml-auto bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center :focus:ring-gray-600 :bg-gray-800 :border-gray-700 :text-white :hover:bg-gray-700">
-                                <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="none"
-                                    stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1">
-                                    </path>
-                                </svg>
-                                Asignar
-                            </button>
-                         </div>
-                        `;
-
-                    ul_results.forEach((ul) => {
-                        const data_id = ul.getAttribute('data-id');
-                        if (data_id === id_colab) {
-                            ul.appendChild(li);
-                        }
-                    });
-
-                    const btnSupers = li.querySelector('.btn-super-colab');
-                    btnSupers.addEventListener('click', async function() {
-                        const idColab = this.getAttribute("id_colab");
-                        const idSuper = this.getAttribute("id_super");
-                        try {
-                            await axios.post(
-                                '/colaboradores/update-supervisor', {
-                                    id_colab: idColab,
-                                    id_super: idSuper
-                                })
-                            location.reload()
-                        } catch (error) {
-                            Swal.fire({
-                                icon: 'error',
-                                title: 'Error al crear el objetivo',
-                                text: error.message,
-                            });
-                        }
-                    });
-                });
-            });
-        });
-    </script>
