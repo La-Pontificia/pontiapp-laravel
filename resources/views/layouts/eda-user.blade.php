7565b41d9f76242bc10d885f87b8c82a6b1dd376
Daustinn <daustinn.music@gmail.com>
2024-07-03 13:52:41 -0500

    refactor: Update sidebar layout, fix image empty condition, and add avatar component

--- resources/views/layouts/eda-user.blade.php
+++ resources/views/layouts/eda-user.blade.php
@@ -7,65 +7,85 @@
 @endphp
 
 @section('content')
-    <div class="text-black h-full py-2 w-full flex-grow flex flex-col overflow-y-auto">
-        <header class="space-y-4">
-            <nav class="flex items-center gap-2 p-4 pb-0 hover:[&>a]:text-blue-600 hover:[&>a]:underline text-base">
-                <div class="flex text-sm items-center gap-3">
-                    <div class="w-28 rounded-full overflow-hidden border aspect-square">
-                        <img src={{ $user->profile }} class="w-full h-full object-cover" alt="">
+    <div class="text-black h-full py-2 max-sm:py-1 w-full flex-grow flex flex-col overflow-y-auto">
+        <header class="space-y-4 mb-3">
+            <div>
+                <h2 class="text-xl text-neutral-700 tracking-tight font-semibold pb-3">Colaborador</h2>
+                <div class="bg-white w-fit max-sm:w-full rounded-xl border border-neutral-100">
+                    <div class="border-b p-2 px-3">
+                        <p class="text-xl">
+                            <a title="Ir al perfil de {{ $user->first_name }} {{ $user->last_name }}"
+                                href="/profile/{{ $user->id }}" class="hover:underline">
+                                <span>{{ ucfirst(strtolower(explode(' ', $user->first_name)[0])) }}</span>
+                                <span>{{ ucfirst(strtolower(explode(' ', $user->last_name)[0])) }}</span>
+                            </a>
+                        </p>
                     </div>
-                    <div class="flex flex-col gap-1">
-                        <p class="text-xl font-bold tracking-wide">
-                            <span>{{ ucfirst(strtolower(explode(' ', $user->first_name)[0])) }}</span>
-                            <span>{{ ucfirst(strtolower(explode(' ', $user->last_name)[0])) }}</span>
-                        </p>
-                        <p class="font-normal text-neutral-600">
-                            {{ $user->role_position->name }} • {{ $user->role_position->job_position->name }}
+                    <div class="flex w-fit items-center gap-6 p-3">
+                        @include('commons.avatar', [
+                            'src' => $user->profile,
+                            'className' => 'w-28 max-sm:w-14',
+                            'alt' => $user->first_name . ' ' . $user->last_name,
+                            'altClass' => 'text-2xl',
+                        ])
+                        <div class="flex flex-col gap-1">
 
-                        </p>
-
-                        @if ($user->id_supervisor)
-                            <p class="text-neutral-600 inline-flex items-center gap-2">
-                                Supervisado por:
-                                <a href="/profile/" class="text-indigo-700 inline-flex gap-1 items-center hover:underline">
-                                    <span class="w-4 border inline-block rounded-xl overflow-hidden aspect-square">
-                                        <img src={{ $user->supervisor->profile }} class="w-full h-full object-cover"
-                                            alt="">
-                                    </span>
-                                    {{ $user->supervisor->last_name }},
-                                    {{ $user->supervisor->first_name }}
+                            <p class="font-normal text-neutral-600">
+                                {{ $user->role_position->name }} • {{ $user->role_position->department->name }}
+                            </p>
+                            <p class="font-normal text-neutral-600">
+                                <a title="Ir al perfil de {{ $user->first_name }} {{ $user->last_name }}"
+                                    href="/profile/{{ $user->id }}" class="hover:underline text-blue-600">
+                                    Ver perfil
                                 </a>
                             </p>
-                        @endif
-                        <a href="" class="text-[#5b5fc7] text-sm hover:underline flex items-center gap-1">
-                            Organización
-                            <svg xmlns="http://www.w3.org/2000/svg" width="15" viewBox="0 0 24 24" fill="none"
-                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                class="lucide lucide-network">
-                                <rect x="16" y="16" width="6" height="6" rx="1" />
-                                <rect x="2" y="16" width="6" height="6" rx="1" />
-                                <rect x="9" y="2" width="6" height="6" rx="1" />
-                                <path d="M5 16v-3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3" />
-                                <path d="M12 12V8" />
-                            </svg>
-                        </a>
+
+                            @if ($user->id_supervisor)
+                                <p class="text-neutral-600 max-sm:hidden inline-flex items-center gap-2">
+                                    Supervisado por:
+                                    <a title="Ir al perfil de {{ $user->supervisor->first_name }} {{ $user->supervisor->last_name }}"
+                                        href="/profile/"
+                                        class="text-blue-700 inline-flex gap-1 items-center hover:underline">
+
+                                        {{ $user->supervisor->last_name }},
+                                        {{ $user->supervisor->first_name }}
+                                    </a>
+                                </p>
+                            @endif
+
+                        </div>
                     </div>
                 </div>
-            </nav>
-            <nav class="border-b flex text-neutral-700">
+            </div>
+            <nav class="flex items-center font-semibold overflow-x-auto text-neutral-700 gap-2.5">
+                <h1>Edas</h1>
                 @foreach ($years as $year)
                     <a {{ request()->is('edas/' . $user->id . '/eda/' . $year->id . '*') ? 'data-state=open' : '' }}
                         href="{{ route('edas.user.eda', ['id_user' => $user->id, 'year' => $year->id]) }}"
-                        class="p-3 px-6 data-[state=open]:text-[#5b5fc7] data-[state=open]:font-medium rounded-sm relative hover:bg-neutral-200">
+                        class="p-1.5 px-5 border-neutral-300 data-[state=open]:border-2 data-[state=open]:border-blue-600 text-black data-[state=open]:bg-blue-100 hover:bg-blue-50 border data-[state=open]:font-medium rounded-full relative">
                         {{ $year->name }}
-                        <span {{ request()->is('edas/' . $user->id . '/eda/' . $year->id . '*') ? 'data-state=open' : '' }}
-                            class="absolute hidden data-[state=open]:block left-0 bottom-0 inset-x-0 border-b-2 border-[#5b5fc7]"></span>
+
                     </a>
                 @endforeach
             </nav>
         </header>
-        <div class="h-full pt-4 overflow-y-auto">
-            @yield('content-eda-user')
-        </div>
+        @if ($eda)
+            <div class="h-full overflow-y-auto bg-white border rounded-xl">
+                @yield('content-eda-user')
+            </div>
+        @else
+            <div class="grid text-center h-full gap-2 place-content-center p-10">
+                <h2 class="text-xl font-semibold tracking-tight">Eda no disponible</h2>
+                <img src="/empty_filter.webp" width="200" class="mx-auto" alt="">
+                <p class="text-neutral-400">Aun no se registró el eda del año {{ $year->name }}</p>
+                @if ($current_user->hasPrivilege('create_edas'))
+                    <button {{ !$isPosibleCreateEda ? 'disabled' : '' }} data-id-year="{{ $id_year }}"
+                        data-id-user="{{ $user->id }}" id="create-eda"
+                        class="p-2 disabled:opacity-50 rounded-full w-fit mx-auto px-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold text-base">
+                        Registrar ahora
+                    </button>
+                @endif
+            </div>
+        @endif
     </div>
 @endsection
