bbe1961116d2429a4fa60d6a66d02494324da848
Daustinn <daustinn.music@gmail.com>
2024-07-03 13:52:47 -0500

    refactor: Update home page to include avatar component and improve user greeting

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,10 +3,13 @@
 @section('content')
     <div class="grid place-content-center p-4 text-black w-full flex-grow">
         <h1 class="text-md max-w-[20ch] text-center">
-            <div class="w-28 rounded-full overflow-hidden mx-auto border aspect-square">
-                <img src={{ $current_user->profile }} class="w-full h-full object-cover" alt="">
-            </div>
-            Hola, <b class="text-indigo-700">{{ $current_user->first_name }}</b> Bienvenido al sistema
+            @include('commons.avatar', [
+                'src' => $current_user->profile,
+                'className' => 'w-28 mx-auto',
+                'alt' => $current_user->first_name . ' ' . $current_user->last_name,
+                'altClass' => 'text-3xl',
+            ])
+            Hola, <b class="text-blue-700">{{ $current_user->first_name }}</b> Bienvenido al sistema
         </h1>
     </div>
 @endsection
