85a62138024328d4dc049675dbe274091f6df961
Daustinn <daustinn.music@gmail.com>
2024-07-03 13:52:26 -0500

    refactor: Add avatar component for displaying user avatars

---
+++ resources/views/commons/avatar.blade.php
@@ -0,0 +1,13 @@
+<div class="rounded-full overflow-hidden border aspect-square {{ $className }}">
+    @if ($src)
+        <img src={{ $src }} class="w-full h-full object-cover" alt="">
+    @else
+        @php
+            $name = explode(' ', $alt);
+            $initials = strtoupper($name[0][0] . $name[1][0]);
+        @endphp
+        <div class="w-full h-full pointer-events-none select-none grid place-content-center bg-neutral-200/70">
+            <span class="font-semibold tracking-tight text-stone-600 {{ $altClass }}">{{ $initials }}</span>
+        </div>
+    @endif
+</div>
