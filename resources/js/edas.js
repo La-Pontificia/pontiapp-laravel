4d94427e4ba64d4f1bc15b9ca20552c0cfd2ddd4
Daustinn <daustinn.music@gmail.com>
2024-07-03 19:00:50 -0500

    refactor: Add close EDA functionality and confirmation dialog

--- resources/js/edas.js
+++ resources/js/edas.js
@@ -9,6 +9,7 @@ const Toast = Swal.mixin({
 document.addEventListener("DOMContentLoaded", function () {
     $ = document.querySelector.bind(document);
     const CreateButton = $("#create-eda");
+    const CloseButton = $("#close-eda");
     CreateButton?.addEventListener("click", async () => {
         // disable the button
         CreateButton.disabled = true;
@@ -28,8 +29,40 @@ document.addEventListener("DOMContentLoaded", function () {
                 icon: "error",
                 title:
                     error.response.data ??
-                    "Error al cambiar el estado de la plantilla",
+                    "Ocurrió un error al intentar crear el EDA",
             });
         }
+    });
+
+    CloseButton?.addEventListener("click", () => {
+        CloseButton.disabled = true;
+
+        const id_eda = CloseButton.getAttribute("data-id");
+
+        Swal.fire({
+            title: "¿Estas seguro de cerrar el EDA?",
+            text: "No podras deshacer esta acción",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: `Sí, cerrar`,
+            cancelButtonText: "Cancelar",
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    await axios.post(`/api/edas/close/${id_eda}`);
+                    // reload the page
+                    location.reload();
+                } catch (error) {
+                    window.toast.fire({
+                        icon: "error",
+                        title:
+                            error.response.data ??
+                            "Ocurrió un error al intentar cerrar el EDA",
+                    });
+                }
+            }
+        });
     });
 });
