5d72be862b854851207fa1650d5a044af719fbfd
Daustinn <daustinn.music@gmail.com>
2024-07-04 20:14:56 -0500

    refactor: Update user avatar display in multiple files

--- resources/views/commons/avatar.blade.php
+++ resources/views/commons/avatar.blade.php
@@ -6,7 +6,7 @@
             $name = explode(' ', $alt);
             $initials = strtoupper($name[0][0] . $name[1][0]);
         @endphp
-        <div class="w-full h-full pointer-events-none select-none grid place-content-center bg-neutral-200/70">
+        <div class="w-full h-full pointer-events-none select-none grid place-content-center bg-stone-50/70">
             <span class="font-semibold tracking-tight text-stone-600 {{ $altClass }}">{{ $initials }}</span>
         </div>
     @endif
