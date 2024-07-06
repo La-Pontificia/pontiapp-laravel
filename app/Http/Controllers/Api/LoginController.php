58ae0b7bdefd224cc38b890e015e092a04ce02ea
Daustinn <daustinn.music@gmail.com>
2024-07-05 21:35:09 -0500

    feat: Add email validation and error handling to login functionality

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Email;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,9 +13,25 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)) {
-            return redirect()->intended('/');
+        $find = Email::where('email', $credentials['email'])->first();
+
+        if (!$find) {
+            return back()->withErrors(['email' => 'Invalid credentials'])->onlyInput('email');
         }
+
+        $user = $find->user;
+
+        if (!$user->status) {
+            return back()->withErrors(['email' => 'Tu cuenta no está activa. Comunícate con el administrador.'])->onlyInput('email');
+        }
+
+
+        // verify the password
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended($this->redirectPath());
+        }
+
         return back()->withErrors(['email' => 'Invalid credentials'])->onlyInput('email');
     }
 
