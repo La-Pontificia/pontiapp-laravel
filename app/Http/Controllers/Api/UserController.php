609189e295ba0e6560c9d02591ae23655526d7b4
Daustinn <daustinn.music@gmail.com>
2024-07-08 14:23:26 -0500

    refactor: Add email assignment and discharge functionality to UserController

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Email;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -16,28 +17,28 @@ class UserController extends Controller
         $privileges = [];
 
         if ($request->role === 'user') {
-            $privileges = ["view_edas", "create_edas"];
+            $privileges = ["view_users", "create_users", "edit_users", "view_attendance", "create_schedule", "edit_schedule", "delete_schedule", "view_edas", "create_edas", "edit_edas", "restart_edas", "view_collaborators", "approve_goals", "self_qualify", "average_evaluation", "close_evaluation", "closed_edas"];
         }
         if ($request->role === 'admin') {
-            $privileges = ["view_edas", "create_edas"];
+            $privileges = ["view_users", "create_users", "edit_users", "assign_email", "disable_users", "view_attendance", "create_schedule", "edit_schedule", "delete_schedule", "view_edas", "create_edas", "edit_edas", "restart_edas", "view_collaborators", "approve_goals", "self_qualify", "average_evaluation", "close_evaluation", "closed_edas"];
         }
 
         $already = User::where('dni', $request->dni)->first();
         if ($already)
             return response()->json('El usuario con el dni ingresado ya existe', 400);
 
-        $email_already = User::where('email', $request->email)->first();
-        if ($email_already)
-            return response()->json('El usuario con el Correo ingresado ya existe', 400);
+        $email_already = Email::where('email', $request->email)->get();
+
+        if ($email_already->count() > 0)
+            return response()->json('El correo ingresado ya esta en uso por otra cuenta', 400);
 
         $cuser = auth()->user();
 
-        $user =  User::create([
+        $user = User::create([
             'profile' => $request->profile ? $request->profile : 'https://res.cloudinary.com/dc0t90ahb/image/upload/v1706396604/gxhlhgd1aa7scbneae3s.jpg',
             'dni' => $request->dni,
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
-            'email' => $request->email,
             'password' => bcrypt($request->dni),
             'role' => $request->role,
             'privileges' => json_encode($privileges),
@@ -46,6 +47,14 @@ class UserController extends Controller
             'id_branch' => $request->id_branch,
             'id_supervisor' => $request->supervisor,
             'created_by' => $cuser->id,
+        ]);
+
+
+        Email::create([
+            'email' => $request->email,
+            'id_user' => $user->id,
+            'reason' => 'Email asignado al momento de crear el usuario',
+            'assigned_by' => $cuser->id,
         ]);
 
         return response()->json(['success' => $user], 200);
@@ -65,10 +74,6 @@ class UserController extends Controller
         if ($already && $already->id !== $id)
             return response()->json('El usuario con el dni ingresado ya existe', 400);
 
-        $email_already = User::where('email', $request->email)->first();
-        if ($email_already && $email_already->id !== $id)
-            return response()->json('El usuario con el Correo ingresado ya existe', 400);
-
         $cuser = auth()->user();
 
         $privileges = json_decode($request->input('privileges'), true);
@@ -78,7 +83,6 @@ class UserController extends Controller
             'dni' => $request->dni,
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
-            'email' => $request->email,
             'privileges' => $privileges,
             'role' => $request->role,
             'id_role' => $request->id_role,
