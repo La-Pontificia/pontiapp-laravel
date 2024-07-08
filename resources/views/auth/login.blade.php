f79be58f21288ce383d8d6c74261f90b3b7f5167
Daustinn <daustinn.music@gmail.com>
2024-07-08 14:23:55 -0500

    refactor: Update login form styling and validation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,13 +13,14 @@
                             Centro de Administración La Pontificia
                         </h1>
                         <div class="w-full">
-                            <form method="POST" action="/api/login" class="grid gap-2">
+                            <form method="POST" action="/api/login" class="grid gap-5">
                                 @csrf
                                 <div class="divide-y border rounded-xl">
-                                    <input required autofocus class="w-full border-0 p-3 rounded-t-xl" name="email"
-                                        type="email" value="{{ old('email') }}" placeholder="Correo electrónico" />
+                                    <input data-notstyles required autofocus class="w-full border-0 p-3 rounded-t-xl"
+                                        name="email" type="email" value="{{ old('email') }}"
+                                        placeholder="Correo electrónico" />
 
-                                    <input required class="w-full p-3 rounded-b-xl" name="password"
+                                    <input data-notstyles required class="w-full p-3 rounded-b-xl" name="password"
                                         value="{{ old('password') }}" placeholder="Contraseña" />
                                 </div>
                                 @error('email')
@@ -29,19 +30,19 @@
                                 @enderror
 
                                 <button type="submit"
-                                    class="p-3 rounded-xl font-semibold bg-black text-white w-full hover:text-neutral-300">
+                                    class="p-4 rounded-xl font-semibold bg-black text-white w-full hover:text-neutral-300">
                                     <span class="ml-3">
                                         Entrar
                                     </span>
                                 </button>
                             </form>
-                            <div class="flex items-center gap-2 mt-2 text-sm">
+                            <div class="flex items-center gap-2 mt-4 text-sm">
                                 <span class="w-full border-b block"></span>
                                 <p class="text-nowrap">o continuar con</p>
                                 <span class="w-full border-b block"></span>
                             </div>
                             <a title="Aun no disponible" href="{{ route('login.azure') }}"
-                                class="w-full mt-2 gap-2 rounded-xl justify-center text-gray-900 bg-white hover:border-blue-600 border border-neutral-200 text-sm p-4 text-center inline-flex items-center">
+                                class="w-full shadow-md hover:shadow-lg mt-4 gap-2 rounded-lg justify-center text-gray-900 bg-white hover:border-blue-600 text-sm p-4 text-center inline-flex items-center">
                                 <img src="/RE1Mu3b.png" class="w-24" alt="">
                             </a>
                             @if (session('error'))
