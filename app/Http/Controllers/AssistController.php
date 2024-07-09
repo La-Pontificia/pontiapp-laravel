16d9a87e2e6f92717423ce1403ea9022f7c4e347
Daustinn <daustinn.music@gmail.com>
2024-07-08 19:17:38 -0500

    refactor: Add user attendance schedule page for specific user

--- app/Http/Controllers/AssistController.php
+++ app/Http/Controllers/AssistController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Attendance;
+use App\Models\User;
 
 class AssistController extends Controller
 {
@@ -10,6 +11,18 @@ class AssistController extends Controller
     {
         $assists = Attendance::orderBy('punch_time', 'desc')->paginate();
         return view('pages.assists.index', compact('assists'))
+            ->with('i', (request()->input('page', 1) - 1) * $assists->perPage());
+    }
+
+    public function user($id_user)
+    {
+        $user = User::find($id_user);
+
+        if (!$user) return view('pages.500', ['error' => 'User not found']);
+
+        $assists = Attendance::where('emp_code', $user->dni)->orderBy('punch_time', 'desc')->paginate();
+
+        return view('pages.assists.user.schedules', compact('user', 'assists'))
             ->with('i', (request()->input('page', 1) - 1) * $assists->perPage());
     }
 }
