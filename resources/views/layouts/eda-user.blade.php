377d83d9e02be8b2bb713b3e6e240751fff5b959
Daustinn <daustinn.music@gmail.com>
2024-07-08 19:18:06 -0500

    refactor: Update user profile layout and navigation

--- resources/views/layouts/eda-user.blade.php
+++ resources/views/layouts/eda-user.blade.php
@@ -10,60 +10,15 @@
     <div class="text-black h-full py-2 max-sm:py-1 w-full flex-grow flex flex-col overflow-y-auto">
         <header class="space-y-3 pb-2">
             <div>
-                <div class="bg-white shadow-md w-fit max-sm:w-full rounded-2xl border border-neutral-200">
-                    <div class="border-b p-2 px-3">
-                        <p class="text-base font-semibold">
-                            <a title="Ir al perfil de {{ $user->first_name }} {{ $user->last_name }}"
-                                href="/profile/{{ $user->id }}" class="hover:underline">
-                                <span>{{ ucfirst(strtolower(explode(' ', $user->first_name)[0])) }}</span>
-                                <span>{{ ucfirst(strtolower(explode(' ', $user->last_name)[0])) }}</span>
-                            </a>
-                        </p>
-                    </div>
-                    <div class="flex w-fit items-center gap-6 p-3">
-                        @include('commons.avatar', [
-                            'src' => $user->profile,
-                            'className' => 'w-24 max-sm:w-14',
-                            'alt' => $user->first_name . ' ' . $user->last_name,
-                            'altClass' => 'text-3xl',
-                        ])
-                        <div class="flex flex-col gap-1">
-
-                            <p class="font-normal text-neutral-600">
-                                {{ $user->role_position->name }} • {{ $user->role_position->department->name }}
-                            </p>
-                            <p class="font-normal text-neutral-600">
-                                <a title="Ir al perfil de {{ $user->first_name }} {{ $user->last_name }}"
-                                    href="/profile/{{ $user->id }}" class="hover:underline text-blue-600">
-                                    Ver perfil
-                                </a>
-                            </p>
-
-                            @if ($user->id_supervisor)
-                                <p class="text-neutral-600 max-sm:hidden inline-flex items-center gap-2">
-                                    Supervisado por:
-                                    <a title="Ir al perfil de {{ $user->supervisor->first_name }} {{ $user->supervisor->last_name }}"
-                                        href="/profile/"
-                                        class="text-blue-700 inline-flex gap-1 items-center hover:underline">
-
-                                        {{ $user->supervisor->last_name }},
-                                        {{ $user->supervisor->first_name }}
-                                    </a>
-                                </p>
-                            @endif
-
-                        </div>
-                    </div>
-                </div>
+                @include('components.users.card', ['user' => $user])
             </div>
             <nav class="flex items-center font-semibold overflow-x-auto text-neutral-700 gap-2.5">
                 <h1>Edas</h1>
-                @foreach ($years as $year)
-                    <a {{ request()->is('edas/' . $user->id . '/eda/' . $year->id . '*') ? 'data-state=open' : '' }}
-                        href="{{ route('edas.user', ['id_user' => $user->id, 'year' => $year->id]) }}"
+                @foreach ($years as $y)
+                    <a {{ request()->is('edas/' . $user->id . '/eda/' . $y->id . '*') ? 'data-state=open' : '' }}
+                        href="{{ route('edas.user.year', ['id_user' => $user->id, 'id_year' => $y->id]) }}"
                         class="p-1.5 px-5 border-neutral-300 data-[state=open]:border-2 data-[state=open]:border-blue-600 text-black data-[state=open]:bg-blue-100 hover:bg-blue-50 border data-[state=open]:font-medium rounded-full relative">
-                        {{ $year->name }}
-
+                        {{ $y->name }}
                     </a>
                 @endforeach
             </nav>
@@ -87,9 +42,9 @@
             <div class="grid text-center h-full gap-2 place-content-center p-10">
                 <h2 class="text-xl font-semibold tracking-tight">Eda no disponible</h2>
                 <img src="/empty_filter.webp" width="200" class="mx-auto" alt="">
-                <p class="text-neutral-400">Aun no se registró el eda del año {{ $year->name }}</p>
+                <p class="text-neutral-400">Aun no se registró el eda del año {{ $current_year->name }}</p>
                 @if ($current_user->hasPrivilege('create_edas'))
-                    <button {{ !$hasPosibleCreate ? 'disabled' : '' }} data-id-year="{{ $id_year }}"
+                    <button {{ !$hasPosibleCreate ? 'disabled' : '' }} data-id-year="{{ $current_year->id }}"
                         data-id-user="{{ $user->id }}" id="create-eda"
                         class="p-2 disabled:opacity-50 rounded-full w-fit mx-auto px-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold text-base">
                         Registrar ahora
