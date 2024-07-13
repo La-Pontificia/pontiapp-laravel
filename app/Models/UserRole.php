1a90b5678b616ec49780e83eeca7b80f13922d37
Daustinn <daustinn.music@gmail.com>
2024-07-13 18:20:15 -0500

    refactor: Add UserRole model and update UserController to handle user role-related functionality

---
+++ app/Models/UserRole.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserRole extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'user_roles';
+
+    static $rules = [
+        'title' => 'required|string|max:255',
+        'privileges' => 'required|array',
+    ];
+
+    protected $fillable = [
+        'id',
+        'title',
+        'privileges',
+        'status',
+        'created_by',
+        'updated_by',
+    ];
+
+    protected $casts = [
+        'privileges' => 'array',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'id_role', 'id');
+    }
+}
