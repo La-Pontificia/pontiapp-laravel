5b25f301365b682cdda12c34d6b7a24138f6b885
Daustinn <daustinn.music@gmail.com>
2024-07-13 18:21:59 -0500

    refactor: Update app.blade.php layout to remove sidebar component and improve content display

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,12 +14,10 @@
             <div id="app" class="flex flex-col h-full flex-grow overflow-y-auto">
                 @include('components.header')
                 <main class="flex h-full overflow-auto">
-                    @include('components.sidebar')
                     <div class="overflow-auto w-full flex-grow p-2 flex flex-col h-full">
                         @yield('content')
                     </div>
                 </main>
-
             </div>
         @endguest
     </div>
