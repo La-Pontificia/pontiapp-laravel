c020ed51ced5beb822368b4f231f04a88101489d
Daustinn <daustinn.music@gmail.com>
2024-07-14 02:43:16 -0500

    refactor: Update user module layout and components

---
+++ resources/views/modules/users/+layout.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Gestión de usuarios')
+
+@section('content')
+    <div class="text-black w-full flex-grow flex overflow-y-auto">
+        @include('components.modules.users.sidebar')
+        <div class="w-full overflow-y-auto flex flex-col">
+            @yield('layout.users')
+        </div>
+    </div>
+@endsection
