605bb75a58947503139ded5a91780e8d425d12b0
Daustinn <daustinn.music@gmail.com>
2024-07-14 02:43:46 -0500

    refactor: Add 403 error page

---
+++ resources/views/pages/+403.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.headers')
+
+@section('app')
+    <div class="flex flex-col h-svh">
+        <div class="w-full grid h-full place-content-center">
+            <div class="text-center space-y-3">
+                <h1 class="text-7xl font-semibold tracking-tight">401</h1>
+                <p class="text-sm">
+                    No tienes permisos para acceder a esta página.
+                </p>
+                <a href="/"
+                    class="w-fit mx-auto block rounded-full font-semibold border-2 text-blue-700 hover:bg-blue-200 border-blue-700 py-1.5 px-4">
+                    Ir a inicio
+                </a>
+            </div>
+        </div>
+        <footer class="p-4 text-center text-xs">
+            Desarrollado por <a href="https://daustinn.com" target="_blank" rel="noopener noreferrer"
+                class=" font-semibold hover:underline">Daustinn</a> &copy; 2024
+        </footer>
+    </div>
+@endsection
