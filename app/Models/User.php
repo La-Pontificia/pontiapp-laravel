2ee965ec994c513a7281e7d8a26587a8db01c863
Daustinn <daustinn.music@gmail.com>
2024-07-15 00:29:09 -0500

    refactor: Add 'group_schedule_id' field to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,12 +25,14 @@ class User extends Authenticatable
         'dni',
         'first_name',
         'last_name',
+        'email',
         'password',
         'id_role_user',
         'privileges',
         'status',
         'id_role',
         'id_branch',
+        'group_schedule_id',
         'created_by',
         'updated_by',
     ];
@@ -53,51 +55,53 @@ class User extends Authenticatable
     static $rules = [
         'dni' => 'required|numeric|digits:8',
         'first_name' => 'required',
+        'username' => 'required',
+        'domain' => 'required',
         'last_name' => 'required',
         'id_role' => ['required', 'max:36'],
         'id_role_user' => ['required', 'uuid'],
         'id_branch' => ['required', 'uuid'],
+        'group_schedule_id' => ['uuid'],
     ];
-
 
 
     public function role_position()
     {
-        return $this->hasOne('App\Models\Role', 'id', 'id_role');
+        return $this->hasOne(Role::class, 'id', 'id_role');
     }
 
     public function branch()
     {
-        return $this->hasOne('App\Models\Branch', 'id', 'id_branch');
+        return $this->hasOne(Branch::class, 'id', 'id_branch');
     }
 
     public function createdBy()
     {
-        return $this->hasOne('App\Models\User', 'id', 'created_by');
+        return $this->hasOne(User::class, 'id', 'created_by');
     }
 
     public function updatedBy()
     {
-        return $this->hasOne('App\Models\User', 'id', 'updated_by');
+        return $this->hasOne(User::class, 'id', 'updated_by');
     }
 
     public function emails()
     {
-        return $this->hasMany('App\Models\Email', 'id_user', 'id');
-    }
-
-    public function email()
-    {
-        return $this->emails()->first()->email;
+        return $this->hasMany(Email::class, 'id_user', 'id');
     }
 
     public function role()
     {
-        return $this->hasOne('App\Models\UserRole', 'id', 'id_role_user');
+        return $this->hasOne(UserRole::class, 'id', 'id_role_user');
     }
 
     public function hasPrivilege($key)
     {
         return in_array($key, $this->role->privileges);
+    }
+
+    public function groupSchedule()
+    {
+        return $this->hasOne(GroupSchedule::class, 'id', 'group_schedule_id');
     }
 }
