275a3e1b6ee3ea25f1b3a734f4cc4e440a478c2f
Daustinn <daustinn.music@gmail.com>
2024-07-15 00:30:40 -0500

    refactor: Add page for displaying user domains

---
+++ resources/views/modules/users/emails/+page.blade.php
@@ -0,0 +1,9 @@
+@extends('modules.users.+layout')
+
+@section('title', 'Horarios')
+
+@section('layout.users')
+    <div class="text-black w-full flex-col flex-grow flex overflow-y-auto">
+        Dominios
+    </div>
+@endsection
