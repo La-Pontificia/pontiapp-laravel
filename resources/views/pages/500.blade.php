ff1088f0bc153a4ce65f34897e38625b93dac84b
Daustinn <daustinn.music@gmail.com>
2024-07-15 00:32:08 -0500

    chore: Update 500 error page layout and styling

--- resources/views/pages/500.blade.php
+++ resources/views/pages/500.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.headers')
 
 @section('app')
-    <div class="flex flex-col h-svh">
+    <div class="flex flex-col h-svh bg-black text-white">
         <div class="w-full grid h-full place-content-center">
             <div class="text-center space-y-3">
-                <h1 class="text-7xl font-semibold tracking-tight">500</h1>
-                <p class="text-sm">
-                    A ocurrido un error, por favor intenta de nuevo.
-                    "<span class="text-blue-600">{{ $error }}</span>"
+                <h1 class="text-4xl font-serif tracking-tight">Error 500</h1>
+                <p class="text-sm max-w-[35ch] text-stone-300">
+                    A ocurrido un error, por favor intenta de nuevo
+                    o volver al <a href="/" class="text-blue-400 hover:underline">inicio</a>.
                 </p>
-                <a href="/"
-                    class="w-fit mx-auto block rounded-full font-semibold border-2 text-blue-700 hover:bg-blue-200 border-blue-700 py-1.5 px-4">
-                    Ir a inicio
-                </a>
+
             </div>
         </div>
-        <footer class="p-4 text-center text-xs">
+        <footer class="p-4 text-center text-xs text-stone-400">
             Desarrollado por <a href="https://daustinn.com" target="_blank" rel="noopener noreferrer"
                 class=" font-semibold hover:underline">Daustinn</a> &copy; 2024
         </footer>
