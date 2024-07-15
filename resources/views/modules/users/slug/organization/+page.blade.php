b6e7439e6b593cfefd3b94f7baf3c6ad349d674e
Daustinn <daustinn.music@gmail.com>
2024-07-15 00:31:31 -0500

    refactor: Add organization page for user profile

---
+++ resources/views/modules/users/slug/organization/+page.blade.php
@@ -0,0 +1,12 @@
+@extends('modules.users.slug.+layout')
+
+@section('title', 'Organización: ' . $user->first_name . ', ' . $user->last_name)
+
+
+@section('layout.users.slug')
+    <div class="p-20 space-y-4 flex flex-col items-center w-full">
+        <h1 class="font-serif text-2xl tracking-tight">
+            Modulo de organización
+        </h1>
+    </div>
+@endsection
