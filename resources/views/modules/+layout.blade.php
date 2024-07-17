46afe5fe18214120d7f0ed5e290eab5158224fff
Daustinn <daustinn.music@gmail.com>
2024-07-17 15:26:28 -0500

    refactor: Update user roles page layout
    Include conditional check for user privileges in the user roles page layout. Display form if user has edit role privilege, otherwise show a 403 error page with a message indicating lack of permission. Improves user experience by providing appropriate access control for role editing functionality.

--- resources/views/modules/+layout.blade.php
+++ resources/views/modules/+layout.blade.php
@@ -14,7 +14,36 @@
             <div id="app" class="flex flex-col h-full flex-grow overflow-y-auto">
                 @include('components.header')
                 <main class="flex h-full overflow-auto">
-                    @include('components.modules.sidebar')
+                    <aside id="cta-sidebar"
+                        class="z-40 w-[270px] min-w-[270px] transition-all h-full max-md:top-12 max-md:bg-white max-md:border-r overflow-y-auto max-md:-translate-x-full max-md:fixed"
+                        aria-label="Sidebar">
+                        <div class="h-full pr-0 flex flex-col">
+                            <div class="flex-grow">
+                                @if (request()->is('/'))
+                                    @include('modules.sidebar')
+                                @elseif(request()->is('users*'))
+                                    @include('modules.users.sidebar')
+                                @endif
+                            </div>
+                            <footer class="p-5 hover:[&>a]:underline text-xs text-black text-center">
+                                <a href="">
+                                    Terminos y condiciones
+                                </a>
+                                ·
+                                <a href="">
+                                    Política de privacidad
+                                </a>
+                                ·
+                                <a href="">
+                                    Ayuda
+                                </a>
+                                ·
+                                <a target="_blank" href="https://daustinn.com">
+                                    Daustinn
+                                </a>
+                            </footer>
+                        </div>
+                    </aside>
                     <div class="overflow-auto w-full flex-grow p-2 flex flex-col h-full">
                         @yield('content')
                     </div>
