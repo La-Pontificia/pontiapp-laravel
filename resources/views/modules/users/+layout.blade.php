954758a3a709bdabf2488d391d5dbc95bb9f9c83
Daustinn <daustinn.music@gmail.com>
2024-07-17 15:26:08 -0500

    refactor: Update user roles page layout
    Include conditional check for user privileges in the user roles page layout. Display form if user has edit role privilege, otherwise show a 403 error page with a message indicating lack of permission. Improves user experience by providing appropriate access control for role editing functionality.

--- resources/views/modules/users/+layout.blade.php
+++ resources/views/modules/users/+layout.blade.php
@@ -1,12 +1,13 @@
-@extends('layouts.app')
+@extends('modules.+layout')
 
 @section('title', 'Gestión de usuarios')
 
 @section('content')
-    <div class="text-black w-full flex-grow flex overflow-auto">
+    {{-- <div class="text-black w-full flex-grow flex overflowy-auto">
         @include('modules.users.sidebar')
         <div class="w-full overflow-auto flex flex-col h-full">
             @yield('layout.users')
         </div>
-    </div>
+    </div> --}}
+    @yield('layout.users')
 @endsection
