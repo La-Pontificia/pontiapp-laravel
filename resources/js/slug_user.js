b54505038e617b986fce78d4c73ff6398c10a0ac
Daustinn <daustinn.music@gmail.com>
2024-07-16 23:52:02 -0500

    refactor: Update date range picker functionality and improve UI accessibility

--- resources/js/slug_user.js
+++ resources/js/slug_user.js
@@ -1,19 +1,45 @@
-import DateRangePicker from "vanillajs-datepicker/DateRangePicker";
+import moment from "moment";
+import { DateRangePicker } from "vanillajs-datepicker";
 import "vanillajs-datepicker/css/datepicker-bulma.css";
+import "vanillajs-datepicker/locales/es";
 
 document.addEventListener("DOMContentLoaded", async () => {
     $ = document.querySelector.bind(document);
 
     const elem = $("#date-range") || null;
-    const el = document.createElement("input");
+    const el = document.createElement("div");
+
+    const startDate = moment().startOf("month").format("YYYY-MM-DD");
+    const endDate = moment().endOf("month").format("YYYY-MM-DD");
+
     const rangepicker = new DateRangePicker(elem ?? el, {
         format: "yyyy-mm-dd",
+        clearButton: true,
+        enableOnReadonly: true,
+        language: "es",
     });
 
     const buttonFilter = $("#filter");
 
-    filter?.addEventListener("click", () => {
+    const element = elem ?? el;
+    const [startInput, endInput] = element?.querySelectorAll("input");
+    const startDefault = startInput?.getAttribute("data-default");
+    const endDefault = endInput?.getAttribute("data-default");
+
+    if (startInput && endInput) {
+        rangepicker.setDates(startDefault ?? startDate, endDefault ?? endDate);
+    }
+
+    buttonFilter?.addEventListener("click", () => {
         const dates = rangepicker.getDates();
-        console.log(dates);
+        const params = new URLSearchParams(window.location.search);
+        if (dates[0] && dates[1]) {
+            params.set("start_date", moment(dates[0]).format("YYYY-MM-DD"));
+            params.set("end_date", moment(dates[1]).format("YYYY-MM-DD"));
+        } else {
+            params.delete("start_date");
+            params.delete("end_date");
+        }
+        window.location.search = params.toString();
     });
 });
