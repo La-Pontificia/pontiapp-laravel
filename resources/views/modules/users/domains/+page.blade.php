a46d53bcc075d7c7ef72ded4e279fb9c602ea820
Daustinn <daustinn.music@gmail.com>
2024-07-16 23:52:14 -0500

    refactor: Update page title to 'Dominios'

--- resources/views/modules/users/domains/+page.blade.php
+++ resources/views/modules/users/domains/+page.blade.php
@@ -1,6 +1,6 @@
 @extends('modules.users.+layout')
 
-@section('title', 'Horarios')
+@section('title', 'Dominios')
 
 @section('layout.users')
     <div class="text-black w-full flex-col flex-grow flex overflow-y-auto">
