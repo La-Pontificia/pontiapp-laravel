b7f41dbeca1d52bf0b6b9860e4b471b37807ecc4
Daustinn <daustinn.music@gmail.com>
2024-07-17 15:25:43 -0500

    refactor: Update user roles page layout
    The commit updates the user roles page layout to include a conditional check for user privileges. If the current user has the privilege to edit roles, the form is displayed. Otherwise, a 403 error page is shown with a message indicating that the user does not have permission to edit roles. This change improves the user experience by providing appropriate access control for role editing functionality.

--- resources/views/modules/users/roles/slug/+page.blade.php
+++ resources/views/modules/users/roles/slug/+page.blade.php
@@ -7,18 +7,24 @@
 @endphp
 
 @section('layout.users')
-    <div class="w-full h-full">
-        <form role="form" action="/api/users/roles/{{ $role->id }}" method="POST" id="role-form"
-            class="p-3 dinamic-form flex flex-col h-full gap-3" enctype="multipart/form-data">
-            <div class="flex-grow flex flex-col overflow-y-auto">
-                @include('modules.users.roles.form', ['role' => $role])
-            </div>
-            <div class="border-t pt-4">
-                <button form="role-form" type="submit"
-                    class="bg-blue-700 w-fit hover:bg-blue-600 disabled:opacity-40 disabled:pointer-events-none flex items-center rounded-xl p-2.5 gap-1 text-white font-semibold px-3">
-                    Actualizar
-                </button>
-            </div>
-        </form>
-    </div>
+    @if ($current_user->hasPrivilege('users:roles:edit'))
+        <div class="w-full h-full">
+            <form role="form" action="/api/users/roles/{{ $role->id }}" method="POST" id="role-form"
+                class="p-3 dinamic-form flex flex-col h-full gap-3" enctype="multipart/form-data">
+                <div class="flex-grow flex flex-col overflow-y-auto">
+                    @include('modules.users.roles.form', ['role' => $role])
+                </div>
+                <div class="border-t pt-4">
+                    <button form="role-form" type="submit"
+                        class="bg-blue-700 w-fit hover:bg-blue-600 disabled:opacity-40 disabled:pointer-events-none flex items-center rounded-xl p-2.5 gap-1 text-white font-semibold px-3">
+                        Actualizar
+                    </button>
+                </div>
+            </form>
+        </div>
+    @else
+        @include('+403', [
+            'message' => 'No tienes permisos para editar roles',
+        ])
+    @endif
 @endsection
