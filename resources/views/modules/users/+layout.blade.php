9598b02a1c7d0b0f05579089b5da64ac47d904c9
Daustinn <daustinn.music@gmail.com>
2024-07-16 23:52:32 -0500

    refactor: Update layout.blade.php for improved UI accessibility and responsiveness

--- resources/views/modules/users/+layout.blade.php
+++ resources/views/modules/users/+layout.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Gestión de usuarios')
 
 @section('content')
-    <div class="text-black w-full flex-grow flex overflow-y-auto">
-        @include('components.modules.users.sidebar')
-        <div class="w-full overflow-y-auto flex flex-col h-full">
+    <div class="text-black w-full flex-grow flex overflow-auto">
+        @include('modules.users.sidebar')
+        <div class="w-full overflow-auto flex flex-col h-full">
             @yield('layout.users')
         </div>
     </div>
