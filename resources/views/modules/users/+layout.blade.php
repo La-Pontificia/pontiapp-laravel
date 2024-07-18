d6642dab8590d61502dd0f36d7e456ac6488dc22
Daustinn <daustinn.music@gmail.com>
2024-07-17 21:38:02 -0500

    refactor: Remove commented out code in users module layout
    This commit removes the commented out code in the users module layout file. The code was no longer needed and was causing confusion for developers. Removing it improves code organization and reduces clutter in the repository.

--- resources/views/modules/users/+layout.blade.php
+++ resources/views/modules/users/+layout.blade.php
@@ -3,11 +3,5 @@
 @section('title', 'Gestión de usuarios')
 
 @section('content')
-    {{-- <div class="text-black w-full flex-grow flex overflowy-auto">
-        @include('modules.users.sidebar')
-        <div class="w-full overflow-auto flex flex-col h-full">
-            @yield('layout.users')
-        </div>
-    </div> --}}
     @yield('layout.users')
 @endsection
