0838b74789bf5b45b0b6cc9fb4dcac15b2e07db0
Daustinn <daustinn.music@gmail.com>
2024-07-17 21:36:29 -0500

    refactor: Update view paths for user roles in UserRoleController
    Update the view paths for the user roles in the UserRoleController to match the new directory structure. This change ensures that the correct views are rendered when accessing the user roles functionality.

--- app/Http/Controllers/UserRoleController.php
+++ app/Http/Controllers/UserRoleController.php
@@ -10,17 +10,17 @@ class UserRoleController extends Controller
     public function index()
     {
         $roles = UserRole::orderBy('created_at', 'asc')->get();
-        return view('modules.users.roles.+page', compact('roles'));
+        return view('modules.users.user-roles.+page', compact('roles'));
     }
 
     public function create()
     {
-        return view('modules.users.roles.create.+page');
+        return view('modules.users.user-roles.create.+page');
     }
 
     public function slug($id)
     {
         $role = UserRole::find($id);
-        return view('modules.users.roles.slug.+page', compact('role'));
+        return view('modules.users.user-roles.slug.+page', compact('role'));
     }
 }
