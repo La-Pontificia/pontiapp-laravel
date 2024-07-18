03ec8a8a02bfd83742e7017b1be8c9ecfbf07e7a
Daustinn <daustinn.music@gmail.com>
2024-07-17 21:37:42 -0500

    refactor: Remove unused create role page

--- resources/views/modules/users/roles/form.blade.php
+++ resources/views/modules/users/roles/form.blade.php
@@ -1,80 +1,73 @@
 @php
-    $privileges = $role ? $role->privileges : [];
+    $code = $role->code ?? null;
+    $name = $role->name ?? null;
+    $id_job_position = $role->id_job_position ?? null;
+    $id_department = $role->id_department ?? null;
 @endphp
-<label class="w-[300px] block px-1 pb-4">
-    <span>Título:</span>
-    <input value="{{ $role ? $role->title : '' }}" required type="text" name="title"
-        placeholder="Ejemplo: Administrador" class="font-medium">
-</label>
-<div class="px-1 overflow-y-auto">
-    <p class="py-4 pt-1 font-semibold tracking-tight text-sm text-stone-600">Privilegios:</p>
-    <div class="grid gap-5 grid-cols-3 max-xl:grid-cols-2 items-start">
-        @foreach ($system_privileges as $system_privilege)
-            <div class="border border-transparent">
-                <div class="flex mb-4 items-center gap-2">
-                    <button type="button" class="toggle-group">
-                        <svg xmlns="http://www.w3.org/2000/svg" style="transform: rotate(90deg);" width="24"
-                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
-                            <path d="m9 18 6-6-6-6" />
-                        </svg>
-                    </button>
-                    <label class="flex items-center gap-2 font-semibold">
-                        <input type="checkbox" class="select-all-group rounded-md p-2.5 border-neutral-400">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="17" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-file">
-                            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
-                            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
-                        </svg>
-                        <span>{{ $system_privilege['name'] }}</span>
-                    </label>
-                </div>
-                <div class="group-content grid gap-2 mt-2">
-                    @foreach ($system_privilege['items'] as $item)
-                        <div class="border border-neutral-200 bg-white p-2 rounded-lg">
-                            <div class="flex items-center gap-2">
-                                <button type="button" class="toggle-subgroup">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round"
-                                        class="lucide lucide-chevron-right">
-                                        <path d="m9 18 6-6-6-6" />
-                                    </svg>
-                                </button>
-                                <label class="flex items-center gap-2 font-semibold">
-                                    <input type="checkbox"
-                                        class="select-all-subgroup rounded-md p-2.5 border-neutral-400">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="17" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="lucide lucide-file">
-                                        <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
-                                        <path d="M14 2v4a2 2 0 0 0 2 2h4" />
-                                    </svg>
-                                    <span>{{ $item['name'] }}</span>
-                                </label>
-                                <span class="privilege-count text-sm text-gray-500">(0/0)</span>
-                            </div>
-                            <div class="subgroup-content mt-4 grid pl-10 gap-3" style="display: none;">
-                                @foreach ($item['privileges'] as $privilege => $privilege_name)
-                                    <label class="flex items-center gap-2">
-                                        <input {{ in_array($privilege, $privileges) ? 'checked' : '' }} type="checkbox"
-                                            name="privileges[]" value="{{ $privilege }}"
-                                            class="privilege-checkbox rounded-md p-2.5 border-neutral-400">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="17" viewBox="0 0 24 24"
-                                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                            stroke-linejoin="round" class="lucide lucide-file">
-                                            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
-                                            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
-                                        </svg>
-                                        <span>{{ $privilege_name }}</span>
-                                    </label>
-                                @endforeach
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        @endforeach
+
+<div class="grid gap-2">
+    <label>
+        <span class="block pb-1 text-sm font-semibold opacity-50 ">Codigo</span>
+        <input required type="text" name="code" placeholder= "{{ $code ?? $newCode }}" value="{{ $code }}"
+            class="bg-neutral-100 w-full border-2 border-neutral-400 p-1 px-2 rounded-lg">
+    </label>
+    <label>
+        <span class="block pb-1 text-sm font-semibold opacity-50 ">Nombre</span>
+        <input required value="{{ $name }}" type="text" name="name" placeholder="Nombre del cargo"
+            class="bg-neutral-100 w-full border-2 border-neutral-400 p-1 px-2 rounded-lg">
+    </label>
+    <label>
+        <span class="block pb-1 text-sm font-semibold opacity-50 ">Puesto</span>
+        <select name="id_job_position" class="bg-neutral-100 w-full border-2 border-neutral-400 p-1 px-2 rounded-lg">
+            @foreach ($jobPositions as $job)
+                <option @if ($job->id == $id_job_position) selected @endif value="{{ $job->id }}">
+                    {{ $job->name }}
+                </option>
+            @endforeach
+        </select>
+    </label>
+    <label>
+        <span class="block pb-1 text-sm font-semibold opacity-50 ">Departamento</span>
+        <select name="id_department" class="bg-neutral-100 w-full border-2 border-neutral-400 p-1 px-2 rounded-lg">
+            @foreach ($departments as $department)
+                <option @if ($department->id == $id_department) selected @endif value="{{ $department->id }}">
+                    {{ $department->name }}
+                </option>
+            @endforeach
+        </select>
+    </label>
+    {{-- <div class="autocompletes" data-atitle="full_name" data-value="id" data-adescription="email"
+        data-params="/api/users/search">
+        <input {{ $userId ? "data-value=$userId" : '' }} name="id_user" required value="{{ $names }}"
+            data-strategy="dataset" class="autocomplete-input" placeholder="Buscar usuario" aria-label="Search user">
+        <ul class="autocomplete-result-list" class="bg-white"></ul>
     </div>
+    <div class="flex items-center gap-1">
+        <input value="{{ $username }}" required type="text" name="username" placeholder="Nombre de usuario"
+            class="w-full">
+        <select style="width: 200px" required name="domain">
+            @foreach ($domains as $domain)
+                <option {{ $userDomain === $domain->domain ? 'selected' : '' }} value="{{ $domain->domain }}">
+                    {{ '@' . $domain->domain }}</option>
+            @endforeach
+        </select>
+    </div>
+    <textarea name="description" required class="min-h-32" placeholder="Descripción">{{ $description }}</textarea>
+    <div>
+        <p class="text-xs font-semibold opacity-50 p-2">Accesos</p>
+        <div class="grid gap-2 grid-cols-2">
+            @foreach ($access as $item)
+                <label class="inline-flex items-center cursor-pointer">
+                    <input {{ $emailAccess && in_array($item['value'], $emailAccess) ? 'checked' : '' }}
+                        data-notoutline-styles type="checkbox" name="access[]" value="{{ $item['value'] }}"
+                        class="sr-only peer">
+                    <div
+                        class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600">
+                    </div>
+                    <span class="ms-3 font-medium text-gray-900">{{ $item['name'] }}</span>
+                </label>
+            @endforeach
+        </div>
+
+    </div> --}}
 </div>
