c16ae165340900738ba4059396e5be08bd48e394
Daustinn <daustinn.music@gmail.com>
2024-07-20 00:31:35 -0500

    ```text refactor: Update AssistController to paginate assists from multiple connections
    This commit updates the AssistController.php file to paginate assists from multiple connections. Instead of retrieving assists from a single connection, the code now retrieves assists from multiple connections specified in the `$connections` array. This change allows for a more flexible and scalable approach to retrieving assists and improves the performance of the application.

--- app/Http/Controllers/AssistController.php
+++ app/Http/Controllers/AssistController.php
@@ -4,14 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\Attendance;
 use App\Models\User;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class AssistController extends Controller
 {
     public function index()
     {
-        $assists = Attendance::orderBy('punch_time', 'desc')->paginate();
-        return view('pages.assists.index', compact('assists'))
-            ->with('i', (request()->input('page', 1) - 1) * $assists->perPage());
+
+        $connections = [
+            // 'pl-alameda',
+            // 'pl-andahuaylas',
+            // 'pl-casuarina',
+            // 'pl-cybernet',
+            // 'pl-jazmines',
+            // 'rh-alameda',
+            // 'rh-andahuaylas',
+            // 'rh-casuarina',
+            // 'rh-jazmines',
+        ];
+
+        $allAssists = collect();
+
+        foreach ($connections as $connection) {
+            $assists = (new Attendance())
+                ->setConnection($connection)
+                ->orderBy('punch_time', 'desc')
+                ->where('emp_code', '48182459')
+                ->get();
+            $allAssists = $allAssists->merge($assists);
+        }
+
+        $perPage = 20;
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $currentPageItems = $allAssists->slice(($currentPage - 1) * $perPage, $perPage)->all();
+        $assists = new LengthAwarePaginator($currentPageItems, $allAssists->count(), $perPage);
+        $assists->setPath(request()->url());
+
+        return view('modules.assists.+page', compact('assists'))
+            ->with('i', (request()->input('page', 1) - 1) * $perPage);
     }
 
     public function user($id_user)
