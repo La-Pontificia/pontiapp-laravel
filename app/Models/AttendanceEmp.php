73301faa8432d22d600ab8181d62aeb39d4849d5
Daustinn <daustinn.music@gmail.com>
2024-07-20 00:31:56 -0500

    feat: Add AttendanceEmp model for employee attendance records
    This commit adds the AttendanceEmp model, which represents the employee attendance records. The model is associated with the 'personnel_employee' table and includes the 'id', 'first_name', 'last_name', and 'department_id' fields. This addition allows for better organization and management of employee attendance data.

---
+++ app/Models/AttendanceEmp.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AttendanceEmp extends Model
+{
+    use HasFactory;
+
+    protected $table = 'personnel_employee';
+
+    protected $perPage = 20;
+
+    protected $fillable = [
+        'id',
+        'first_name',
+        'last_name',
+        'department_id',
+    ];
+
+    public function department()
+    {
+        return $this->belongsTo(AttendanceDept::class, 'department_id', 'id');
+    }
+}
