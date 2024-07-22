bc005fa729e953c08029358d45c0512c67812897
Daustinn <daustinn.music@gmail.com>
2024-07-22 16:18:59 -0500

    refactor: Add MaintenanceController to handle maintenance module functionality

---
+++ app/Http/Controllers/MaintenanceController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Area;
+use Illuminate\Http\Request;
+
+class MaintenanceController extends Controller
+{
+    public function index(Request $request)
+    {
+        $match = Area::orderBy('created_at', 'desc');
+        $query = $request->get('q');
+
+        if ($query) {
+            $match->where('code', 'like', '%' . $query . '%')
+                ->orWhere('name', 'like', '%' . $query . '%')
+                ->get();
+        }
+
+        $areas = $match->paginate();
+
+        return view('modules.maintenance.+page', [
+            'areas' => $areas
+        ])
+            ->with('i', (request()->input('page', 1) - 1) * $areas->perPage());
+    }
+}
