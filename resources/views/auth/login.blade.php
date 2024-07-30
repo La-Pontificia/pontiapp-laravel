899ed79568ecaeef14c654182e63e2f758372fd8
Daustinn <daustinn.music@gmail.com>
2024-07-30 12:59:03 -0500

    refactor: Update routes, views, and templates for YearController, ScheduleController, and schedules module

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,82 +4,79 @@
 
 @section('content')
     <section class="">
-        <div class="min-h-svh max-sm:w-full text-gray-900 flex max-sm:justify-start">
+        <div class="min-h-svh max-sm:w-full text-white flex max-sm:justify-start">
             <div class="flex h-full w-full">
-                <div style="background-image: url(/background.jpg)" class="w-full min-h-svh bg-center max-lg:hidden bg-cover">
-                </div>
-                <div
-                    class="flex flex-col flex-grow max-sm:w-full gap-4 relative sm:rounded-lg items-center max-sm:justify-start max-sm:items-start">
-                    <div class="flex-grow flex flex-col justify-center">
-                        <div class="p-10 relative z-10 w-[500px] max-sm:w-full max-w-full  ">
-                            <div class="w-full">
-                                <h1 class="text-2xl max-w-[20ch] mx-auto pb-3 text-center font-bold tracking-tighter">
-                                    Centro de Administración La Pontificia
-                                </h1>
-                                <p class="pb-3 text-center">
-                                    Ingresa tus credenciales para continuar.
-                                </p>
-                                <div class="w-full">
-                                    <form method="POST" action="/api/login" class="grid gap-5">
-                                        @csrf
-                                        <div class="space-y-3 font-semibold">
-                                            <input style="padding: 17px" required autofocus name="email" type="email"
-                                                value="{{ old('email') }}"
-                                                placeholder="Correo electrónico o nombre de usuario" />
-                                            <input type="password" style="padding: 17px" required name="password"
-                                                value="{{ old('password') }}" placeholder="Contraseña" />
-                                        </div>
-                                        @error('email')
-                                            <span class="text-sm px-2 text-red-500" role="alert">
-                                                Credenciales incorrectas. Intente de nuevo.
-                                            </span>
-                                        @enderror
+                <div style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.70), rgba(0, 0, 0, 0.70)), url('/background.jpg');"
+                    class="w-full min-h-svh bg-center max-lg:hidden bg-cover grid place-content-center">
+                    <div
+                        class="flex flex-col flex-grow justify-center gap-4 relative sm:rounded-lg items-center max-sm:justify-start max-sm:items-start">
+                        <div class="flex-grow  flex flex-col justify-center">
+                            <div class="p-10 relative z-10 w-[450px] max-sm:w-full max-w-full">
+                                <div class="w-full ">
+                                    {{-- <h1 class="text-2xl max-w-[20ch] mx-auto pb-3 text-center font-bold tracking-tighter">
+                                        Centro de Administración La Pontificia
+                                    </h1> --}}
+                                    <p class="pb-3 text-center">
+                                        Ingresa tus credenciales para continuar.
+                                    </p>
+                                    <div class="w-full">
+                                        <form method="POST" action="/api/login" class="grid gap-5">
+                                            @csrf
+                                            <div class="space-y-3 font-semibold">
+                                                <input style="padding: 17px; border-radius: 10px" required autofocus
+                                                    name="email" type="email" value="{{ old('email') }}"
+                                                    class="h-12 font-normal" placeholder="Correo electrónico" />
+                                                <input type="password" style="padding: 17px; border-radius: 10px" required
+                                                    name="password" value="{{ old('password') }}" class="h-12 font-normal"
+                                                    placeholder="Contraseña" />
+                                            </div>
+                                            @error('email')
+                                                <span class="text-sm px-2 text-red-500" role="alert">
+                                                    Credenciales incorrectas. Intente de nuevo.
+                                                </span>
+                                            @enderror
 
-                                        <button type="submit"
-                                            class="p-5 rounded-xl font-semibold bg-black text-white w-full hover:text-neutral-300">
-                                            <span class="ml-3">
-                                                Entrar
-                                            </span>
-                                        </button>
-                                    </form>
-                                    <div class="flex items-center gap-2 mt-4 text-sm">
-                                        <span class="w-full border-b block"></span>
-                                        <p class="text-nowrap">o continuar con</p>
-                                        <span class="w-full border-b block"></span>
-                                    </div>
-                                    <a title="Aun no disponible" href="{{ route('login.azure') }}"
-                                        class="w-full shadow-md hover:shadow-lg mt-4 gap-2 rounded-xl justify-center text-gray-900 bg-white hover:border-blue-600 text-sm p-5 text-center inline-flex items-center">
-                                        <img src="/RE1Mu3b.png" class="w-24" alt="">
-                                    </a>
-                                    @if (session('error'))
-                                        <div class="text-base max-w-max p-2 text-red-600">
-                                            {{ session('error') }}
+                                            <button type="submit"
+                                                class="p-4 rounded-xl font-semibold bg-black text-white w-full hover:text-neutral-300">
+                                                <span class="ml-3">
+                                                    Entrar
+                                                </span>
+                                            </button>
+                                        </form>
+                                        <div class="flex items-center gap-2 mt-4 text-sm">
+                                            <span class="w-full border-b block"></span>
+                                            <p class="text-nowrap">o continuar con</p>
+                                            <span class="w-full border-b block"></span>
                                         </div>
-                                    @endif
+                                        <a href="{{ route('login.azure') }}"
+                                            class="w-full shadow-md hover:shadow-lg mt-4 gap-2 rounded-xl justify-center text-gray-900 bg-white hover:border-blue-600 text-sm p-4 text-center inline-flex items-center">
+                                            <img src="/RE1Mu3b.png" class="w-24" alt="">
+                                        </a>
+                                        @if (session('error'))
+                                            <div class="text-base max-w-max p-2 text-red-600">
+                                                {{ session('error') }}
+                                            </div>
+                                        @endif
+                                    </div>
+                                    <p class="mt-6 text-xs text-gray-100 text-center">
+                                        @2024 <a href="https://lp.com.pe" target="_blank"
+                                            class="border-b border-gray-500 border-dotted">
+                                            Grupo La Pontificia.
+                                        </a> All rights reserved.
+                                    </p>
                                 </div>
-                                <p class="mt-6 text-xs text-gray-600 text-center">
-                                    @2024 <a href="https://elp.edu.pe" target="_blank"
-                                        class="border-b border-gray-500 border-dotted">
-                                        Grupo La Pontificia.
-                                    </a> All rights reserved.
-                                </p>
-                                {{-- <p class="opacity-60 text-center text-xs mt-2">Developed by <a href="https://daustinn.com"
-                                        class="hover:underline" target="_blank">Daustinn</a>
-                                    & <a href="https://www.instagram.com/diana_margoth000" class="hover:underline"
-                                        target="_blank">Diana</a>
-                                </p> --}}
+                                <div class="flex items-center mt-6 justify-center w-full  gap-5">
+                                    <img src="/elp.webp" alt="Logo - Escuela Superior La Pontificia" class="h-6" />
+                                    <img src="/ilp.webp" alt="Logo - Instituto La Pontificia" class="h-6" />
+                                    <img src="/ec.webp" alt="Logo - Educación Continua La Pontificia" class="h-6 invert" />
+                                </div>
                             </div>
                         </div>
-                        <div class="flex items-center justify-center w-full max-md:p-5 p-10 gap-5">
-                            <img src="/elp.webp" alt="Logo - Escuela Superior La Pontificia" class="h-6" />
-                            <img src="/ilp.webp" alt="Logo - Instituto La Pontificia" class="h-6" />
-                            <img src="/ec.webp" alt="Logo - Educación Continua La Pontificia" class="h-6 invert" />
-                        </div>
+                        <footer class="p-4 text-center text-xs text-white drop-shadow-md">
+                            Developed by <a href="https://daustinn.com" target="_blank" rel="noopener noreferrer"
+                                class=" font-semibold hover:underline">Daustinn</a> &copy; 2024
+                        </footer>
                     </div>
-                    <footer class="p-4 text-center text-xs text-stone-600">
-                        Developed by <a href="https://daustinn.com" target="_blank" rel="noopener noreferrer"
-                            class=" font-semibold hover:underline">Daustinn</a> &copy; 2024
-                    </footer>
                 </div>
             </div>
             <div class="fixed max-sm:hidden pointer-events-none inset-0 opacity-60">
