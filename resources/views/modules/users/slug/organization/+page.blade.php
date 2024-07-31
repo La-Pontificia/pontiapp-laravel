c71edb2943c17104cae1efa46316fdb085e1e0f0
Daustinn <daustinn.music@gmail.com>
2024-07-31 17:24:38 -0500

    refactor: Update organization page layout to use full height flex container

--- resources/views/modules/users/slug/organization/+page.blade.php
+++ resources/views/modules/users/slug/organization/+page.blade.php
@@ -4,7 +4,7 @@
 
 
 @section('layout.users.slug')
-    <div class="p-20 space-y-4 flex flex-col items-center w-full">
+    <div class="p-20 space-y-4 flex flex-col h-full items-center w-full">
         <h1 class="font-serif text-2xl tracking-tight">
             Modulo de organización
         </h1>
