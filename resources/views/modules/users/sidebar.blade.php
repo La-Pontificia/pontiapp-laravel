cb72a6b4abc9cbccf7beabb43ad48c79ff061bc6
Daustinn <daustinn.music@gmail.com>
2024-07-30 19:11:59 -0500

    Update sidebar translation for "Emails" to "Correos y accesos"

--- resources/views/modules/users/sidebar.blade.php
+++ resources/views/modules/users/sidebar.blade.php
@@ -59,7 +59,7 @@
                 <circle cx="12" cy="12" r="4" />
                 <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-4 8" />
             </svg>
-            <p>Emails</p>
+            <p>Correos y accesos</p>
         </a>
         {{-- <a href="{{ route('users.domains') }}" {{ request()->is('users/domains*') ? 'data-active' : '' }}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
