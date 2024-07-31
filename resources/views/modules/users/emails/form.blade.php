4a9275528da26147f1956aedc30abe0e3fb03c17
Daustinn <daustinn.music@gmail.com>
2024-07-30 19:11:39 -0500

    refactor: Remove commented out code and optimize email form view

--- resources/views/modules/users/emails/form.blade.php
+++ resources/views/modules/users/emails/form.blade.php
@@ -1,44 +1,59 @@
 @php
-    $username = $email ? explode('@', $email->email)[0] : null;
-    $userDomain = $email ? explode('@', $email->email)[1] : null;
-    $names = $email ? $email->user->first_name . ' ' . $email->user->last_name : null;
-    $userId = $email ? $email->user->id : null;
-    $description = $email ? $email->description : null;
-    $emailAccess = $email ? json_decode($email->access, true) : null;
-@endphp
-<div class="grid gap-2">
-    <div class="autocompletes" data-atitle="full_name" data-value="id" data-adescription="email"
-        data-params="/api/users/search">
-        <input {{ $userId ? "data-value=$userId" : '' }} name="id_user" required value="{{ $names }}"
-            data-strategy="dataset" class="autocomplete-input" placeholder="Buscar usuario" aria-label="Search user">
-        <ul class="autocomplete-result-list" class="bg-white"></ul>
-    </div>
-    <div class="flex items-center gap-1">
-        <input value="{{ $username }}" required type="text" name="username" placeholder="Nombre de usuario"
-            class="w-full">
-        <select style="width: 200px" required name="domain">
-            @foreach ($domains as $domain)
-                <option {{ $userDomain === $domain->domain ? 'selected' : '' }} value="{{ $domain->domain }}">
-                    {{ '@' . $domain->domain }}</option>
-            @endforeach
-        </select>
-    </div>
-    <textarea name="description" required class="min-h-32" placeholder="Descripción">{{ $description }}</textarea>
-    <div>
-        <p class="text-xs font-semibold opacity-50 p-2">Accesos</p>
-        <div class="grid gap-2 grid-cols-2">
-            @foreach ($access as $item)
-                <label class="inline-flex items-center cursor-pointer">
-                    <input {{ $emailAccess && in_array($item['value'], $emailAccess) ? 'checked' : '' }}
-                        data-notoutline-styles type="checkbox" name="access[]" value="{{ $item['value'] }}"
-                        class="sr-only peer">
-                    <div
-                        class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600">
-                    </div>
-                    <span class="ms-3 font-medium text-gray-900">{{ $item['name'] }}</span>
-                </label>
-            @endforeach
-        </div>
+    // $username = $email ? explode('@', $email->email)[0] : null;
+    // $userDomain = $email ? explode('@', $email->email)[1] : null;
+    // $names = $email ? $email->user->first_name . ' ' . $email->user->last_name : null;
+    // $userId = $email ? $email->user->id : null;
+    // $description = $email ? $email->description : null;
+    // $emailAccess = $email ? json_decode($email->access, true) : null;
 
+    $username = isset($user) ? $user->username : null;
+    $userAccess = isset($user) ? json_decode($user->email_access, true) : null;
+
+@endphp
+<div class="gap-2 flex flex-col overflow-y-auto">
+    <div class="p-2">
+        <input type="text" value="{{ $username }}" required name="username" placeholder="Nombre de usuario">
+    </div>
+    <div class="border-t p-3 overflow-y-auto flex flex-col gap-2">
+        @foreach ($business_unit as $business)
+            <div class="rounded-lg">
+                <div class="flex items-center gap-2 access-button">
+                    <button type="button" class="flex items-center gap-2 toggle-access">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="17" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="lucide lucide-building-2">
+                            <path d="M6 22V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v18Z" />
+                            <path d="M6 12H4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h2" />
+                            <path d="M18 9h2a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2h-2" />
+                            <path d="M10 6h4" />
+                            <path d="M10 10h4" />
+                            <path d="M10 14h4" />
+                            <path d="M10 18h4" />
+                        </svg>
+                        <span>{{ $business['long_name'] }}</span>
+                        <span class="opacity-50">{{ '@' . $business['domain'] }}</span>
+                    </button>
+                </div>
+                <div class="mt-2 pl-10 gap-3 access-list">
+                    @php
+                        $new_access = array_filter($email_access, function ($access) use ($business) {
+                            return in_array($access['code'], $business['services_key']);
+                        });
+                    @endphp
+                    @foreach ($new_access as $access)
+                        @php
+                            $value = $business['code'] . ':' . $access['code'];
+                        @endphp
+                        <label class="flex items-center gap-2 label-child">
+                            <input type="checkbox" name="access[]"
+                                {{ is_array($userAccess) && in_array($value, $userAccess) ? 'checked' : '' }}
+                                value="{{ $value }}" class="rounded-md p-2.5 border-neutral-400 child-checkbox"
+                                data-child-id="{{ $business['code'] }}">
+                            <span>{{ $access['name'] }}</span>
+                        </label>
+                    @endforeach
+                </div>
+            </div>
+        @endforeach
     </div>
 </div>
