96a5b28c22a95af694576a2d46f722b9cc56b4ce
Daustinn <daustinn.music@gmail.com>
2024-08-06 12:14:54 -0500

    refactor: Update UserController to use 'slug_assists' instead of 'slug_attendance'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,24 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Attendance;
 use App\Models\Role;
 use App\Models\JobPosition;
 use App\Models\Branch;
 use App\Models\Department;
 use App\Models\Domain;
-use App\Models\Email;
 use App\Models\GroupSchedule;
 use App\Models\Schedule;
 use App\Models\User;
 use App\Models\UserRole;
+use App\services\AssistsService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
+
+    protected $assistsService;
+
+    public function __construct(AssistsService $assistsService)
+    {
+        $this->assistsService = $assistsService;
+    }
 
     public function index(Request $request)
     {
@@ -118,147 +122,13 @@ class UserController extends Controller
     {
         $user = User::find($id);
         if (!$user) return view('pages.500', ['error' => 'User not found']);
-        $groupSchedules = $user->groupSchedule->schedules;
-        $userSchedules = Schedule::where('user_id', $user->id)->get();
-        $allSchedules = $groupSchedules->merge($userSchedules);
-        $schedulesGenerated = [];
 
-        $currentStartDate = Carbon::now()->startOfMonth()->format('Y-m-d');
-        $currentEndDate = Carbon::now()->endOfMonth()->format('Y-m-d');
-        $startDate = $request->get('start_date', $currentStartDate);
-        $endDate = $request->get('end_date', $currentEndDate);
+        $terminals = $request->get('terminals') ? explode(',', $request->get('terminals')) : ['pl-alameda'];
+        $startDate = $request->get('start_date', Carbon::now()->startOfMonth()->format('Y-m-d'));
+        $endDate = $request->get('end_date', Carbon::now()->endOfMonth()->format('Y-m-d'));
 
-        foreach ($allSchedules as $schedule) {
-            $start = Carbon::parse($schedule->start_date);
-            $end = Carbon::parse($schedule->end_date);
-            for ($date = $start->copy(); $date->lte($end); $date->addDay()) {
-                if (in_array($date->dayOfWeekIso, json_decode($schedule->days))) {
-                    $schedulesGenerated[] = [
-                        'id' => $schedule->id,
-                        'group_id' => $schedule->group_id,
-                        'user_id' => $schedule->user_id,
-                        'title' => $schedule->title,
-                        'date' => $date->format('Y-m-d'),
-                        'from' => Carbon::parse($schedule->from)->setDateFrom($date)->format('Y-m-d H:i:s'),
-                        'from_start' => Carbon::parse($schedule->from_start)->setDateFrom($date)->format('Y-m-d H:i:s'),
-                        'from_end' => Carbon::parse($schedule->from_end)->setDateFrom($date)->format('Y-m-d H:i:s'),
-                        'to' => Carbon::parse($schedule->to)->setDateFrom($date)->format('Y-m-d H:i:s'),
-                        'to_start' => Carbon::parse($schedule->to_start)->setDateFrom($date)->format('Y-m-d H:i:s'),
-                        'to_end' => Carbon::parse($schedule->to_end)->setDateFrom($date)->format('Y-m-d H:i:s'),
-                        'created_at' => $schedule->created_at,
-                        'updated_at' => $schedule->updated_at,
-                    ];
-                }
-            }
-        }
-        $schedules = $schedulesGenerated;
-        usort($schedules, function ($a, $b) {
-            return strcmp($a['date'], $b['date']);
-        });
-
-        $schedules = array_filter($schedules, function ($schedule) use ($startDate, $endDate) {
-            $date = Carbon::parse($schedule['date']);
-            return $date->between(Carbon::parse($startDate), Carbon::parse($endDate));
-        });
-
-        $terminals = [
-            'pl-alameda',
-            'pl-andahuaylas',
-            'pl-casuarina',
-            'pl-cybernet',
-            'pl-jazmines',
-            'rh-alameda',
-            'rh-andahuaylas',
-            'rh-casuarina',
-            'rh-jazmines',
-        ];
-
-        //--------------------------- attendances ---------------------------
-        $queryTerminals = $request->get('terminals') ? explode(',', $request->get('terminals')) : ['pl-alameda'];
-        $queryUsersIds = $request->get('users') ? explode(',', $request->get('users')) : [$user->dni];
-        $finalTerminals = array_filter($terminals, function ($terminal) use ($queryTerminals) {
-            return in_array($terminal, $queryTerminals);
-        });
-
-        $connections = $finalTerminals;
-
-
-        $assistances = collect();
-
-        foreach ($connections as $connection) {
-            $match = (new Attendance())
-                ->setConnection($connection)
-                ->where('emp_code', $user->dni)
-                ->whereRaw("CAST(punch_time AS DATE) >= ?", [$startDate])
-                ->whereRaw("CAST(punch_time AS DATE) <= ?", [$endDate])
-                ->orderBy('punch_time', 'desc')
-                ->get();
-            $assistances = $assistances->merge($match);
-        }
-
-
-        $schedules = array_filter($schedules, function ($schedule) use ($startDate, $endDate) {
-            $date = Carbon::parse($schedule['date']);
-            return $date->between(Carbon::parse($startDate), Carbon::parse($endDate));
-        });
-
-        foreach ($schedules as &$schedule) {
-            $date = Carbon::parse($schedule['date']);
-            $from = Carbon::parse($schedule['from']);
-            $fromStart = Carbon::parse($schedule['from_start']);
-            $fromEnd = Carbon::parse($schedule['from_end']);
-            $to = Carbon::parse($schedule['to']);
-            $toStart = Carbon::parse($schedule['to_start']);
-            $toEnd = Carbon::parse($schedule['to_end']);
-
-            $entry = $assistances->first(function ($assistance) use ($fromStart, $fromEnd) {
-                $time = Carbon::parse($assistance->punch_time);
-                return $time->between($fromStart, $fromEnd);
-            });
-
-            $exit = $assistances->first(function ($assistance) use ($toStart, $toEnd) {
-                $time = Carbon::parse($assistance->punch_time);
-                return $time->between($toStart, $toEnd);
-            });
-
-
-            $schedule['marked_in'] = $entry ? Carbon::parse($entry->punch_time)->format('H:i:s') : null;
-            $schedule['marked_out'] = $exit ? Carbon::parse($exit->punch_time)->format('H:i:s') : null;
-            $schedule['terminal'] = $entry ? $entry->terminal_alias : ($exit ? $exit->terminal_alias : null);
-
-            // Observations & Owes Time
-            $observations = [];
-            $owesTime = null;
-
-            if (!$entry) {
-                $observations[] = 'No marcó entrada';
-            } elseif (Carbon::parse($entry->punch_time)->gt($from)) {
-                $observations[] = 'Tardanza';
-            }
-
-            if (!$exit) {
-                $observations[] = 'No marcó salida';
-            } elseif (Carbon::parse($exit->punch_time)->lt($to)) {
-                $observations[] = 'Salida Temprana';
-            }
-
-            if ($entry && $exit) {
-                if (Carbon::parse($entry->punch_time)->lte($from) && Carbon::parse($exit->punch_time)->gte($to)) {
-                    $observations[] = null;
-                    $owesTime = 0;
-                } else {
-                    if (Carbon::parse($entry->punch_time)->gt($from)) {
-                        $owesTime = Carbon::parse($entry->punch_time)->diffInMinutes($from);
-                    } else $owesTime = 0;
-                    if (Carbon::parse($exit->punch_time)->lt($to)) {
-                        $owesTime += $to->diffInMinutes(Carbon::parse($exit->punch_time));
-                    }
-                }
-            }
-            $schedule['observations'] = implode(', ', $observations);
-            $schedule['owes_time'] = is_numeric($owesTime) ? gmdate('H:i:s', $owesTime * 60) : null;
-        }
-        return view('modules.users.slug.assists.+page', compact('user', 'schedules', 'assistances'));
+        $schedules = $this->assistsService->assistsByUser($user->id, $terminals, $startDate, $endDate);
+        return view('modules.users.slug.assists.+page', compact('user', 'schedules'));
     }
 
     // schedules
