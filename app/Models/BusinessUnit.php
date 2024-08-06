81959500c30352e12051f1dbec16251ffed76c5a
Daustinn <daustinn.music@gmail.com>
2024-08-06 12:14:58 -0500

    refactor: Create BusinessUnit model for managing business units

---
+++ app/Models/BusinessUnit.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BusinessUnit extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'business_units';
+
+    protected $perPage = 20;
+
+    protected $fillable = [
+        'name',
+        'domain',
+        'services',
+        'created_by',
+        'updated_by',
+    ];
+
+    protected $casts = [
+        'services' => 'array',
+    ];
+
+    static $rules = [
+        'name' => 'required',
+        'domain' => 'required',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    public function createdBy()
+    {
+        return $this->hasOne(User::class, 'id', 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->hasOne(User::class, 'id', 'updated_by');
+    }
+}
