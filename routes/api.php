8045ac61224ed73fe967205cad9ff07893efb26e
Daustinn <daustinn.music@gmail.com>
2024-08-06 12:16:57 -0500

    refactor: Remove unused files and optimize code structure

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,29 @@
 <?php
 
+use App\Http\Controllers\Api\EdaController;
+use App\Http\Controllers\Api\EmailController;
+use App\Http\Controllers\Api\EvaluationController;
+use App\Http\Controllers\Api\GoalController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\QuestionController;
+use App\Http\Controllers\Api\RolController;
+use App\Http\Controllers\Api\ScheduleController;
+use App\Http\Controllers\Api\TemplateController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\UserRoleController;
+use App\Http\Controllers\AreaController;
+use App\Http\Controllers\BranchController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\JobPositionController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\YearController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 // -------------------------------- AUTH ROUTES ---------------------------
 
-Route::post('login', 'App\Http\Controllers\Api\LoginController@login');
+Route::post('login', [LoginController::class, 'login']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -15,85 +32,83 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function () {
     // Users routes
-    Route::post('users', 'App\Http\Controllers\Api\UserController@create');
-    Route::post('users/{id}', 'App\Http\Controllers\Api\UserController@update');
-    Route::post('users/{id}/profile', 'App\Http\Controllers\Api\UserController@profile');
-    Route::get('users/search', 'App\Http\Controllers\Api\UserController@search');
-    Route::post('users/email-access/{id}', 'App\Http\Controllers\Api\UserController@updateEmailAccess');
+    Route::post('users', [UserController::class, 'create']);
+    Route::post('users/{id}', [UserController::class, 'update']);
+    Route::post('users/{id}/profile', [UserController::class, 'profile']);
+    Route::get('users/search', [UserController::class, 'search']);
+    Route::post('users/email-access/{id}', [UserController::class, 'updateEmailAccess']);
 
     // User user roles routes 
-    Route::post('user-roles', 'App\Http\Controllers\Api\UserRoleController@create');
-    Route::post('user-roles/{id}', 'App\Http\Controllers\Api\UserRoleController@update');
+    Route::post('user-roles', [UserRoleController::class, 'create']);
+    Route::post('user-roles/{id}', [UserRoleController::class, 'update']);
 
     // Cargo routes
-    Route::get('roles/by_job_position/{id}', 'App\Http\Controllers\Api\RolController@by_job_position');
+    Route::get('roles/by_job_position/{id}', [RolController::class, 'by_job_position']);
 
     // Edas routes
-    Route::post('edas', 'App\Http\Controllers\Api\EdaController@create');
-    Route::post('edas/close/{id}', 'App\Http\Controllers\Api\EdaController@close');
+    Route::post('edas', [EdaController::class, 'create']);
+    Route::post('edas/close/{id}', [EdaController::class, 'close']);
 
     // Goals routes
-    Route::post('goals/sent', 'App\Http\Controllers\Api\GoalController@sent');
-    Route::post('goals/approve', 'App\Http\Controllers\Api\GoalController@approve');
-    Route::post('goals/update/{id}', 'App\Http\Controllers\Api\GoalController@update');
-    Route::get('goals/by-eda/{id}', 'App\Http\Controllers\Api\GoalController@byEda');
-    Route::get('goals/by-evaluation/{id}', 'App\Http\Controllers\Api\GoalController@byEvaluation');
+    Route::post('goals/sent', [GoalController::class, 'sent']);
+    Route::post('goals/approve', [GoalController::class, 'approve']);
+    Route::post('goals/update/{id}', [GoalController::class, 'update']);
+    Route::get('goals/by-eda/{id}', [GoalController::class, 'byEda']);
+    Route::get('goals/by-evaluation/{id}', [GoalController::class, 'byEvaluation']);
 
     // evaluations routes
-    Route::post('evaluation/self-qualification', 'App\Http\Controllers\Api\EvaluationController@selfQualification');
-    Route::post('evaluation/average', 'App\Http\Controllers\Api\EvaluationController@average');
-    Route::post('evaluation/close', 'App\Http\Controllers\Api\EvaluationController@close');
+    Route::post('evaluation/self-qualification', [EvaluationController::class, 'selfQualification']);
+    Route::post('evaluation/average', [EvaluationController::class, 'average']);
+    Route::post('evaluation/close', [EvaluationController::class, 'close']);
 
     // Areas routes
-    Route::post('areas', 'App\Http\Controllers\AreaController@store');
-    Route::post('areas/{id}', 'App\Http\Controllers\AreaController@update');
+    Route::post('areas', [AreaController::class, 'store']);
+    Route::post('areas/{id}', [AreaController::class, 'update']);
 
     // Departments routes
-    Route::post('departments', 'App\Http\Controllers\DepartmentController@store');
-    Route::post('departments/{id}', 'App\Http\Controllers\DepartmentController@update');
+    Route::post('departments', [DepartmentController::class, 'store']);
+    Route::post('departments/{id}', [DepartmentController::class, 'update']);
 
     // Job Positions routes
-    Route::post('job-positions', 'App\Http\Controllers\JobPositionController@store');
-    Route::post('job-positions/{id}', 'App\Http\Controllers\JobPositionController@update');
+    Route::post('job-positions', [JobPositionController::class, 'store']);
+    Route::post('job-positions/{id}', [JobPositionController::class, 'update']);
 
     // Roles routes
-    Route::post('roles', 'App\Http\Controllers\RoleController@store');
-    Route::post('roles/{id}', 'App\Http\Controllers\RoleController@update');
+    Route::post('roles', [RoleController::class, 'store']);
+    Route::post('roles/{id}', [RoleController::class, 'update']);
 
     // Branches routes
-    Route::post('branches', 'App\Http\Controllers\BranchController@store');
-    Route::post('branches/{id}', 'App\Http\Controllers\BranchController@update');
+    Route::post('branches', [BranchController::class, 'store']);
+    Route::post('branches/{id}', [BranchController::class, 'update']);
 
     // Template routes
-    Route::post('templates', 'App\Http\Controllers\Api\TemplateController@create');
-    Route::post('templates/{id}', 'App\Http\Controllers\Api\TemplateController@update');
-    Route::post('templates/{id}/change-in-use', 'App\Http\Controllers\Api\TemplateController@changeInUse');
+    Route::post('templates', [TemplateController::class, 'create']);
+    Route::post('templates/{id}', [TemplateController::class, 'update']);
+    Route::post('templates/{id}/change-in-use', [TemplateController::class, 'changeInUse']);
 
     // Questions routes
-    Route::get('questions/by-template/{id}', 'App\Http\Controllers\Api\QuestionController@by_template');
+    Route::get('questions/by-template/{id}', [QuestionController::class, 'by_template']);
 
     // years routes
-    Route::post('years', 'App\Http\Controllers\YearController@create');
-    Route::post('years/{id}', 'App\Http\Controllers\YearController@update');
-    Route::post('years/open/{id}', 'App\Http\Controllers\YearController@open');
-    Route::post('years/close/{id}', 'App\Http\Controllers\YearController@close');
-
-
+    Route::post('years', [YearController::class, 'create']);
+    Route::post('years/{id}', [YearController::class, 'update']);
+    Route::post('years/open/{id}', [YearController::class, 'open']);
+    Route::post('years/close/{id}', [YearController::class, 'close']);
 
     // Emails routes
-    Route::post('emails', 'App\Http\Controllers\Api\EmailController@assign');
-    Route::post('emails/{id}', 'App\Http\Controllers\Api\EmailController@update');
-    Route::post('emails/discharge/{id}', 'App\Http\Controllers\Api\EmailController@discharge');
+    Route::post('emails', [EmailController::class, 'assign']);
+    Route::post('emails/{id}', [EmailController::class, 'update']);
+    Route::post('emails/discharge/{id}', [EmailController::class, 'discharge']);
 
     // Schedules routes
-    Route::post('schedules/delete/{id}', 'App\Http\Controllers\Api\ScheduleController@remove');
-    Route::post('schedules/archive/{id}', 'App\Http\Controllers\Api\ScheduleController@archive');
+    Route::post('schedules/delete/{id}', [ScheduleController::class, 'remove']);
+    Route::post('schedules/archive/{id}', [ScheduleController::class, 'archive']);
 
-    Route::post('schedules/group', 'App\Http\Controllers\Api\ScheduleController@group');
-    Route::post('schedules/group/{id}', 'App\Http\Controllers\Api\ScheduleController@update');
-    Route::post('schedules/group/{id}/add', 'App\Http\Controllers\Api\ScheduleController@add');
-    Route::post('schedules/{id}', 'App\Http\Controllers\Api\ScheduleController@updateSchedule');
+    Route::post('schedules/group', [ScheduleController::class, 'group']);
+    Route::post('schedules/group/{id}', [ScheduleController::class, 'update']);
+    Route::post('schedules/group/{id}/add', [ScheduleController::class, 'add']);
+    Route::post('schedules/{id}', [ScheduleController::class, 'updateSchedule']);
 
-    Route::get('schedules/user/{id}', 'App\Http\Controllers\Api\ScheduleController@by_user');
-    Route::get('schedules/group/{id}', 'App\Http\Controllers\Api\ScheduleController@schedules');
+    Route::get('schedules/user/{id}', [ScheduleController::class, 'by_user']);
+    Route::get('schedules/group/{id}', [ScheduleController::class, 'schedules']);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,30 @@
 <?php
 
+use App\Http\Controllers\AreaController;
+use App\Http\Controllers\AssistsController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserRoleController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\BranchController;
+use App\Http\Controllers\BusinessUnitController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EdaController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\JobPositionController;
+use App\Http\Controllers\ModuleController;
+use App\Http\Controllers\QuestionnaireTemplateController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\SurveyController;
+use App\Http\Controllers\YearController;
 
 // -------------------------------- AUTH ROUTES ---------------------------
 
-Route::get('login/azure', 'App\Http\Controllers\Auth\LoginController@redirectToAzure')->name('login.azure');
-Route::get('login/azure/callback', 'App\Http\Controllers\Auth\LoginController@handleAzureCallback');
+Route::get('login/azure', [LoginController::class, 'redirectToAzure']);
+Route::get('login/azure/callback', [LoginController::class, 'handleAzureCallback']);
 Auth::routes();
 
 // -------------------------------- DASHBOARD ROUTES ---------------------------
@@ -15,87 +32,66 @@ Auth::routes();
 Route::group(['middleware' => 'authMiddleware'], function () {
 
     // Home routes
-    Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
-    Route::get('/home', 'App\Http\Controllers\HomeController@index');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/home', [HomeController::class, 'index']);
 
     // Module routes
-    Route::get('/', 'App\Http\Controllers\ModuleController@index')->name('modules');
+    Route::get('/', [ModuleController::class, 'index']);
 
     // User roles routes
-    Route::get('users/user-roles', 'App\Http\Controllers\UserRoleController@index')->name('users.user-roles');
-    Route::get('users/user-roles/create', 'App\Http\Controllers\UserRoleController@create')->name('users.user-roles.create');
-    Route::get('users/user-roles/{id}', 'App\Http\Controllers\UserRoleController@slug')->name('users.user-roles.slug');
+    Route::get('users/user-roles', [UserRoleController::class, 'index']);
+    Route::get('users/user-roles/create', [UserRoleController::class, 'create']);
+    Route::get('users/user-roles/{id}', [UserRoleController::class, 'slug']);
 
     // Users routes
-    Route::get('users', 'App\Http\Controllers\UserController@index')->name('users');
+    Route::get('users', [UserController::class, 'index']);
 
-    Route::get('users/schedules', 'App\Http\Controllers\ScheduleController@index')->name('users.schedules');
-    Route::get('users/schedules/create', 'App\Http\Controllers\ScheduleController@create')->name('users.schedules.create');
-    Route::get('users/schedules/{id}', 'App\Http\Controllers\ScheduleController@schedule')->name('users.schedules.schedule');
+    Route::get('users/schedules', [ScheduleController::class, 'index']);
+    Route::get('users/schedules/create', [ScheduleController::class, 'create']);
+    Route::get('users/schedules/{id}', [ScheduleController::class, 'schedule']);
 
-    Route::get('users/emails', 'App\Http\Controllers\UserController@emails')->name('users.emails');
+    Route::get('users/emails', [UserController::class, 'emails']);
 
-    Route::get('users/domains', 'App\Http\Controllers\UserController@domains')->name('users.domains');
+    Route::get('users/domains', [UserController::class, 'domains']);
 
-    Route::get('users/sessions', 'App\Http\Controllers\UserController@sessions')->name('users.sessions');
+    Route::get('users/job-positions', [UserController::class, 'jobPositions']);
 
-    Route::get('users/job-positions', 'App\Http\Controllers\UserController@jobPositions')->name('users.job-positions');
+    Route::get('users/roles', [UserController::class, 'roles']);
 
-    Route::get('users/roles', 'App\Http\Controllers\UserController@roles')->name('users.job-positions');
+    Route::get('users/create', [UserController::class, 'create']);
+    Route::get('users/edit/{id}', [UserController::class, 'edit']);
 
-    Route::get('users/create', 'App\Http\Controllers\UserController@create')->name('users.create');
-    Route::get('users/edit/{id}', 'App\Http\Controllers\UserController@edit')->name('users.edit');
-
-    Route::get('users/{id}', 'App\Http\Controllers\UserController@slug')->name('users.slug');
-    Route::get('users/{id}/organization', 'App\Http\Controllers\UserController@slug_organization')->name('users.slug.organization');
-    Route::get('users/{id}/schedules', 'App\Http\Controllers\UserController@slug_schedules')->name('users.slug.schedules');
-    Route::get('users/{id}/attendance', 'App\Http\Controllers\UserController@slug_attendance')->name('users.slug.attendance');
+    Route::get('users/{id}', [UserController::class, 'slug']);
+    Route::get('users/{id}/organization', [UserController::class, 'slug_organization']);
+    Route::get('users/{id}/schedules', [UserController::class, 'slug_schedules']);
+    Route::get('users/{id}/assists', [UserController::class, 'slug_assists']);
 
 
     // Edas routes
-    Route::get('edas', 'App\Http\Controllers\EdaController@index');
-    Route::get('edas/years', 'App\Http\Controllers\YearController@index');
-    Route::get('edas/questionnaires-templates', 'App\Http\Controllers\QuestionnaireTemplateController@index');
-    Route::get('edas/questionnaires-templates/create', 'App\Http\Controllers\QuestionnaireTemplateController@create');
-    Route::get('edas/questionnaires-templates/{id}/questions', 'App\Http\Controllers\QuestionnaireTemplateController@questions');
+    Route::get('edas', [EdaController::class, 'index']);
+    Route::get('edas/years', [YearController::class, 'index']);
+    Route::get('edas/questionnaires-templates', [QuestionnaireTemplateController::class, 'index']);
+    Route::get('edas/questionnaires-templates/create', [QuestionnaireTemplateController::class, 'create']);
+    Route::get('edas/questionnaires-templates/{id}/questions', [QuestionnaireTemplateController::class, 'questions']);
 
-    Route::get('edas/me', 'App\Http\Controllers\EdaController@me');
+    Route::get('edas/me', [EdaController::class, 'me']);
 
-    Route::get('edas/{id_user}/eda', 'App\Http\Controllers\EdaController@user')->name('edas.slug');
-    Route::get('edas/{id_user}/eda/{id_year}', 'App\Http\Controllers\EdaController@year')->name('edas.slug.year');
-    Route::get('edas/{id_user}/eda/{id_year}/goals', 'App\Http\Controllers\EdaController@goals')->name('edas.slug.goals');
-    Route::get('edas/{id_user}/eda/{id_year}/evaluation/{id_evaluation}', 'App\Http\Controllers\EdaController@evaluation')->name('edas.slug.evaluation');
-    Route::get('edas/{id_user}/eda/{id_year}/questionnaires', 'App\Http\Controllers\EdaController@questionnaires')->name('edas.slug.questionnaires');
+    Route::get('edas/{id_user}/eda', [EdaController::class, 'user']);
+    Route::get('edas/{id_user}/eda/{id_year}', [EdaController::class, 'year']);
+    Route::get('edas/{id_user}/eda/{id_year}/goals', [EdaController::class, 'goals']);
+    Route::get('edas/{id_user}/eda/{id_year}/evaluation/{id_evaluation}', [EdaController::class, 'evaluation']);
+    Route::get('edas/{id_user}/eda/{id_year}/questionnaires', [EdaController::class, 'questionnaires']);
 
     // Assists routes
-    Route::get('assists', 'App\Http\Controllers\AssistController@index')->name('assists');
-    Route::get('assists/{id_user}', 'App\Http\Controllers\AssistController@user')->name('assists.user');
-    Route::get('assists/{id_user}/schedules', 'App\Http\Controllers\AssistController@user')->name('assists.user.schedules');
-    Route::get('assists/schedules', 'App\Http\Controllers\AssistController@schedules')->name('assists.schedules');
+    Route::get('assists', [AssistsController::class, 'index']);
 
-    // Audit routes
-    Route::get('audit', 'App\Http\Controllers\AuditController@index')->name('audit');
-
-    // -------- MAINTAINANCE ROUTES ---------------------------
-    Route::get('maintenance', 'App\Http\Controllers\MaintenanceController@index');
-
-    // Areas routes
-    Route::get('areas', 'App\Http\Controllers\AreaController@index')->name('areas');
-
-    // Departments routes
-    Route::get('departments', 'App\Http\Controllers\DepartmentController@index')->name('departments');
-
-    // Job Positions routes
-    Route::get('job-positions', 'App\Http\Controllers\JobPositionController@index')->name('job-positions');
-
-    // Roles routes
-    Route::get('roles', 'App\Http\Controllers\RoleController@index')->name('roles');
-
-    // Branches routes
-    Route::get('branches', 'App\Http\Controllers\BranchController@index')->name('branches');
-
-    // Surveys routes
-    Route::get('surveys', 'App\Http\Controllers\SurveyController@index')->name('surveys');
+    // -------- SETTINGS ROUTES ---------------------------
+    Route::get('settings', [SettingController::class, 'index']);
+    Route::get('settings/departments', [DepartmentController::class, 'index']);
+    Route::get('settings/branches', [BranchController::class, 'index']);
+    Route::get('settings/business-units', [BusinessUnitController::class, 'index']);
+    Route::post('settings/business-units', [BusinessUnitController::class, 'store']);
+    Route::post('settings/business-units/{id}', [BusinessUnitController::class, 'updated']);
 
     // routes if no route is found
     Route::fallback(function () {
