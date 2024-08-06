d674a82a3edbe235f91ff923a4b947976957fdad
Daustinn <daustinn.music@gmail.com>
2024-08-06 12:16:28 -0500

    refactor: Remove unused page.blade.php file for user sessions

--- resources/views/modules/users/sessions/+page.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('modules.users.+layout')
-
-@section('title', 'Horarios')
-
-@section('layout.users')
-    <div class="text-black w-full flex-col flex-grow flex overflow-y-auto">
-        Sesiones
-    </div>
-@endsection
