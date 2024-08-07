6fc880b2cd1050d4ad73d1b2adaf7e6a15fd542e
Daustinn <daustinn.music@gmail.com>
2024-08-07 10:41:09 -0500

    refactor: Optimize code structure and update form action URL in assists page

--- resources/views/modules/header.blade.php
+++ resources/views/modules/header.blade.php
@@ -1,39 +1,22 @@
 <nav class="bg-[#f5f0f0] text-blue-700 h-14 w-full z-30">
     <div class="flex w-full gap-5 items-center h-full px-3">
         <div class="flex-grow flex gap-2 items-center">
-            <button id="toogle-sidebar" class="p-1 rounded-lg">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="lucide lucide-grip">
-                    <circle cx="12" cy="5" r="1" />
-                    <circle cx="19" cy="5" r="1" />
-                    <circle cx="5" cy="5" r="1" />
-                    <circle cx="12" cy="12" r="1" />
-                    <circle cx="19" cy="12" r="1" />
-                    <circle cx="5" cy="12" r="1" />
-                    <circle cx="12" cy="19" r="1" />
-                    <circle cx="19" cy="19" r="1" />
-                    <circle cx="5" cy="19" r="1" />
-                </svg>
-            </button>
             <a href="/"><img src="/lp.webp" class="w-24 invert" /></a>
             <p class="font-semibold text-ellipsis overflow-hidden text-nowrap tracking-tight">
                 Grupo La Pontificia
             </p>
         </div>
-        <button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"
-                class="lucide lucide-settings">
+        {{-- <button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings">
                 <path
                     d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
                 <circle cx="12" cy="12" r="3" />
             </svg>
-        </button>
+        </button> --}}
         <button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"
-                class="lucide lucide-bell">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell">
                 <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
                 <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
             </svg>
