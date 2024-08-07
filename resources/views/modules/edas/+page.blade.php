2cb184ade5b62a6bc0c57d51ce9f8b2a1bb611bc
Daustinn <daustinn.music@gmail.com>
2024-08-07 10:39:44 -0500

    refactor: Update layout.blade.php for improved UI consistency and optimized code structure

--- resources/views/modules/edas/+page.blade.php
+++ resources/views/modules/edas/+page.blade.php
@@ -4,18 +4,53 @@
 
 @section('layout.users')
     <div class="text-black h-full w-full flex-grow flex flex-col overflow-y-auto">
-        @include('modules.edas.nav')
+        <nav class="flex p-1 flex-grow gap-3 items-center">
+            <label class="relative">
+                <div class="absolute z-[1] inset-y-0 flex items-center pl-2 opacity-60">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="lucide lucide-search">
+                        <circle cx="11" cy="11" r="8" />
+                        <path d="m21 21-4.3-4.3" />
+                    </svg>
+                </div>
+                <input value="{{ request()->query('q') }}" type="search" class="dinamic-search" placeholder="Filtrar"
+                    style="padding-left: 40px;">
+            </label>
+            <div>
+                <select class="dinamic-select" name="job_position">
+                    <option value="0">Todos los puestos</option>
+                    @foreach ($job_positions as $job)
+                        <option {{ request()->query('job_position') === $job->id ? 'selected' : '' }}
+                            value="{{ $job->id }}">
+                            {{ $job->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div>
+                <select class="dinamic-select" name="role">
+                    <option value="0">Todos los cargos</option>
+                    @foreach ($roles as $role)
+                        <option {{ request()->query('role') === $role->id ? 'selected' : '' }} value="{{ $role->id }}">
+                            {{ $role->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </nav>
+
         <h2 class="py-3 pb-0 font-semibold tracking-tight text-lg px-2">
-            Lista de usuarios
+            Usuarios bajo tu supervisión
         </h2>
+
         @if ($cuser->hasPrivilege('edas:view'))
             <div class="overflow-auto flex-grow h-full pt-0">
                 <table class="w-full text-left">
                     <thead class="border-b">
                         <tr class="[&>th]:font-medium [&>th]:text-nowrap [&>th]:p-3">
                             <th>Usuario</th>
-                            <th>Bajo supervision de</th>
+                            <th>Bajo supervisor de</th>
                             <th class="text-center">Edas</th>
+                            <th class="text-center">Objetivos</th>
                             <th></th>
                         </tr>
                     </thead>
@@ -54,15 +89,16 @@
                                     </td>
                                     <td>
                                         @if ($user->supervisor_id)
-                                            <div class="flex p-2 items-center gap-3">
+                                            <button
+                                                class="p-2 relative flex text-left items-center gap-1 rounded-lg px-3 bg-white shadow-lg font-semibold text-sm">
                                                 @include('commons.avatar', [
                                                     'src' => $user->supervisor->profile,
-                                                    'className' => 'w-12',
+                                                    'className' => 'w-8',
                                                     'alt' =>
                                                         $user->supervisor->first_name .
                                                         ' ' .
                                                         $user->supervisor->last_name,
-                                                    'altClass' => 'text-lg',
+                                                    'altClass' => 'text-md',
                                                 ])
                                                 <div>
                                                     <p class="font-medium text-nowrap">
@@ -74,85 +110,29 @@
                                                         {{ $user->supervisor->role_position->name }}
                                                     </p>
                                                 </div>
-                                            </div>
-                                        @else
-                                            -
+                                            </button>
                                         @endif
                                     </td>
                                     <td class="text-center">
-                                        <a href="/edas/{{ $user->id }}" class="group-hover:underline text-blue-600">
+                                        <p class="group-hover:underline text-blue-600">
                                             {{ $user->edas->count() }}
-                                        </a>
+                                        </p>
+                                    </td>
+                                    <td class="text-center">
+                                        <p class="group-hover:underline text-blue-600">
+                                            @php
+                                                $totalGoals = $user->edas->sum(function ($eda) {
+                                                    return $eda->goals->count();
+                                                });
+                                            @endphp
+                                            {{ $totalGoals }}
+                                        </p>
                                     </td>
                                     <td>
                                         <p class="opacity-70">
                                             {{ \Carbon\Carbon::parse($user->created_at)->isoFormat('LL') }}
                                         </p>
                                     </td>
-                                    {{-- <td>
-                                        <div class="flex items-center gap-4">
-                                            <a class="absolute inset-0" href="/users/{{ $user->id }}">
-                                            </a>
-                                            @include('commons.avatar', [
-                                                'src' => $user->profile,
-                                                'className' => 'w-12',
-                                                'alt' => $user->first_name . ' ' . $user->last_name,
-                                                'altClass' => 'text-lg',
-                                            ])
-                                            <p class="text-sm font-normal flex-grow text-nowrap">
-                                                <span class="text-base block font-semibold">
-                                                    {{ $user->last_name }},
-                                                    {{ $user->first_name }}</span>
-                                                {{ $user->role_position->name }},
-                                                {{ $user->role_position->department->name }} -
-                                                {{ $user->role_position->department->area->name }}
-                                            </p>
-                                            <div class="relative flex items-center">
-                                                <button id="dropdownDividerButton"
-                                                    data-dropdown-toggle="dropdown-user-{{ $user->id }}"
-                                                    class="group-hover:opacity-100 opacity-0 hover:bg-neutral-200/80 rounded-md p-1">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                        class="lucide lucide-ellipsis">
-                                                        <circle cx="12" cy="12" r="1" />
-                                                        <circle cx="19" cy="12" r="1" />
-                                                        <circle cx="5" cy="12" r="1" />
-                                                    </svg>
-                                                </button>
-                                                <div id="dropdown-user-{{ $user->id }}"
-                                                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-xl p-1 shadow-xl w-60">
-                                                    <button
-                                                        class="p-2 text-left hover:bg-neutral-100 w-full block rounded-md">Restablecer
-                                                        contraseña</button>
-                                                    <a href="/users/{{ $user->id }}/schedules"
-                                                        class="p-2 hover:bg-neutral-100 w-full block rounded-md">Horario
-                                                        de trabajo</a>
-                                                    @if (($user->status && $current_user->hasPrivilege('users:disable')) || (!$user->status && $current_user->hasPrivilege('users:enable')))
-                                                        <a href="#"
-                                                            class="p-2 hover:bg-neutral-100 w-full block rounded-md text-red-500 hover:bg-gray-10">
-                                                            {{ $user->status ? 'Deshabilitar' : 'Habilitar' }}
-                                                        </a>
-                                                    @endif
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="relative">
-                                        <div class="flex items-center gap-2">
-                                            <a href="mailto:{{ $user->email }}"
-                                                title="Enviar correo a {{ $user->email }}"
-                                                class="bg-white flex text-sm items-center gap-1 rounded-xl hover:shadow-lg shadow-md p-3 font-medium w-fit">
-                                                {{ $user->email }}
-                                            </a>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="opacity-70">
-                                            {{ \Carbon\Carbon::parse($user->created_at)->isoFormat('LL') }}
-                                        </p>
-                                    </td> --}}
                                 </tr>
                             @endforeach
                         @endif
