4dbe5149c5afe8e256a4175dc6c3dbd782eb231f
Daustinn <daustinn.music@gmail.com>
2024-08-07 19:58:47 -0500

    refactor: Update button ID for exporting assists on assists page

--- resources/views/modules/users/slug/assists/+page.blade.php
+++ resources/views/modules/users/slug/assists/+page.blade.php
@@ -81,7 +81,7 @@
                     </div>
                 </div>
             </div>
-            <button {{ count($schedules) === 0 ? 'disabled' : '' }} id="export-individuals-attendances"
+            <button {{ count($schedules) === 0 ? 'disabled' : '' }} id="button-export-assists"
                 class="bg-white hover:shadow-md flex items-center rounded-full gap-2 p-2 text-sm font-semibold px-3">
                 <svg width="20" height="20" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"
                     xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000">
