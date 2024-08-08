2cc00ea218f55840a03afeb22d8a0e9084f757a4
Daustinn <daustinn.music@gmail.com>
2024-08-07 19:59:07 -0500

    refactor: Update user roles form layout and styling

--- resources/views/modules/users/user-roles/+page.blade.php
+++ resources/views/modules/users/user-roles/+page.blade.php
@@ -5,24 +5,53 @@
 @section('layout.users')
     <div class="h-full flex flex-col">
         @if ($current_user->hasPrivilege('users:user-roles:create'))
-            <div class="flex">
-                <a href="/users/user-roles/create"
-                    class="bg-blue-700 shadow-md shadow-blue-500/30 font-semibold hover:bg-blue-600 min-w-max flex items-center rounded-full p-2 gap-1 text-white text-sm px-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                        class="lucide lucide-plus">
-                        <path d="M5 12h14" />
-                        <path d="M12 5v14" />
-                    </svg>
-                    <span class="max-lg:hidden">Nuevo rol</span>
-                </a>
+            <button type="button" data-modal-target="dialog" data-modal-toggle="dialog"
+                class="bg-blue-700 w-fit shadow-md shadow-blue-500/30 font-semibold hover:bg-blue-600 min-w-max flex items-center rounded-full p-2 gap-1 text-white text-sm px-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-plus">
+                    <path d="M5 12h14" />
+                    <path d="M12 5v14" />
+                </svg>
+                <span class="max-lg:hidden">Nuevo rol</span>
+            </button>
+            <div id="dialog" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
+                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative w-full flex flex-col overflow-y-auto max-w-lg max-h-full bg-white rounded-2xl shadow">
+                    <div class="flex items-center justify-between p-3 border-b rounded-t">
+                        <h3 class="text-lg font-semibold text-gray-900">
+                            Crear nuevo rol
+                        </h3>
+                        <button type="button"
+                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                            data-modal-hide="dialog">
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                            </svg>
+                        </button>
+                    </div>
+                    @include('components.users.auditory-card')
+                    <form data-redirect='/users/user-roles' action="/api/user-roles" method="POST" id="dialog-form"
+                        class="p-3 dinamic-form grid gap-4 overflow-y-auto">
+                        @include('modules.users.user-roles.form')
+                    </form>
+                    <div class="flex items-center p-3 border-t border-gray-200 rounded-b">
+                        <button form="dialog-form" type="submit"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-sm px-5 py-2.5 text-center">
+                            Guardar</button>
+                        <button id="button-close-scheldule-modal" data-modal-hide="dialog" type="button"
+                            class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-xl border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Cancelar</button>
+                    </div>
+                </div>
             </div>
         @endif
         <h2 class="py-3 font-semibold tracking-tight text-lg px-1">
             Gestión de roles
         </h2>
-        @if ($current_user->hasPrivilege('users:user-roles:view'))
-            <div class="bg-white shadow-sm rounded-2xl">
+        @if ($current_user->hasPrivilege('users:user-roles:show'))
+            <div class="">
                 <table class="w-full text-left" id="table-users">
                     <thead class="border-b">
                         <tr class="[&>th]:font-medium [&>th]:text-nowrap [&>th]:p-1.5 [&>th]:px-2">
@@ -46,30 +75,95 @@
                                     class="[&>td]:py-3 hover:border-transparent hover:[&>td]shadow-md [&>td>p]:text-nowrap relative group first:[&>td]:rounded-l-2xl last:[&>td]:rounded-r-2xl hover:bg-white [&>td]:px-2">
                                     <td>
                                         <div class="flex gap-2 items-center">
-                                            <p class="text-nowrap">
-                                                <a href="/users/user-roles/{{ $role->id }}"
-                                                    class="hover:underline hover:text-blue-600">
-                                                    {{ $role->title }}
-                                                </a>
+                                            <p class="text-nowrap flex items-center gap-2">
+                                                @svg('heroicon-o-folder', 'w-5 h-5')
+                                                {{ $role->title }}
                                             </p>
                                         </div>
-                                    </td>
-                                    <td class="relative">
-                                        <div class="flex items-center gap-2">
-                                            <p>
-                                                <a href="/users/user-roles/{{ $role->id }}"
-                                                    class="hover:underline text-blue-600">
-                                                    {{ count($role->privileges) }} privilegios
-                                                </a>
-                                            </p>
-                                        </div>
+                                        @if ($current_user->hasPrivilege('users:user-roles:edit'))
+                                            <button type="button" data-modal-target="dialog-{{ $role->id }}"
+                                                data-modal-toggle="dialog-{{ $role->id }}" class="absolute inset-0">
+
+                                            </button>
+                                            <div id="dialog-{{ $role->id }}" data-modal-backdrop="static" tabindex="-1"
+                                                aria-hidden="true"
+                                                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                                <div
+                                                    class="relative w-full flex flex-col overflow-y-auto max-w-lg max-h-full bg-white rounded-2xl shadow">
+                                                    <div class="flex items-center justify-between p-3 border-b rounded-t">
+                                                        <h3 class="text-lg font-semibold text-gray-900">
+                                                            Editar rol
+                                                        </h3>
+                                                        <button type="button"
+                                                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                                                            data-modal-hide="dialog-{{ $role->id }}">
+                                                            <svg class="w-3 h-3" aria-hidden="true"
+                                                                xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                viewBox="0 0 14 14">
+                                                                <path stroke="currentColor" stroke-linecap="round"
+                                                                    stroke-linejoin="round" stroke-width="2"
+                                                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                                            </svg>
+                                                        </button>
+                                                    </div>
+                                                    @include('components.users.auditory-card')
+                                                    <form data-redirect='/users/user-roles'
+                                                        action="/api/user-roles/{{ $role->id }}" method="POST"
+                                                        id="dialog-{{ $role->id }}-form"
+                                                        class="p-3 dinamic-form grid gap-4 overflow-y-auto">
+                                                        @include('modules.users.user-roles.form', [
+                                                            'role' => $role,
+                                                        ])
+                                                    </form>
+                                                    <div class="flex items-center p-3 border-t border-gray-200 rounded-b">
+                                                        <button form="dialog-{{ $role->id }}-form" type="submit"
+                                                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-sm px-5 py-2.5 text-center">
+                                                            Guardar</button>
+                                                        <button id="button-close-scheldule-modal"
+                                                            data-modal-hide="dialog-{{ $role->id }}" type="button"
+                                                            class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-xl border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Cancelar</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @endif
                                     </td>
                                     <td>
-                                        <p class="opacity-70">
+                                        <p>
+                                            <span class="hover:underline text-blue-600">
+                                                {{ count($role->privileges) }} privilegios
+                                            </span>
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p class="line-clamp-2 flex items-center gap-2">
+                                            @svg('heroicon-o-clock', 'w-5 h-5')
                                             Registrado el {{ \Carbon\Carbon::parse($role->created_at)->isoFormat('LL') }}
                                             por
                                             {{ $role->createdBy->first_name }} {{ $role->createdBy->last_name }}
                                         </p>
+                                    </td>
+                                    <td>
+                                        <button class="opacity-60 relative hover:opacity-100"
+                                            data-dropdown-toggle="dropdown-{{ $role->id }}">
+                                            <svg width="20" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                class="lucide lucide-ellipsis-vertical">
+                                                <circle cx="12" cy="12" r="1" />
+                                                <circle cx="12" cy="5" r="1" />
+                                                <circle cx="12" cy="19" r="1" />
+                                            </svg>
+                                        </button>
+
+                                        <div id="dropdown-{{ $role->id }}"
+                                            class="z-10 hidden bg-white border divide-y divide-gray-100 rounded-xl p-1 shadow-xl w-60">
+                                            <button data-alertvariant="warning"
+                                                data-atitle="¿Estás seguro de eliminar el rol?"
+                                                data-adescription="No podrás deshacer esta acción."
+                                                data-param="/api/user-roles/delete/{{ $role->id }}"
+                                                class="p-2 dinamic-alert hover:bg-neutral-100 text-left w-full block rounded-md text-red-600 hover:bg-gray-10">
+                                                Eliminar
+                                            </button>
+                                        </div>
                                     </td>
                                 </tr>
                             @endforeach
--- resources/views/modules/users/user-roles/form.blade.php
+++ resources/views/modules/users/user-roles/form.blade.php
@@ -1,72 +1,48 @@
 @php
-    $privileges = $role ? $role->privileges : [];
+    $privileges = isset($role) ? $role->privileges : [];
+    $title = isset($role) ? $role->title : null;
 @endphp
-<label class="w-[300px] block px-1 pb-4">
+<label class="block px-1">
     <span>Título:</span>
-    <input value="{{ $role ? $role->title : '' }}" required type="text" name="title"
-        placeholder="Ejemplo: Administrador" class="font-medium">
+    <input value="{{ $title }}" required type="text" name="title" placeholder="Ejemplo: Administrador">
 </label>
 <div class="px-1 overflow-y-auto">
-    <p class="py-4 pt-1 font-semibold tracking-tight text-sm text-stone-600">Privilegios:</p>
-    <div class="grid gap-5 grid-cols-3 max-xl:grid-cols-2 items-start">
+    <p class="tracking-tight text-stone-600">Privilegios:</p>
+    <div class="grid items-start gap-2">
         @foreach ($system_privileges as $system_privilege)
-            <div class="border border-transparent">
-                <div class="flex mb-4 items-center gap-2">
+            <div class="content">
+                <div class="flex items-center gap-2">
                     <button type="button" class="toggle-group">
-                        <svg xmlns="http://www.w3.org/2000/svg" style="transform: rotate(90deg);" width="24"
-                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
-                            <path d="m9 18 6-6-6-6" />
-                        </svg>
+                        @svg('heroicon-o-chevron-right', 'w-5 h-5')
                     </button>
-                    <label class="flex items-center gap-2 font-semibold">
+                    <label class="flex items-center gap-2 ">
                         <input type="checkbox" class="select-all-group rounded-md p-2.5 border-neutral-400">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="17" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-file">
-                            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
-                            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
-                        </svg>
+                        @svg('heroicon-o-folder', 'w-5 h-5')
                         <span>{{ $system_privilege['name'] }}</span>
                     </label>
                 </div>
-                <div class="group-content grid gap-2 mt-2">
+                <div style="display: none" class="group-content gap-2 mt-2 pl-5">
                     @foreach ($system_privilege['items'] as $item)
-                        <div class="border border-neutral-200 bg-white p-2 rounded-lg">
+                        <div class="content">
                             <div class="flex items-center gap-2">
                                 <button type="button" class="toggle-subgroup">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round"
-                                        class="lucide lucide-chevron-right">
-                                        <path d="m9 18 6-6-6-6" />
-                                    </svg>
+                                    @svg('heroicon-o-chevron-right', 'w-5 h-5')
                                 </button>
-                                <label class="flex items-center gap-2 font-semibold">
+                                <label class="flex items-center gap-2 ">
                                     <input type="checkbox"
                                         class="select-all-subgroup rounded-md p-2.5 border-neutral-400">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="17" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="lucide lucide-file">
-                                        <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
-                                        <path d="M14 2v4a2 2 0 0 0 2 2h4" />
-                                    </svg>
+                                    @svg('heroicon-o-folder', 'w-5 h-5')
                                     <span>{{ $item['name'] }}</span>
                                 </label>
                                 <span class="privilege-count text-sm text-gray-500">(0/0)</span>
                             </div>
-                            <div class="subgroup-content mt-4 grid pl-10 gap-3" style="display: none;">
+                            <div class="subgroup-content mt-2 grid pl-10 gap-3" style="display: none;">
                                 @foreach ($item['privileges'] as $privilege => $privilege_name)
                                     <label class="flex items-center gap-2">
                                         <input {{ in_array($privilege, $privileges) ? 'checked' : '' }} type="checkbox"
                                             name="privileges[]" value="{{ $privilege }}"
                                             class="privilege-checkbox rounded-md p-2.5 border-neutral-400">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="17" viewBox="0 0 24 24"
-                                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                            stroke-linejoin="round" class="lucide lucide-file">
-                                            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
-                                            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
-                                        </svg>
+                                        @svg('heroicon-o-folder', 'w-5 h-5')
                                         <span>{{ $privilege_name }}</span>
                                     </label>
                                 @endforeach
