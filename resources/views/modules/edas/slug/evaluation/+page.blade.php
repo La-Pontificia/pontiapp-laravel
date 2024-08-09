83660208c735a543daee32a1ce9063a48982dd6e
Daustinn <daustinn.music@gmail.com>
2024-08-08 21:55:13 -0500

    refactor: Update EDA page layout, sidebar, privilege checks, and model attributes

--- resources/views/modules/edas/slug/evaluation/+page.blade.php
+++ resources/views/modules/edas/slug/evaluation/+page.blade.php
@@ -2,6 +2,8 @@
 
 @section('title', 'Eda: ' . $current_year->name . ' - ' . $user->first_name . ' ' . $user->last_name)
 
+@section('title_eda', 'Evaluación N°' . $evaluation->number)
+
 @section('layout.edas.slug')
 
     @php
@@ -10,133 +12,183 @@
         $isSupervisor = $current_user->id === $user->supervisor_id;
 
         // if has self qualification
-        $hasSelfQualification =
+        $hasSelfQualify =
             $current_user->hasPrivilege('edas:evaluations:self-qualify') &&
             !$evaluation->closed &&
             !$evaluation->self_qualification;
 
         // if has average evaluation
-        $hasAverage =
+        $hasQualify =
             $current_user->hasPrivilege('edas:evaluations:qualify') &&
             !$evaluation->closed &&
             $isSupervisor &&
             $evaluation->self_qualification &&
-            !$evaluation->average;
+            !$evaluation->qualification;
 
         // if has close evaluation
         $hasCloseEvaluation =
             $current_user->hasPrivilege('edas:evaluations:close') &&
             !$evaluation->closed &&
-            $evaluation->average &&
+            $evaluation->qualification &&
             $evaluation->self_qualification &&
             $isSupervisor;
     @endphp
-    <div class="h-full flex flex-col mt-3 bg-white overflow-auto rounded-xl">
-        <input type="hidden" id="input-hidden-id-evaluation" value="{{ $evaluation->id }}">
+    <div class="h-full flex overflow-hidden flex-col pt-0 overflow-x-auto">
 
-        @if ($hasSelfQualification)
-            <span id="has-self-quaification"></span>
-        @endif
+        @if ($hasSelfQualify || $hasQualify || $hasCloseEvaluation)
+            <div class="flex items-center px-3 gap-2">
+                <div class="flex-grow">
+                </div>
+                <div class="flex gap-2 items-center p-1 tracking-tight">
+                    @if ($hasSelfQualify)
+                        <button id="evaluation-self-qualify-button" data-id="{{ $evaluation->id }}"
+                            class="bg-sky-600 shadow-sm shadow-sky-500/10 hover:bg-sky-700 data-[hidden]:hidden text-white font-semibold justify-center min-w-max flex items-center rounded-full p-1 gap-1 text-sm px-3">
+                            @svg('heroicon-s-check', [
+                                'class' => 'w-5 h-5',
+                            ])
+                            Autocalificar
+                        </button>
+                    @endif
+                    @if ($hasQualify)
+                        <button data-id="{{ $evaluation->id }}" id="evaluation-qualify-button"
+                            class="bg-violet-700 shadow-sm shadow-violet-500/10 data-[hidden]:hidden font-semibold justify-center hover:bg-violet-600 min-w-max flex items-center rounded-full p-1 gap-1 text-white text-sm px-3">
+                            @svg('heroicon-o-clipboard-document-check', [
+                                'class' => 'w-5 h-5',
+                            ])
+                            Calificiar
+                        </button>
+                    @endif
 
-        @if ($hasAverage)
-            <span id="has-average"></span>
-        @endif
-
-        <div class="flex gap-2 p-1 items-center border-b">
-            <div class="flex-grow">
-                <button onclick="window.history.back()"
-                    class="text-blue-700 hover:bg-indigo-100 font-semibold justify-center min-w-max flex items-center rounded-full p-2 gap-1 px-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                        class="lucide lucide-arrow-left">
-                        <path d="m12 19-7-7 7-7" />
-                        <path d="M19 12H5" />
-                    </svg>
-                    <span class="max-lg:hidden">Evaluacion N°{{ $evaluation->number }}</span>
-                </button>
+                    @if ($hasCloseEvaluation)
+                        <button data-id="{{ $evaluation->id }}" id="evaluation-close-button"
+                            class="bg-red-600 shadow-sm shadow-red-500/10 data-[hidden]:hidden font-semibold justify-center hover:bg-red-700 min-w-max flex items-center rounded-full p-1 gap-1 text-white text-sm px-3">
+                            @svg('heroicon-o-x-mark', [
+                                'class' => 'w-5 h-5',
+                            ])
+                            Cerrar
+                        </button>
+                    @endif
+                </div>
             </div>
-            <button {{ !$hasSelfQualification ? 'disabled' : '' }} id="self-qualification-button"
-                class="bg-violet-600 disabled:opacity-30 {{ !$hasSelfQualification ? 'hidden' : '' }} hover:bg-violet-700 text-white font-semibold justify-center min-w-max flex items-center rounded-full p-1.5 gap-1 text-sm px-3">
-                Autocalificar
-            </button>
-            <button {{ !$hasAverage ? 'disabled' : '' }} id="average-button"
-                class="bg-green-600 disabled:opacity-30 {{ !$hasAverage ? 'hidden' : '' }} hover:bg-green-700 text-white font-semibold justify-center min-w-max flex items-center rounded-full p-1.5 gap-1 text-sm px-3">
-                Calificar
-            </button>
-            <button {{ !$hasCloseEvaluation ? 'disabled' : '' }} id="close-button"
-                class="{{ !$hasCloseEvaluation ? 'hidden' : '' }} bg-red-600 disabled:opacity-30 hover:bg-red-700 text-white font-semibold justify-center min-w-max flex items-center rounded-full p-1.5 gap-1 text-sm px-3">
-                Cerrar
-            </button>
-        </div>
-        <div id="panel-goals" class="w-full flex-grow overflow-x-auto">
-            <table class="w-full">
-                <thead class="border-b">
-                    <tr class="[&>th]:p-2 [&>th]:text-nowrap text-left [&>th]:font-semibold text-sm">
-                        <th class="text-center">N°</th>
-                        <th class="min-w-[200px]">Objetivo</th>
+        @endif
+
+        @if ($evaluation->closed)
+            <span class="text-neutral-400 block text-center w-full p-3">Esta evaluación ha sido cerrada
+                el {{ \Carbon\Carbon::parse($evaluation->closed_at)->isoFormat('LL') }} por
+                {{ $evaluation->closedBy->first_name }}
+                {{ $evaluation->closedBy->last_name }}.
+            </span>
+        @endif
+
+        <div class="h-full flex-grow overflow-y-auto w-full">
+            <table class="pt-3 text-sm w-full max-lg:grid-cols-1 px-1 py-1 gap-5">
+                <thead class="border-b border-neutral-300">
+                    <tr class="[&>th]:font-medium [&>th]:px-3 [&>th]:py-2">
+                        <th>N°</th>
+                        <th>Título</th>
                         <th class="min-w-[300px]">Descripción</th>
-                        <th class="min-w-[400px]">Indicadores</th>
-                        <th>Porcentaje</th>
-                        <th>N. A.</th>
-                        <th>Promedio</th>
-                        <th class="w-[50px]"></th>
+                        <th class="min-w-[300px]">Indicadores</th>
+                        <th class="text-center">Porcentaje</th>
+                        <th>Autocalificación</th>
+                        <th>Calificación</th>
                     </tr>
                 </thead>
-                <tbody id="table-goals" class="divide-y text-[15px]">
-
+                <tbody id="evaluations" class="divide-y divide-neutral-300">
+                    @foreach ($goalevaluations as $index => $eva)
+                        <tr data-id="{{ $eva->id }}"
+                            class="[&>td]:py-3 [&>td]:align-top hover:border-transparent hover:[&>td]shadow-md relative group [&>td]:px-2">
+                            <td class="text-center goal-index whitespace-pre-line">
+                                {{ str_pad($index + 1, 2, '0', STR_PAD_LEFT) }}
+                            </td>
+                            <td>
+                                <p class="goal-title whitespace-pre-line">
+                                    {{ $eva->goal->title }}
+                                </p>
+                            </td>
+                            <td>
+                                <p class="goal-description whitespace-pre-line">
+                                    {{ $eva->goal->description }}
+                                </p>
+                            </td>
+                            <td>
+                                <p class="goal-indicators whitespace-pre-line">
+                                    {{ $eva->goal->indicators }}
+                                </p>
+                            </td>
+                            <td>
+                                <p data-percentage="{{ $eva->goal->percentage }}"
+                                    class="text-center goal-percentage percentage whitespace-pre-line">
+                                    {{ $eva->goal->percentage }}%
+                                </p>
+                            </td>
+                            <td>
+                                <div class="flex justify-center whitespace-pre-line">
+                                    @if ($hasSelfQualify)
+                                        <select style="width: 60px" class="self-qualification">
+                                            <option value="" selected> - </option>
+                                            @foreach ([1, 2, 3, 4, 5] as $i)
+                                                <option value="{{ $i }}"
+                                                    {{ $i == $eva->self_qualification ? 'selected' : '' }}>
+                                                    {{ $i }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    @else
+                                        {{ $eva->self_qualification ?? '-' }}
+                                    @endif
+                                </div>
+                            </td>
+                            <td>
+                                <div class="flex justify-center whitespace-pre-line">
+                                    @if ($hasQualify)
+                                        <select style="width: 60px" class="qualification">
+                                            <option value="" selected>-</option>
+                                            @foreach ([1, 2, 3, 4, 5] as $i)
+                                                <option value="{{ $i }}"
+                                                    {{ $i == $eva->qualification ? 'selected' : '' }}>
+                                                    {{ $i }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    @else
+                                        {{ $eva->qualification ?? '-' }}
+                                    @endif
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
                 <tbody>
-                    <tr class="text-sm">
-                        <td colspan="5" class="text-lg px-4 font-semibold tracking-tight">Total</td>
-                        <td class="px-4">
-                            <div
-                                class="p-2 w-fit mx-auto rounded-xl border-2 border-violet-500 px-3 bg-violet-100 font-medium">
-                                <span id="total-self-qualification">0</span>
-                            </div>
+                    <tr class="border-t border-neutral-300">
+                        <td colspan="5" class="px-4 py-2">Totales</td>
+                        <td class="text-center">
+                            <p id="total-self-qualification" class="font-semibold text-blue-600">
+                                {{ $evaluation->self_qualification ?? '-' }}
+                            </p>
                         </td>
-                        <td class="px-4">
-                            <div
-                                class="p-2 w-fit mx-auto rounded-xl border-2 border-green-500 px-3 bg-green-100 font-medium">
-                                <span id="total-average">0</span>
-                            </div>
+                        <td class="text-center">
+                            <p id="total-qualification" class="font-semibold text-blue-600">
+                                {{ $evaluation->qualification ?? '-' }}
+                            </p>
                         </td>
-                        <td></td>
                     </tr>
                 </tbody>
             </table>
         </div>
-        <div class="border-t p-2 text-sm flex flex-col gap-1">
-            @if ($evaluation->self_qualification)
-                <p class="text-neutral-400 text-sm">Autocalificado
-                    el {{ \Carbon\Carbon::parse($evaluation->self_rated_at)->isoFormat('LL') }} por
-                    <a title="Ir al perfil de {{ $evaluation->selfRatedBy->first_name }} {{ $evaluation->selfRatedBy->last_name }}"
-                        href="/profile/{{ $evaluation->selfRatedBy->id }}" class="hover:underline text-blue-600">
-                        {{ $evaluation->selfRatedBy->first_name }}
-                        {{ $evaluation->selfRatedBy->last_name }}.
-                    </a>
-                </p>
+        <p class="p-2 text-xs">
+            @if ($evaluation->selfRatedBy)
+                <span class="text-neutral-400">Autocaficado el
+                    {{ \Carbon\Carbon::parse($evaluation->self_rated_at)->isoFormat('LL') }} por
+                    {{ $evaluation->selfRatedBy->last_name }}, {{ $evaluation->selfRatedBy->first_name }}
+                </span>
             @endif
-
-            @if ($evaluation->average)
-                <p class="text-neutral-400 text-sm">Calificado
-                    el {{ \Carbon\Carbon::parse($evaluation->averaged_at)->isoFormat('LL') }} por
-                    <a title="Ir al perfil de {{ $evaluation->averagedBy->first_name }} {{ $evaluation->averagedBy->last_name }}"
-                        href="/profile/{{ $evaluation->averagedBy->id }}" class="hover:underline text-blue-600">
-                        {{ $evaluation->averagedBy->first_name }}
-                        {{ $evaluation->averagedBy->last_name }}.
-                    </a>
-                </p>
+            @if ($evaluation->qualifiedBy)
+                <span class="text-neutral-400">y Aprobado
+                    el {{ \Carbon\Carbon::parse($evaluation->qualified_at)->isoFormat('LL') }} por
+                    {{ $evaluation->qualifiedBy->first_name }}
+                    {{ $evaluation->qualifiedBy->last_name }}.
+                </span>
             @endif
-            @if ($evaluation->closed)
-                <p class="text-neutral-400 text-sm">Cerrado
-                    el {{ \Carbon\Carbon::parse($evaluation->closed_at)->isoFormat('LL') }} por
-                    <a title="Ir al perfil de {{ $evaluation->closedBy->first_name }} {{ $evaluation->closedBy->last_name }}"
-                        href="/profile/{{ $evaluation->closedBy->id }}" class="hover:underline text-blue-600">
-                        {{ $evaluation->closedBy->first_name }}
-                        {{ $evaluation->closedBy->last_name }}.
-                    </a>
-                </p>
-            @endif
-        </div>
+        </p>
     </div>
 @endsection
