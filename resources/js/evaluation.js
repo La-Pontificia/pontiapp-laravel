a26beb269b1f46f501e2e10979b2d8323ed8793d
Daustinn <daustinn.music@gmail.com>
2024-08-08 21:54:48 -0500

    refactor: Update privilege checks, model attributes, and controller methods

--- resources/js/evaluation.js
+++ resources/js/evaluation.js
@@ -1,295 +1,182 @@
+import axios from "axios";
+
 document.addEventListener("DOMContentLoaded", async () => {
     $ = document.querySelector.bind(document);
-    let goalsEvaluations = [];
-    const inputHiddenIdEvaluation = $("#input-hidden-id-evaluation");
-    const tableGoals = $("#table-goals");
-    const notes = Array.from({ length: 5 }, (_, i) => i + 1);
+    const $table = $("#evaluations");
+    const $selfQualify = $("#evaluation-self-qualify-button");
+    const $qualify = $("#evaluation-qualify-button");
+    const $close = $("#evaluation-close-button");
 
-    const SpanTotalAverage = $("#total-average");
-    const SpanTotalSelfQualification = $("#total-self-qualification");
+    const $totalSelfQualification = $("#total-self-qualification");
+    const $totalQualification = $("#total-qualification");
+    const $rows = Array.from($table?.querySelectorAll("tr") ?? []);
 
-    const selfQualifacationButton = $("#self-qualification-button");
-    const averageButton = $("#average-button");
-    const closeButton = $("#close-button");
+    function getTotalSelfQualification() {
+        const total = $rows.reduce((acc, $row) => {
+            const percentage =
+                $row
+                    .querySelector(".percentage")
+                    ?.getAttribute("data-percentage") || 0;
 
-    const hasAverage = !!$("#has-average");
-    const hasSelfQualification = !!$("#has-self-quaification");
+            const selfQualification =
+                $row.querySelector(".self-qualification")?.value || 0;
 
-    const getTotalAverage = () =>
-        goalsEvaluations.reduce(
-            (acc, item) =>
-                acc +
-                (parseInt(item.goal.average ?? "0") * item.goal.percentage) /
-                    100,
-            0
-        );
-
-    const getTotalSelfQualification = () =>
-        goalsEvaluations.reduce(
-            (acc, item) =>
-                acc +
-                (parseInt(item.goal.self_qualification ?? "0") *
-                    item.goal.percentage) /
-                    100,
-            0
-        );
-
-    if (inputHiddenIdEvaluation) {
-        const res = await axios.get(
-            `/api/goals/by-evaluation/${inputHiddenIdEvaluation.value}`
-        );
-        goalsEvaluations = res.data.map((i) => ({
-            ...i,
-            goal: {
-                ...i.goal,
-                self_qualification: i.self_qualification ?? 1,
-                average: i.average ?? 1,
-            },
-        }));
+            return acc + (Number(selfQualification) * Number(percentage)) / 100;
+        }, 0);
+        return total;
     }
 
-    const verifyGoals = () => {
-        if (!SpanTotalAverage) return;
-        SpanTotalAverage.innerHTML = getTotalAverage().toFixed(2);
-        SpanTotalSelfQualification.innerHTML =
-            getTotalSelfQualification().toFixed(2);
-    };
+    function getTotalQualification() {
+        const total = $rows.reduce((acc, $row) => {
+            const percentage =
+                $row
+                    .querySelector(".percentage")
+                    ?.getAttribute("data-percentage") || 0;
 
-    const renderGoals = () => {
-        if (!tableGoals) return;
+            const qualification =
+                $row.querySelector(".qualification")?.value || 0;
 
-        tableGoals.innerHTML = "";
-        goalsEvaluations.forEach((item, index) => {
-            // Row
-            const row = document.createElement("tr");
-            row.className =
-                "[&>td]:align-top [&>td>div]:p-2 [&>td>div]:min-h-20 [&>td>div]:overflow-y-auto [&>td>div]:max-h-[100px] [&>td>div]:bg-transparent [&>td>div]:rounded-md";
+            return acc + (Number(qualification) * Number(percentage)) / 100;
+        }, 0);
+        return total;
+    }
 
-            // Goal
-            const divGoal = document.createElement("div");
-            divGoal.className = "w-full text-blue-600 font-semibold";
-            divGoal.innerHTML = item.goal.goal;
-
-            // Description
-            const divDescription = document.createElement("div");
-            divDescription.className = "w-full";
-            divDescription.innerHTML = item.goal.description;
-
-            // Indicators
-            const divIndicators = document.createElement("div");
-            divIndicators.className = "w-full";
-            divIndicators.ariaPlaceholder = "Ingrese los indicadores";
-            divIndicators.innerHTML = item.goal.indicators;
-
-            // Percentage
-            const divPercentage = document.createElement("div");
-            divPercentage.className =
-                "border-0 bg-transparent font-semibold px-4 text-center";
-            divPercentage.innerHTML = `${item.goal.percentage}%`;
-
-            // Select N. A.
-            const sNA = document.createElement("select");
-            sNA.className =
-                "border-0 bg-transparent disabled:grayscale disabled:font-normal cursor-pointer text-violet-500 font-bold";
-            sNA.disabled = !hasSelfQualification;
-            notes.forEach((note) => {
-                const option = document.createElement("option");
-                option.selected =
-                    item.goal.self_qualification?.toString() ===
-                    note.toString();
-                option.value = note;
-                option.textContent = `${note}.0`;
-                sNA.appendChild(option);
-            });
-            sNA.onchange = (e) => {
-                goalsEvaluations[index].goal.self_qualification =
-                    e.target.value;
-                verifyGoals();
+    function getItems() {
+        const items = $rows.map(($row) => {
+            const id = $row.getAttribute("data-id");
+            const selfQualification = $row.querySelector(
+                ".self-qualification"
+            )?.value;
+            const qualification = $row.querySelector(".qualification")?.value;
+            return {
+                id,
+                self_qualification: Number(selfQualification),
+                qualification: Number(qualification),
             };
-
-            // Select average.
-            const sAverage = document.createElement("select");
-            sAverage.disabled = !hasAverage;
-            sAverage.className =
-                "border-0 bg-transparent disabled:grayscale disabled:font-normal cursor-pointer text-green-500 font-bold";
-            notes.forEach((note) => {
-                const option = document.createElement("option");
-                option.selected =
-                    item.goal.average?.toString() === note.toString();
-                option.value = note;
-                option.textContent = `${note}.0`;
-                sAverage.appendChild(option);
-            });
-            sAverage.onchange = (e) => {
-                goalsEvaluations[index].goal.average = e.target.value;
-                verifyGoals();
-            };
-
-            // C Index
-            const cIndex = document.createElement("td");
-            cIndex.className = "font-semibold text-center p-2";
-            cIndex.innerHTML = `${index + 1}`;
-
-            const cGoal = document.createElement("td");
-            cGoal.appendChild(divGoal);
-
-            const cDescription = document.createElement("td");
-            cDescription.appendChild(divDescription);
-
-            const cIndicators = document.createElement("td");
-            cIndicators.appendChild(divIndicators);
-
-            const cPercentage = document.createElement("td");
-            cPercentage.appendChild(divPercentage);
-
-            const cSelfQualification = document.createElement("td");
-            cSelfQualification.appendChild(sNA);
-
-            const cAverage = document.createElement("td");
-            cAverage.appendChild(sAverage);
-
-            // const cButton = document.createElement("td");
-            // cButton.appendChild(deleteButton);
-
-            row.appendChild(cIndex);
-            row.appendChild(cGoal);
-            row.appendChild(cDescription);
-            row.appendChild(cIndicators);
-            row.appendChild(cPercentage);
-            row.appendChild(cSelfQualification);
-            row.appendChild(cAverage);
-            // if (goal.created_by) row.appendChild(cCreatedBy);
-            // else if (inputHiddenId)
-            //     row.appendChild(document.createElement("td"));
-            // if (goal.updated_by) row.appendChild(cUpdatedBy);
-            // else if (inputHiddenId)
-            //     row.appendChild(document.createElement("td"));
-            // row.appendChild(cButton);
-
-            tableGoals.appendChild(row);
         });
-        verifyGoals();
-    };
+        return items;
+    }
 
-    // Updated self qualification
-    if (selfQualifacationButton) {
-        selfQualifacationButton.addEventListener("click", async () => {
-            const items = goalsEvaluations.map((item) => ({
-                id: item.id,
-                self_qualification: Number(item.goal.self_qualification),
-            }));
-            const id_evaluation = inputHiddenIdEvaluation.value;
+    $rows?.forEach(($row) => {
+        const $selfQualificationSelect = $row.querySelector(
+            ".self-qualification"
+        );
+        const $qualificationSelect = $row.querySelector(".qualification");
 
+        $selfQualificationSelect?.addEventListener("change", async (e) => {
+            $totalSelfQualification.innerHTML = getTotalSelfQualification();
+        });
+
+        $qualificationSelect?.addEventListener("change", async (e) => {
+            $totalQualification.innerHTML = getTotalQualification();
+        });
+    });
+
+    $selfQualify?.addEventListener("click", () => {
+        const id = $selfQualify.getAttribute("data-id");
+        void qualify(id, getTotalSelfQualification(), getItems());
+    });
+
+    $qualify?.addEventListener("click", () => {
+        const id = $qualify.getAttribute("data-id");
+        void qualify(id, getTotalQualification(), getItems(), false);
+    });
+
+    async function qualify(id, total, items, selfQualify = true) {
+        if (total == 0) {
             Swal.fire({
-                title: "¿Estás seguro de finalizar la autocalificación de los objetivos?",
+                icon: "error",
+                title: "Oops...",
+                confirmButtonColor: "#d33",
+                cancelButtonColor: "#3085d6",
+                text: `La ${
+                    selfQualify ? "autocalificación" : "calificación"
+                } total no puede ser 0`,
+            });
+            return;
+        }
+        try {
+            const result = await Swal.fire({
+                title: `¿Estás seguro de finalizar la ${
+                    selfQualify ? "autocalificación" : "calificación"
+                } de los objetivos?`,
                 text: "No podrás deshacer esta acción.",
                 icon: "warning",
                 showCancelButton: true,
                 confirmButtonColor: "#d33",
                 cancelButtonColor: "#3085d6",
-                confirmButtonText: "Sí, finalizar",
+                confirmButtonText: `Sí, ${
+                    selfQualify ? "autocalificar" : "calificar"
+                }`,
                 cancelButtonText: "Cancelar",
-            }).then(async (result) => {
-                if (result.isConfirmed) {
-                    try {
-                        await axios.post("/api/evaluation/self-qualification", {
-                            id_evaluation,
-                            items,
-                        });
-                        window.location.reload();
-                    } catch (error) {
-                        console.log(error);
-                        Swal.fire({
-                            icon: "error",
-                            title: "Oops...",
-                            text: "Ocurrio un error al aprobar los objetivos, intentelo de nuevo",
-                        });
-                    }
-                }
             });
-        });
-    }
+            if (!result.isConfirmed) return;
 
-    // Updated average
-    if (averageButton) {
-        averageButton.addEventListener("click", async () => {
-            const items = goalsEvaluations.map((item) => ({
-                id: item.id,
-                average: Number(item.goal.average),
-            }));
-            const id_evaluation = inputHiddenIdEvaluation.value;
+            const { data } = await axios.post(
+                `/api/evaluations/${
+                    selfQualify ? "self-qualify" : "qualify"
+                }/${id}`,
+                {
+                    items,
+                }
+            );
 
             Swal.fire({
-                title: "¿Estás seguro de finalizar la calificación de los objetivos?",
-                text: "No podrás deshacer esta acción.",
-                icon: "warning",
-                showCancelButton: true,
+                icon: "success",
+                title: "¡Hecho!",
                 confirmButtonColor: "#d33",
                 cancelButtonColor: "#3085d6",
-                confirmButtonText: "Sí, finalizar",
-                cancelButtonText: "Cancelar",
-            }).then(async (result) => {
-                if (result.isConfirmed) {
-                    try {
-                        await axios.post("/api/evaluation/average", {
-                            id_evaluation,
-                            items,
-                        });
-                        window.location.reload();
-                    } catch (error) {
-                        console.log(error);
-                        Swal.fire({
-                            icon: "error",
-                            title: "Oops...",
-                            text: "Ocurrio un error al aprobar los objetivos, intentelo de nuevo",
-                        });
-                    }
-                }
+                text: data ?? "Operación exitosa",
+            }).then(() => {
+                window.location.reload();
             });
-
-            // const res = await axios.post(
-            //     `/api/evaluations/${inputHiddenIdEvaluation.value}/self-qualification`,
-            //     data
-            // );
-            // if (res.status === 200) {
-            //     alert("Calificaciones guardadas correctamente");
-            // }
-        });
-    }
-
-    // Close evaluation
-    if (closeButton) {
-        closeButton.addEventListener("click", async () => {
-            const id_evaluation = inputHiddenIdEvaluation.value;
-
+        } catch (error) {
             Swal.fire({
-                title: "¿Estás seguro de finalizar la evaluación?",
-                text: "No podrás deshacer esta acción.",
-                icon: "warning",
-                showCancelButton: true,
+                icon: "error",
+                title: "Oops...",
                 confirmButtonColor: "#d33",
                 cancelButtonColor: "#3085d6",
-                confirmButtonText: "Sí, finalizar",
-                cancelButtonText: "Cancelar",
-            }).then(async (result) => {
-                if (result.isConfirmed) {
-                    try {
-                        await axios.post("/api/evaluation/close", {
-                            id_evaluation,
-                        });
-                        window.location.reload();
-                    } catch (error) {
-                        console.log(error);
-                        Swal.fire({
-                            icon: "error",
-                            title: "Oops...",
-                            text: "Ocurrio un error al aprobar los objetivos, intentelo de nuevo",
-                        });
-                    }
-                }
+                text: error.response.data ?? "Error al enviar el formulario",
             });
-        });
+        }
     }
 
-    renderGoals();
+    // close
+    $close?.addEventListener("click", async () => {
+        const id = $close.getAttribute("data-id");
+
+        const result = await Swal.fire({
+            title: "¿Estás seguro de finalizar la evaluación?",
+            text: "No podrás deshacer esta acción.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Sí, finalizar",
+            cancelButtonText: "Cancelar",
+        });
+
+        if (!result.isConfirmed) return;
+
+        try {
+            const { data } = await axios.post(`/api/evaluations/close/${id}`);
+            Swal.fire({
+                icon: "success",
+                title: "¡Hecho!",
+                confirmButtonColor: "#d33",
+                cancelButtonColor: "#3085d6",
+                text: data ?? "Operación exitosa",
+            }).then(() => {
+                window.location.reload();
+            });
+        } catch (error) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                confirmButtonColor: "#d33",
+                cancelButtonColor: "#3085d6",
+                text: error.response.data ?? "Error al enviar el formulario",
+            });
+        }
+    });
 });
