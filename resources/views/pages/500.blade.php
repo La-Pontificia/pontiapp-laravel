923e08ac0074d479b95a7389e58f87a72d36d64e
Daustinn <daustinn.music@gmail.com>
2024-08-08 21:55:53 -0500

    refactor: Update 500.blade.php to improve error page layout and content

--- resources/views/pages/500.blade.php
+++ resources/views/pages/500.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.headers')
 
 @section('app')
-    <div class="flex flex-col h-svh bg-black text-white">
+    <div class="flex flex-col h-svh bg-gradient-to-tr text-black from-yellow-400 via-rose-400 to-violet-400 ">
         <div class="w-full grid h-full place-content-center">
             <div class="text-center space-y-3">
-                <h1 class="text-4xl font-serif tracking-tight">Error 500</h1>
-                <p class="text-sm max-w-[35ch] text-stone-300">
+                <h2 class="text-2xl font-bold">
+                    500 | Error del servidor
+                </h2>
+                <p class="text-sm max-w-[35ch] text-black">
                     A ocurrido un error, por favor intenta de nuevo
-                    o volver al <a href="/" class="text-blue-400 hover:underline">inicio</a>.
+                    o volver al <a href="/" class="font-bold hover:underline">inicio</a>.
                 </p>
-
             </div>
         </div>
-        <footer class="p-4 text-center text-xs text-stone-400">
+        <footer class="p-4 text-center text-xs text-black">
             Desarrollado por <a href="https://daustinn.com" target="_blank" rel="noopener noreferrer"
                 class=" font-semibold hover:underline">Daustinn</a> &copy; 2024
         </footer>
