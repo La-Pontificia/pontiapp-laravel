eff99f47f5835627050d05e9d07d26e8a076188f
Daustinn <daustinn.music@gmail.com>
2024-08-10 01:47:29 -0500

    refactor: Update sidebar modules layout

--- resources/views/modules/assists/sidebar.blade.php
+++ resources/views/modules/assists/sidebar.blade.php
@@ -22,18 +22,18 @@
             @svg('heroicon-o-arrow-left', [
                 'class' => 'w-5 h-5',
             ])
-            <span class="max-lg:hidden">Gestión de asistencias</span>
+            <span class="max-xl:hidden">Gestión de asistencias</span>
         </a>
     </div>
-    <nav class="px-2 py-2 pt-0">
+    <nav class="px-2 py-3 max-xl:space-y-3">
         @foreach ($items as $item)
             <a {{ $item['active'] ? 'data-active' : '' }} title="{{ $item['text'] }}"
-                class="flex group relative data-[active]:font-medium gap-2 p-2 hover:bg-neutral-200 rounded-lg"
+                class="flex group relative data-[active]:font-medium gap-2 p-2 hover:bg-white rounded-lg"
                 href="{{ $item['href'] }}">
                 @svg($item['active'] ? $item['active-icon'] : $item['icon'], [
-                    'class' => 'w-5 h-5 max-lg:w-6 max-lg:h-6 max-lg:mx-auto group-data-[active]:text-blue-800',
+                    'class' => 'w-5 h-5 max-xl:w-6 max-xl:h-6 max-xl:mx-auto group-data-[active]:text-blue-800',
                 ])
-                <span class="max-lg:hidden">{{ $item['text'] }}</span>
+                <span class="max-xl:hidden">{{ $item['text'] }}</span>
             </a>
         @endforeach
     </nav>
