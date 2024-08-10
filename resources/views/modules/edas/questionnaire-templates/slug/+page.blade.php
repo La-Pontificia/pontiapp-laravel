5d0ec8be3f8310c9e684bf3b13322db5ee6e7a0a
Daustinn <daustinn.music@gmail.com>
2024-08-09 20:52:45 -0500

    refactor: Update page title and layout in questionnaire template view

--- resources/views/modules/edas/questionnaire-templates/slug/+page.blade.php
+++ resources/views/modules/edas/questionnaire-templates/slug/+page.blade.php
@@ -1,9 +1,9 @@
 @extends('modules.edas.+layout')
 
-@section('title', 'Ediat plantilla: ' . $template->name)
+@section('title', 'Editar plantilla: ' . $template->name)
 
 @section('layout.edas')
-    <div class="h-full bg-white w-full shadow-md flex flex-col border rounded-xl">
+    <div class="h-full bg-white relative w-full shadow-md flex flex-col border rounded-xl">
         <nav class="border-b p-1 flex gap-3">
             <button onclick="window.history.back()"
                 class="text-blue-500 hover:bg-white font-semibold justify-center min-w-max flex items-center rounded-md p-2 gap-1 text-sm px-2">
@@ -20,7 +20,7 @@
         <div class="p-3 px-4 border-t border-neutral-300 flex gap-2">
             <button type="submit" form="template-form"
                 class="bg-blue-700 w-fit shadow-md shadow-blue-500/30 font-semibold hover:bg-blue-600 min-w-max flex items-center rounded-full p-1.5 gap-1 text-white text-sm px-3">
-                Registrar
+                Actualizar
             </button>
             <button onclick="window.history.back()" type="button"
                 class="bg-white w-fit font-semibold hover:text-blue-600 text-black min-w-max flex items-center rounded-full p-1.5 gap-1 text-sm px-3">
