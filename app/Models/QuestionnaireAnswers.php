7dbd2847298edaa8d556bdbbdd2b4e4353c7802c
Daustinn <daustinn.music@gmail.com>
2024-08-09 20:51:58 -0500

    feat: Add QuestionnaireAnswers model and relationships

---
+++ app/Models/QuestionnaireAnswers.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionnaireAnswers extends Model
+{
+    use HasUuids, HasFactory;
+
+    protected $table = 'questionnaire_answers';
+
+    protected $perPage = 20;
+
+    protected $fillable = ['answer', 'question_id', 'questionnaire_id'];
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    public function question()
+    {
+        return $this->hasOne(Question::class, 'id', 'question_id');
+    }
+
+    public function questionnaire()
+    {
+        return $this->hasOne(QuestionnaireTemplate::class, 'id', 'questionnaire_id');
+    }
+}
