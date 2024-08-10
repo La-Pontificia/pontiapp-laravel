2144223c3063ab4961c3b25d94070a6864e4e605
Daustinn <daustinn.music@gmail.com>
2024-08-09 20:51:54 -0500

    refactor: Create Questionnaire model and update relationships

---
+++ app/Models/Questionnaire.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Questionnaire extends Model
+{
+    use HasUuids, HasFactory;
+
+    protected $table = 'questionnaires';
+
+    protected $perPage = 20;
+
+    protected $fillable = [
+        'answered_by',
+        'eda_id',
+        'questionnaire_template_id',
+    ];
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $hidden = [
+        'archived',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function answers()
+    {
+        return $this->hasMany(QuestionnaireAnswers::class, 'questionnaire_id', 'id');
+    }
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class, 'template_id', 'id')->where('archived', false)->orderBy('order');
+    }
+
+    public function allQuestions()
+    {
+        return $this->hasMany(Question::class, 'template_id', 'id');
+    }
+
+    public function createdBy()
+    {
+        return $this->hasOne(User::class, 'id', 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->hasOne(User::class, 'id', 'updated_by');
+    }
+}
