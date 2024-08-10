82706f6880c82df50fd1b228141ba3e0f46014f2
Daustinn <daustinn.music@gmail.com>
2024-08-09 20:51:51 -0500

    refactor: Update Question model relationships and hidden attributes

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -15,24 +15,36 @@ class Question extends Model
 
   protected $perPage = 20;
 
-  protected $fillable = ['question', 'order', 'template_id', 'created_by', 'udpated_by'];
+  protected $fillable = ['question', 'order', 'template_id', 'archived', 'created_by', 'udpated_by'];
 
   protected $keyType = 'string';
 
   public $incrementing = false;
 
+
+  protected $hidden = [
+    'archived',
+    'created_by',
+    'updated_by',
+  ];
+
   public function template()
   {
-    return $this->hasOne(QuestionnaireTemplate::class, 'template_id', 'id');
+    return $this->hasOne(QuestionnaireTemplate::class, 'id', 'template_id');
+  }
+
+  public function answers()
+  {
+    return $this->hasMany(QuestionnaireQuestion::class, 'question_id', 'id');
   }
 
   public function createdBy()
   {
-    return $this->hasOne(User::class, 'created_by', 'id');
+    return $this->hasOne(User::class, 'id', 'created_by');
   }
 
   public function updatedBy()
   {
-    return $this->hasOne(User::class, 'updated_by', 'id');
+    return $this->hasOne(User::class, 'id', 'updated_by');
   }
 }
