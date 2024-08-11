702de1d9ca41a2addcec8d95578cbe80eb6a459e
Daustinn <daustinn.music@gmail.com>
2024-08-11 11:31:46 -0500

    Refactor and update edas page layout

--- resources/views/modules/edas/+page.blade.php
+++ resources/views/modules/edas/+page.blade.php
@@ -3,7 +3,74 @@
 @section('title', 'Gestión de Edas')
 
 @section('layout.edas')
-    <div class="text-black h-full w-full flex-grow flex flex-col overflow-y-auto">
+    <div class="w-full max-w-7xl mx-auto">
+        <h2 class="py-5">Colaboradores bajo tu supervisión</h2>
+        <div
+            class="flex flex-col w-full overflow-auto bg-white border-neutral-300 shadow-[0_0_10px_rgba(0,0,0,.2)] border rounded-xl">
+            <div class="w-full flex gap-2 p-2">
+                <label class="relative w-full">
+                    <div class="absolute inset-y-0 z-10 text-neutral-400 grid place-content-center left-2">
+                        @svg('bx-search', 'w-5 h-5')
+                    </div>
+                    <input value="{{ request()->get('q') }}" placeholder="Filtrar usuarios..." type="search"
+                        class="w-full pl-9 dinamic-search">
+                </label>
+            </div>
+            <div class="flex flex-col divide-y">
+                @if ($cuser->has('users:edas:show') || $cuser->isDev())
+                    @forelse ($users as $user)
+                        <div class="flex relative items-center p-3 gap-2">
+                            @include('commons.avatar', [
+                                'src' => $user->profile,
+                                'className' => 'w-8',
+                                'alt' => $user->first_name . ' ' . $user->last_name,
+                                'altClass' => 'text-base',
+                            ])
+                            <div class="flex-grow">
+                                <p>
+                                    <a href="/users/{{ $user->id }}"
+                                        title="Ver perfil de {{ $user->first_name . ' ' . $user->last_name }}"
+                                        class="hover:underline text-nowrap hover:text-blue-700">
+                                        {{ $user->last_name . ', ' . $user->first_name }}
+                                    </a>
+                                </p>
+                                <p class="line-clamp-2 flex text-sm items-center gap-1 text-neutral-600">
+                                    @svg('bx-briefcase-alt-2', 'w-3 h-3')
+                                    {{ $user->role_position->name }}
+                                </p>
+                            </div>
+                            @if ($user->supervisor)
+                                <p class="text-nowrap">
+                                    Bajo supervisión de <a href="/users/{{ $user->supervisor->id }}"
+                                        class="hover:underline text-nowrap hover:text-blue-700">
+                                        {{ $user->supervisor->last_name }},
+                                        {{ $user->supervisor->first_name }}
+                                    </a>
+                                </p>
+                            @endif
+                            <button type="button" data-modal-target="dialog-{{ $user->id }}"
+                                data-modal-toggle="dialog-{{ $user->id }}"
+                                class="rounded-full p-2 hover:bg-neutral-200 transition-colors">
+                                @svg('bx-pencil', 'w-4 h-4')
+                            </button>
+                        </div>
+                    @empty
+                        <p class="p-20 grid place-content-center text-center">
+                            No hay nada que mostrar.
+                        </p>
+                    @endforelse
+                    <footer class="px-5 py-4">
+                        {!! $users->links() !!}
+                    </footer>
+                @else
+                    <p class="p-20 grid place-content-center text-center">
+                        No tienes permisos para visualizar estos datos.
+                    </p>
+                @endif
+            </div>
+        </div>
+    </div>
+    {{-- <div class="text-black h-full w-full flex-grow flex flex-col overflow-y-auto">
         <nav class="flex p-1 flex-grow gap-3 items-center">
 
             <label class="relative">
@@ -151,5 +218,5 @@
                 'message' => 'No tienes permisos para ver los usuarios de los edas.',
             ])
         @endif
-    </div>
+    </div> --}}
 @endsection
--- resources/views/modules/edas/sidebar.blade.php
+++ resources/views/modules/edas/sidebar.blade.php
@@ -1,15 +1,15 @@
 @php
     $items = [
         [
-            'icon' => 'heroicon-o-clipboard-document-check',
+            'icon' => 'bx-folder',
             'text' => 'Mis edas',
             'href' => '/edas/me',
             'active' => request()->is('edas' . '/' . $cuser->id . '*'),
-            'active-icon' => 'heroicon-s-clipboard-document-check',
+            'active-icon' => 'bxs-folder-open',
             'enable' => true,
         ],
         [
-            'icon' => 'heroicon-o-user-group',
+            'icon' => 'bx-group',
             'text' => 'Colaboradores',
             'href' => '/edas',
             'active' =>
@@ -18,31 +18,31 @@
                 !request()->is('edas/years*') &&
                 !request()->is('edas/reports*') &&
                 !request()->is('edas' . '/' . $cuser->id . '*'),
-            'active-icon' => 'heroicon-s-user-group',
+            'active-icon' => 'bxs-group',
             'enable' => true,
         ],
         [
-            'icon' => 'heroicon-o-chart-bar',
+            'icon' => 'bx-bar-chart-alt-2',
             'text' => 'Reporte y estadísticas',
             'href' => '/edas/reports',
             'active' => request()->is('edas/reports*'),
-            'active-icon' => 'heroicon-s-chart-bar',
+            'active-icon' => 'bxs-bar-chart-alt-2',
             'enable' => $cuser->has('edas:reports:show') || $cuser->isDev(),
         ],
         [
-            'icon' => 'heroicon-o-stop',
+            'icon' => 'bx-notification',
             'text' => 'Años',
             'href' => '/edas/years',
             'active' => request()->is('edas/years*'),
-            'active-icon' => 'heroicon-s-stop',
+            'active-icon' => 'bxb-notification',
             'enable' => $cuser->has('edas:years:show') || $cuser->has('edas:years:create') || $cuser->isDev(),
         ],
         [
-            'icon' => 'heroicon-o-clipboard-document',
+            'icon' => 'bx-spreadsheet',
             'text' => 'Plantilla de cuestionarios',
             'href' => '/edas/questionnaire-templates',
             'active' => request()->is('edas/questionnaire-templates*'),
-            'active-icon' => 'heroicon-s-clipboard-document',
+            'active-icon' => 'bxs-spreadsheet',
             'enable' =>
                 $cuser->has('edas:questionnaire-templates:show') ||
                 $cuser->has('edas:questionnaire-templates:create') ||
@@ -50,29 +50,29 @@
         ],
     ];
 @endphp
-<div class="flex flex-col overflow-y-auto">
-    <div class="flex items-center justify-between p-4">
-        <a href="/" class="flex gap-2 font-medium items-center text-gray-900 ">
-            @svg('heroicon-o-arrow-left', [
-                'class' => 'w-5 h-5',
-            ])
-            <span class="max-xl:hidden">Gestión de edas</span>
-        </a>
-    </div>
-    <nav class="px-2 py-3 max-xl:space-y-3">
-        @foreach ($items as $item)
-            @if (!$item['enable'])
-                @continue
-            @endif
 
-            <a {{ $item['active'] ? 'data-active' : '' }} title="{{ $item['text'] }}"
-                class="flex group relative data-[active]:font-medium gap-2 p-2 hover:bg-white rounded-lg"
-                href="{{ $item['href'] }}">
-                @svg($item['active'] ? $item['active-icon'] : $item['icon'], [
-                    'class' => 'w-5 h-5 max-xl:w-6 max-xl:h-6 max-xl:mx-auto group-data-[active]:text-blue-800',
-                ])
-                <span class="max-xl:hidden">{{ $item['text'] }}</span>
-            </a>
-        @endforeach
-    </nav>
+<div class="border-b pb-3">
+    <a href="/" class="flex items-center gap-2 group p-2 px-4 pb-0">
+        @svg('bx-arrow-back', [
+            'class' => 'w-7 h-7 rounded-full p-1 group-hover:bg-neutral-200',
+        ])
+        <span class="opacity-80">Edas la Pontificia</span>
+    </a>
 </div>
+<nav class="py-3 ">
+    @foreach ($items as $item)
+        @if (!$item['enable'])
+            @continue
+        @endif
+
+        <a title="{{ $item['text'] }}" {{ $item['active'] ? 'data-active' : '' }}
+            class="flex group relative items-center data-[active]:font-medium data-[active]:bg-[#e8f0fe] data-[active]:text-[#1967da] gap-3 p-2 px-5 text-[#202124] hover:bg-[#e8eaed] rounded-r-full"
+            href="{{ $item['href'] }}">
+            @svg($item['active'] ? $item['active-icon'] : $item['icon'], [
+                'class' => 'w-5 h-5 m-0.5',
+                'stroke-width' => 1.4,
+            ])
+            <span class="text-nowrap">{{ $item['text'] }}</span>
+        </a>
+    @endforeach
+</nav>
