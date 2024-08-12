6ffbe818fd63fac3a7dbad4dc44bc8fa55eca5a5
Daustinn <daustinn.music@gmail.com>
2024-08-12 04:16:46 -0500

    Refactor and update form.blade.php for better user experience

--- resources/views/modules/edas/questionnaire-templates/slug/+page.blade.php
+++ resources/views/modules/edas/questionnaire-templates/slug/+page.blade.php
@@ -3,7 +3,30 @@
 @section('title', 'Editar plantilla: ' . $template->name)
 
 @section('layout.edas')
-    <div class="h-full bg-white relative w-full shadow-md flex flex-col border rounded-xl">
+
+    <div class="w-full max-w-2xl mx-auto">
+        <h2 class="py-5">
+            <a href="/edas/questionnaire-templates" class="flex items-center gap-2">
+                @svg('bx-left-arrow-alt', 'w-5 h-5')
+                Editar plantilla de cuestionarios: {{ $template->name }}
+            </a>
+        </h2>
+        <div class="flex flex-col w-full bg-white border-neutral-300 shadow-[0_0_10px_rgba(0,0,0,.2)] border rounded-xl">
+            <form id="template-form" class="flex flex-col px-1 h-full overflow-y-auto w-full" role="form">
+                @include('modules.edas.questionnaire-templates.form')
+            </form>
+            <div class="p-3 px-4 border-t border-neutral-300 flex gap-2">
+                <button type="submit" form="template-form" class="primary">
+                    Actualizar plantilla
+                </button>
+                <a href="/edas/questionnaire-templates" class="secondary">
+                    Cancelar
+                </a>
+            </div>
+        </div>
+    </div>
+
+    {{-- <div class="h-full bg-white relative w-full shadow-md flex flex-col border rounded-xl">
         <nav class="border-b p-1 flex gap-3">
             <button onclick="window.history.back()"
                 class="text-blue-500 hover:bg-white font-semibold justify-center min-w-max flex items-center rounded-md p-2 gap-1 text-sm px-2">
@@ -27,5 +50,5 @@
                 Cancelar
             </button>
         </div>
-    </div>
+    </div> --}}
 @endsection
