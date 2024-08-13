83daebbabfe15eec8bb89eb7867cf1e495b37a27
Daustinn <daustinn.music@gmail.com>
2024-08-13 14:04:38 -0500

    Refactor form.blade.php for improved code readability and consistency

--- resources/views/modules/settings/departments/+page.blade.php
+++ resources/views/modules/settings/departments/+page.blade.php
@@ -3,150 +3,84 @@
 @section('title', 'Ajustes del sistema: Departamentos')
 
 @section('layout.settings')
-    <div class="text-black h-full w-full flex-grow flex flex-col overflow-y-auto">
-        <button type="button" data-modal-target="dialog" data-modal-toggle="dialog"
-            class="bg-blue-700 w-fit shadow-md shadow-blue-500/30 font-semibold hover:bg-blue-600 min-w-max flex items-center rounded-full p-2 gap-1 text-white text-sm px-3">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                class="lucide lucide-plus">
-                <path d="M5 12h14" />
-                <path d="M12 5v14" />
-            </svg>
-            <span class="max-lg:hidden">Agregar nuevo departamento</span>
-        </button>
-
-        <div id="dialog" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
-            class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative w-full max-w-md max-h-full bg-white rounded-2xl shadow">
-                <div class="flex items-center justify-between p-3 border-b rounded-t">
-                    <h3 class="text-lg font-semibold text-gray-900">
-                        Agrega nuevo departamento
-                    </h3>
-                    <button type="button"
-                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-                        data-modal-hide="dialog">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                        </svg>
-                    </button>
-                </div>
-                @include('components.users.auditory-card')
-                <form action="/api/departments" method="POST" id="dialog-form" class="p-3 dinamic-form grid gap-4">
-                    @include('modules.settings.departments.form')
-                </form>
-                <div class="flex items-center p-3 border-t border-gray-200 rounded-b">
-                    <button form="dialog-form" type="submit"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-sm px-5 py-2.5 text-center">
-                        Guardar</button>
-                    <button id="button-close-scheldule-modal" data-modal-hide="dialog" type="button"
-                        class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-xl border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Cancelar</button>
+    <div class="w-full max-w-2xl mx-auto">
+        <h2 class="py-5">Departamentos.</h2>
+        <div class="flex flex-col w-full bg-white border-neutral-300 shadow-[0_0_10px_rgba(0,0,0,.2)] border rounded-xl">
+            <button type="button" data-modal-target="dialog" data-modal-toggle="dialog" class="primary m-2">
+                @svg('bx-plus', 'w-5 h-5')
+                <span>Agregar nuevo departamento.</span>
+            </button>
+            <div id="dialog" tabindex="-1" aria-hidden="true" class="dialog hidden">
+                <div class="content lg:max-w-lg max-w-full">
+                    <header>
+                        Registrar nuevo departamento
+                    </header>
+                    <form action="/api/departments" method="POST" id="dialog-form" class="dinamic-form body grid gap-4">
+                        @include('modules.settings.departments.form')
+                    </form>
+                    <footer>
+                        <button data-modal-hide="dialog" type="button">Cancelar</button>
+                        <button form="dialog-form" type="submit">
+                            Guardar</button>
+                    </footer>
                 </div>
             </div>
+            <div class="flex flex-col divide-y">
+                @forelse ($departments as $department)
+                    <div class="flex relative hover:bg-neutral-100 items-center p-2.5 gap-2">
+                        @svg('bx-folder', 'w-5 h-5 mr-2')
+                        <div class="flex-grow">
+                            <p>{{ $department->code }}-{{ $department->name }}</p>
+                            <p class="flex text-nowrap text-sm items-center flex-wrap gap-1 text-neutral-600">
+                                {{ $department->roles->count() }} cargos.
+                            </p>
+                        </div>
+                        <button type="button" data-modal-target="dialog-{{ $department->id }}"
+                            data-modal-toggle="dialog-{{ $department->id }}"
+                            class="rounded-full p-2 hover:bg-neutral-200 transition-colors">
+                            @svg('bx-pencil', 'w-4 h-4')
+                        </button>
+                        <div id="dialog-{{ $department->id }}" tabindex="-1" aria-hidden="true" class="dialog hidden">
+                            <div class="content lg:max-w-lg max-w-full">
+                                <header>
+                                    Editar departamento: {{ $department->name }}
+                                </header>
+                                <form action="/api/department/{{ $department->id }}" method="POST"
+                                    id="dialog-{{ $department->id }}-form"
+                                    class="dinamic-form body grid gap-4 overflow-y-auto">
+                                    @include('modules.settings.departments.form', [
+                                        'department' => $department,
+                                    ])
+                                </form>
+                                <footer>
+                                    <button data-modal-hide="dialog-{{ $department->id }}" type="button">Cancelar</button>
+                                    <button form="dialog-{{ $department->id }}-form" type="submit">
+                                        Guardar</button>
+                                </footer>
+                            </div>
+                        </div>
+                        <button class="rounded-full p-2 hover:bg-neutral-200 transition-colors"
+                            data-dropdown-toggle="dropdown-{{ $department->id }}">
+                            @svg('bx-dots-vertical-rounded', 'w-4 h-4')
+                        </button>
+                        <div id="dropdown-{{ $department->id }}" class="dropdown-content hidden">
+                            <button data-atitle="¿Estás seguro de eliminar?"
+                                data-adescription="Se eliminarán todos los departamentos asociados y los usuarios asignados a estos departamentos."
+                                data-param="/api/department/delete/{{ $department->id }}"
+                                class="p-2 dinamic-alert hover:bg-neutral-100 text-left w-full block rounded-md hover:bg-gray-10">
+                                Eliminar
+                            </button>
+                        </div>
+                    </div>
+                @empty
+                    <p class="p-20 grid place-content-center text-center">
+                        No hay nada que mostrar.
+                    </p>
+                @endforelse
+                <footer class="px-5 py-4">
+                    {!! $departments->links() !!}
+                </footer>
+            </div>
         </div>
-        <div class="overflow-auto flex-grow">
-            <table class="w-full text-left text-sm">
-                <thead class="border-b">
-                    <tr class="[&>th]:font-medium [&>th]:text-nowrap [&>th]:p-4 [&>th]:px-2">
-                        <th class="w-full">Departamento</th>
-                        <th>Area</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody class="divide-y">
-                    @if ($departments->count() === 0)
-                        <tr class="">
-                            <td colspan="11" class="text-center py-4">
-                                <div class="p-10">
-                                    No hay nada por aquí
-                                </div>
-                            </td>
-                        </tr>
-                    @else
-                        @foreach ($departments as $department)
-                            <tr
-                                class="[&>td]:py-4 hover:border-transparent hover:[&>td]shadow-md relative group first:[&>td]:rounded-l-2xl last:[&>td]:rounded-r-2xl hover:bg-white [&>td]:px-2">
-                                <td>
-                                    <div class="flex gap-3 items-center">
-                                        <button title="Click para editar" class="absolute inset-0"
-                                            data-modal-target="dialog-{{ $department->id }}"
-                                            data-modal-toggle="dialog-{{ $department->id }}">
-                                        </button>
-
-                                        <div id="dialog-{{ $department->id }}" data-modal-backdrop="static" tabindex="-1"
-                                            aria-hidden="true"
-                                            class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                                            <div class="relative w-full max-w-md max-h-full bg-white rounded-2xl shadow">
-                                                <div class="flex items-center justify-between p-3 border-b rounded-t">
-                                                    <h3 class="text-lg font-semibold text-gray-900">
-                                                        Editar departamento
-                                                    </h3>
-                                                    <button type="button"
-                                                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-                                                        data-modal-hide="dialog-{{ $department->id }}">
-                                                        <svg class="w-3 h-3" aria-hidden="true"
-                                                            xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                            viewBox="0 0 14 14">
-                                                            <path stroke="currentColor" stroke-linecap="round"
-                                                                stroke-linejoin="round" stroke-width="2"
-                                                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                                        </svg>
-                                                    </button>
-                                                </div>
-                                                @include('components.users.auditory-card')
-                                                <form action="/api/departments/{{ $department->id }}" method="POST"
-                                                    id="dialog-{{ $department->id }}-form"
-                                                    class="p-3 dinamic-form grid gap-4">
-                                                    @include('modules.settings.departments.form', [
-                                                        'area' => $department,
-                                                    ])
-                                                </form>
-                                                <div class="flex items-center p-3 border-t border-gray-200 rounded-b">
-                                                    <button form="dialog-{{ $department->id }}-form" type="submit"
-                                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-sm px-5 py-2.5 text-center">
-                                                        Guardar</button>
-                                                    <button id="button-close-scheldule-modal"
-                                                        data-modal-hide="dialog-{{ $department->id }}" type="button"
-                                                        class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-xl border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Cancelar</button>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <p
-                                            class="bg-blue-500/5 p-1 text-nowrap rounded-md px-3 text-blue-500 font-semibold">
-                                            {{ $department->code }}</p>
-                                        <p class="font-medium">{{ $department->name }}</p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <p class="text-nowrap">
-                                        {{ $department->area->name }}
-                                    </p>
-                                </td>
-                                <td>
-                                    <div class="flex items-center gap-2">
-                                        <p class="text-sm font-normal flex-grow text-nowrap">
-                                            <span class="block">
-                                                {{ $department->createdBy->last_name }},
-                                                {{ $department->createdBy->first_name }}</span>
-                                        </p>
-                                    </div>
-                                </td>
-                                <td>
-                                    <p class="text-nowrap">
-                                        {{ \Carbon\Carbon::parse($department->created_at)->isoFormat('LL') }}
-                                    </p>
-                                </td>
-                            </tr>
-                        @endforeach
-                    @endif
-                </tbody>
-            </table>
-        </div>
-        <footer class="px-5 pt-4">
-            {!! $departments->links() !!}
-        </footer>
     </div>
 @endsection
