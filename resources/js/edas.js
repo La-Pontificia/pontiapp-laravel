d43b254790a2f3cc9c0bf77fba92783a7c8b18a4
Daustinn <daustinn.music@gmail.com>
2024-08-13 14:02:55 -0500

    Refactor edas.js to add export functionality for EDAs

--- resources/js/edas.js
+++ resources/js/edas.js
@@ -1,17 +1,14 @@
 import axios from "axios";
-
-const Toast = Swal.mixin({
-    toast: true,
-    position: "bottom-left",
-    showConfirmButton: false,
-    timer: 1000,
-    timerProgressBar: true,
-});
+import ExcelJS from "exceljs";
+import moment from "moment";
 
 document.addEventListener("DOMContentLoaded", function () {
     $ = document.querySelector.bind(document);
     const CreateButton = $("#create-eda");
     const CloseButton = $("#close-eda");
+    const $formExport = $("#form-export-edas");
+    const $exportButton = $("#button-export-edas");
+
     CreateButton?.addEventListener("click", async () => {
         // disable the button
         CreateButton.disabled = true;
@@ -66,5 +63,116 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             }
         });
+    });
+
+    $exportButton?.addEventListener("click", async () => {
+        $exportButton.disabled = true;
+        $exportButton.classList.add("animation-pulse");
+        $exportButton.querySelector("span").innerText = "Exportando...";
+        const form = new FormData($formExport);
+
+        const type = form.get("type");
+
+        const url = new URL(window.location.href);
+        const searchParams = url.searchParams;
+        searchParams.set("type", type);
+
+        const edas = await window.query(
+            `/api/edas/export?${searchParams.toString()}`
+        );
+
+        const workbook = new ExcelJS.Workbook();
+        const response = await fetch("/templates/basic_edas.xlsx");
+        const arrayBuffer = await response.arrayBuffer();
+
+        if (!arrayBuffer) return;
+
+        await workbook.xlsx.load(arrayBuffer);
+        const worksheet = workbook.getWorksheet("Edas");
+        const startRow = 5;
+
+        edas.forEach((eda, i) => {
+            const row = worksheet.getRow(startRow + i);
+            const index = i + 1 < 10 ? `0${i + 1}` : i + 1;
+            row.getCell(2).value = index;
+            row.getCell(3).value = eda.year;
+            row.getCell(4).value = eda.full_name;
+            row.getCell(5).value = eda.role;
+            row.getCell(6).value = eda.job_position;
+
+            // GOALS
+            row.getCell(7).value = eda.goals.count;
+            row.getCell(8).value = eda.goals.sent
+                ? moment(eda.goals.sent).format("DD/MM/YYYY")
+                : "-";
+            row.getCell(9).value = eda.goals.approved
+                ? moment(eda.goals.approved).format("DD/MM/YYYY")
+                : "-";
+
+            // EVALUATION 01
+            row.getCell(10).value =
+                eda.evaluations["1"].self_qualification ?? "-";
+            row.getCell(11).value = eda.evaluations["1"].qualification ?? "-";
+            row.getCell(12).value = eda.evaluations["1"].closed
+                ? moment(eda.evaluations["1"].closed).format("DD/MM/YYYY")
+                : "-";
+            row.getCell(13).value = eda.evaluations["1"].feedback
+                ? moment(eda.evaluations["1"].feedback).format("DD/MM/YYYY")
+                : "-";
+
+            // EVALUATION 02
+            row.getCell(14).value =
+                eda.evaluations["2"].self_qualification ?? "-";
+            row.getCell(15).value = eda.evaluations["2"].qualification ?? "-";
+            row.getCell(16).value = eda.evaluations["2"].closed
+                ? moment(eda.evaluations["2"].closed).format("DD/MM/YYYY")
+                : "-";
+            row.getCell(17).value = eda.evaluations["2"].feedback
+                ? moment(eda.evaluations["2"].feedback).format("DD/MM/YYYY")
+                : "-";
+
+            row.getCell(18).value = eda.questionnaires.collaborator.resolved
+                ? moment(eda.questionnaires.collaborator.resolved).format(
+                      "DD/MM/YYYY"
+                  )
+                : "-";
+            row.getCell(19).value = eda.questionnaires.supervisor.resolved
+                ? moment(eda.questionnaires.supervisor.resolved).format(
+                      "DD/MM/YYYY"
+                  )
+                : "-";
+
+            row.getCell(20).value = eda.closed
+                ? moment(eda.closed).format("DD/MM/YYYY")
+                : "-";
+
+            row.commit();
+        });
+
+        worksheet.columns.forEach((column) => {
+            let maxLength = 0;
+            column.eachCell({ includeEmpty: true }, (cell) => {
+                const cellValue = cell.value ? cell.value.toString() : "";
+                maxLength = Math.max(maxLength, cellValue.length);
+            });
+            column.width = maxLength < 10 ? 10 : maxLength + 2;
+        });
+
+        const filename = `EDAs_${moment().format("YYYY-MM-DD_HH-mm")}`;
+
+        const buffer = await workbook.xlsx.writeBuffer();
+
+        const blob = new Blob([buffer], {
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        });
+
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = filename + ".xlsx";
+        link.click();
+
+        $exportButton.disabled = false;
+        $exportButton.classList.remove("animation-pulse");
+        $exportButton.querySelector("span").innerText = "Exportar";
     });
 });
