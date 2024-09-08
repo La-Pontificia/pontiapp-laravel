cb3f75f59d8ce13d72660cf7008207cb4c7f68cb
Daustinn <daustinn.music@gmail.com>
2024-09-08 11:06:31 -0500

    Refactor background image opacity in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <section class="">
         <div class="min-h-svh max-sm:w-full text-white flex max-sm:justify-start">
             <div class="flex h-full w-full">
-                <div style="background-image: linear-gradient(to bottom, rgba(6, 9, 13, 0.9), rgba(9, 5, 8, 0.9), rgba(15, 15, 8, 0.9)), url('/background.webp');"
+                <div style="background-image: linear-gradient(to bottom, rgba(6, 9, 13, 0.85), rgba(9, 5, 8, 0.85), rgba(15, 15, 8, 0.85)), url('/background.webp');"
                     class="w-full min-h-svh bg-center bg-cover grid place-content-center">
                     <div
                         class="flex flex-col flex-grow justify-center gap-4 relative sm:rounded-lg items-center max-sm:justify-start max-sm:items-start">
@@ -17,22 +17,29 @@
                                         <img src="/lp-square.webp" class="w-12 drop-shadow-md mx-auto" />
                                     </div>
                                     <div class="w-full">
-                                        {{-- <form method="POST" action="/login" class="grid gap-5">
+
+                                        <p class="text-sm text-center">
+                                            Inicia sesión con tu cuenta institucional de Microsoft.
+                                        </p>
+                                        <a href="/login/azure"
+                                            class="w-full shadow-md hover:shadow-lg mt-4 gap-2 rounded-xl justify-center text-gray-900 bg-white hover:border-blue-600 text-sm p-4 text-center inline-flex items-center">
+                                            <img src="/RE1Mu3b.png" class="w-24" alt="">
+                                        </a>
+                                        <div class="flex items-center gap-2 my-5 text-sm">
+                                            <span class="w-full border-b border-neutral-500 block"></span>
+                                            <p class="text-nowrap">o tambien</p>
+                                            <span class="w-full border-b border-neutral-500 block"></span>
+                                        </div>
+                                        <form method="POST" action="/login" class="grid gap-5">
                                             @csrf
                                             <div class="space-y-3 font-semibold text-black">
                                                 <input style="padding: 17px; border-radius: 10px" required autofocus
                                                     name="email" type="email" value="{{ old('email') }}"
-                                                    class="h-12 w-full font-normal" placeholder="Correo electrónico" />
+                                                    class="h-12 w-full font-semibold" placeholder="Correo electrónico" />
                                                 <input type="password" style="padding: 17px; border-radius: 10px" required
                                                     name="password" value="{{ old('password') }}"
-                                                    class="h-12 font-normal w-full" placeholder="Contraseña" />
+                                                    class="h-12 font-semibold w-full" placeholder="Contraseña" />
                                             </div>
-                                            @error('email')
-                                                <span class="text-sm px-2 text-red-500" role="alert">
-                                                    Credenciales incorrectas. Intente de nuevo.
-                                                </span>
-                                            @enderror
-
                                             <button type="submit"
                                                 class="p-4 rounded-xl font-semibold bg-black text-white w-full hover:text-neutral-300">
                                                 <span class="ml-3">
@@ -40,15 +47,7 @@
                                                 </span>
                                             </button>
                                         </form>
-                                        <div class="flex items-center gap-2 mt-4 text-sm">
-                                            <span class="w-full border-b border-neutral-500 block"></span>
-                                            <p class="text-nowrap">o tambien</p>
-                                            <span class="w-full border-b border-neutral-500 block"></span>
-                                        </div> --}}
-                                        <a href="/login/azure"
-                                            class="w-full shadow-md hover:shadow-lg mt-4 gap-2 rounded-xl justify-center text-gray-900 bg-white hover:border-blue-600 text-sm p-4 text-center inline-flex items-center">
-                                            <img src="/RE1Mu3b.png" class="w-24" alt="">
-                                        </a>
+
                                         @if (session('error'))
                                             <div class="text-sm max-w-max p-2 text-red-600">
                                                 {{ session('error') }}
@@ -56,7 +55,12 @@
                                         @endif
                                     </div>
                                     <p class="mt-6 text-xs text-gray-100 text-center">
-                                        @2024 <a href="https://lp.com.pe" target="_blank"
+
+                                        {{-- GET CURRENT YEAR --}}
+
+                                        &copy; {{ date('Y') }}
+
+                                        <a href="https://lp.com.pe" target="_blank"
                                             class="border-b border-gray-500 border-dotted">
                                             La Pontificia.
                                         </a> All rights reserved.
@@ -71,7 +75,7 @@
                         </div>
                         <footer class="p-4 text-center w-full text-xs text-white drop-shadow-md">
                             Developed by <a href="https://daustinn.com" target="_blank" rel="noopener noreferrer"
-                                class=" font-semibold hover:underline">Daustinn</a> &copy; 2024
+                                class=" font-semibold hover:underline">Daustinn</a> &copy; {{ date('Y') }}
                         </footer>
                     </div>
                 </div>
