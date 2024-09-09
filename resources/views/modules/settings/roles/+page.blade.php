2a4bc948cd6ff8577a6f7e75a5a2a04ea32f9a11
Daustinn <daustinn.music@gmail.com>
2024-09-09 08:44:25 -0500

    Refactor SVG icons and layout in create questionnaire template page

--- resources/views/modules/settings/roles/+page.blade.php
+++ resources/views/modules/settings/roles/+page.blade.php
@@ -7,7 +7,7 @@
         <h2 class="py-5">Cargos.</h2>
         <div class="flex flex-col w-full bg-white border-neutral-300 shadow-[0_0_10px_rgba(0,0,0,.2)] border rounded-xl">
             <button type="button" data-modal-target="dialog" data-modal-toggle="dialog" class="primary m-2">
-                @svg('bx-plus', 'w-5 h-5')
+                @svg('fluentui-add-circle-16-o', 'w-5 h-5')
                 <span>Agregar nuevo cargo.</span>
             </button>
             <div id="dialog" tabindex="-1" aria-hidden="true" class="dialog hidden">
@@ -28,11 +28,11 @@
             <div class="flex flex-col divide-y">
                 @forelse ($roles as $role)
                     <div class="flex relative hover:bg-neutral-100 items-start p-2.5 gap-2">
-                        @svg('bx-folder', 'w-5 h-5 mr-2')
+                        @svg('fluentui-toolbox-20-o', 'w-6 h-6 mr-2')
                         <div class="flex-grow">
                             <p>{{ $role->code }}-{{ $role->name }}</p>
                             <p class="flex text-nowrap text-sm items-center flex-wrap gap-1 text-neutral-600">
-                                {{ $role->users->count() }} usuarios asociados.
+                                @svg('fluentui-people-28-o', 'w-4 h-4') {{ $role->users->count() }} usuarios asociados.
                             </p>
                             <p class="text-sm">
                                 Dapartamento: {{ $role->department->name }}
@@ -41,7 +41,7 @@
                         <button type="button" data-modal-target="dialog-{{ $role->id }}"
                             data-modal-toggle="dialog-{{ $role->id }}"
                             class="rounded-full p-2 hover:bg-neutral-200 transition-colors">
-                            @svg('bx-pencil', 'w-4 h-4')
+                            @svg('fluentui-edit-20', 'w-4 h-4')
                         </button>
                         <div id="dialog-{{ $role->id }}" tabindex="-1" aria-hidden="true" class="dialog hidden">
                             <div class="content lg:max-w-lg max-w-full">
@@ -64,7 +64,7 @@
                         </div>
                         <button class="rounded-full p-2 hover:bg-neutral-200 transition-colors"
                             data-dropdown-toggle="dropdown-{{ $role->id }}">
-                            @svg('bx-dots-vertical-rounded', 'w-4 h-4')
+                            @svg('fluentui-more-horizontal-20-o', 'w-4 h-4')
                         </button>
                         <div id="dropdown-{{ $role->id }}" class="dropdown-content hidden">
                             <button data-atitle="¿Estás seguro de eliminar?"
