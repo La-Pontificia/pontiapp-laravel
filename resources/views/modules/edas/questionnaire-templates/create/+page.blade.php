674fa23f6760a85fb926ae76f514028fe77af973
Daustinn <daustinn.music@gmail.com>
2024-09-09 08:43:46 -0500

    Refactor layout of create questionnaire template page

--- resources/views/modules/edas/questionnaire-templates/create/+page.blade.php
+++ resources/views/modules/edas/questionnaire-templates/create/+page.blade.php
@@ -4,12 +4,14 @@
 
 @section('layout.edas')
     <div class="w-full max-w-2xl mx-auto">
-        <h2 class="py-5">
-            <a href="/edas/questionnaire-templates" class="flex items-center gap-2">
-                svg'bx-left-arrow-alt', 'w-5 h-5')
-                Crear nueva plantilla de cuestionarios.
-            </a>
-        </h2>
+        <div class="border-b pb-1">
+            <div class="flex items-center justify-between p-2">
+                <button onclick="window.history.back()" class="flex gap-2 items-center text-gray-900 ">
+                    @svg('fluentui-arrow-left-20', 'w-5 h-5')
+                    Crear nueva plantilla de cuestionarios.
+                </button>
+            </div>
+        </div>
         <div class="flex flex-col w-full bg-white border-neutral-300 shadow-[0_0_10px_rgba(0,0,0,.2)] border rounded-xl">
             <form id="template-form" class="flex flex-col px-1 h-full overflow-y-auto w-full" role="form">
                 @include('modules.edas.questionnaire-templates.form')
