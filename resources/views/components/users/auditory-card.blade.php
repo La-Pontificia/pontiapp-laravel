0fb1dc075d5a63bf79c2e123865060bcc2072cf3
Daustinn <daustinn.music@gmail.com>
2024-09-09 05:24:20 -0500

    Refactor user details in auditory card component

--- resources/views/components/users/auditory-card.blade.php
+++ resources/views/components/users/auditory-card.blade.php
@@ -1,14 +1,15 @@
 <div class="flex p-3 items-center gap-3">
     @include('commons.avatar', [
-        'src' => $current_user->profile,
+        'key' => $cuser->id,
+        'src' => $cuser->profile,
         'className' => 'w-12',
-        'alt' => $current_user->first_name . ' ' . $current_user->last_name,
+        'alt' => $cuser->first_name . ' ' . $cuser->last_name,
         'altClass' => 'text-lg',
     ])
     <div>
         <p class="text-sm">
-            {{ $current_user->last_name }},
-            {{ $current_user->first_name }}
+            {{ $cuser->last_name }},
+            {{ $cuser->first_name }}
         </p>
         <p class="text-xs text-neutral-500">
             Seguimiento y control con auditoria.
