012727a8d24b3b168be7ed5ed62cbe9b27fab92c
Daustinn <daustinn.music@gmail.com>
2024-09-09 08:44:21 -0500

    Refactor SVG icons and layout in create questionnaire template page

--- resources/views/modules/settings/job-positions/+page.blade.php
+++ resources/views/modules/settings/job-positions/+page.blade.php
@@ -7,7 +7,7 @@
         <h2 class="py-5">Puestos de trabajo.</h2>
         <div class="flex flex-col w-full bg-white border-neutral-300 shadow-[0_0_10px_rgba(0,0,0,.2)] border rounded-xl">
             <button type="button" data-modal-target="dialog" data-modal-toggle="dialog" class="primary m-2">
-                @svg('bx-plus', 'w-5 h-5')
+                @svg('fluentui-add-circle-16-o', 'w-5 h-5')
                 <span>Agregar nuevo puesto.</span>
             </button>
             <div id="dialog" tabindex="-1" aria-hidden="true" class="dialog hidden">
@@ -28,7 +28,7 @@
             <div class="flex flex-col divide-y">
                 @forelse ($jobs as $job)
                     <div class="flex relative hover:bg-neutral-100 items-center p-2.5 gap-2">
-                        @svg('bx-folder', 'w-5 h-5 mr-2')
+                        @svg('fluentui-toolbox-20-o', 'w-6 h-6 mr-2')
                         <div class="flex-grow">
                             <p>{{ $job->code }}-{{ $job->name }}</p>
                             <p class="flex text-nowrap text-sm items-center flex-wrap gap-1 text-neutral-600">
@@ -43,7 +43,7 @@
                         <button type="button" data-modal-target="dialog-{{ $job->id }}"
                             data-modal-toggle="dialog-{{ $job->id }}"
                             class="rounded-full p-2 hover:bg-neutral-200 transition-colors">
-                            @svg('bx-pencil', 'w-4 h-4')
+                            @svg('fluentui-edit-20', 'w-4 h-4')
                         </button>
                         <div id="dialog-{{ $job->id }}" tabindex="-1" aria-hidden="true" class="dialog hidden">
                             <div class="content lg:max-w-lg max-w-full">
@@ -66,7 +66,7 @@
                         </div>
                         <button class="rounded-full p-2 hover:bg-neutral-200 transition-colors"
                             data-dropdown-toggle="dropdown-{{ $job->id }}">
-                            @svg('bx-dots-vertical-rounded', 'w-4 h-4')
+                            @svg('fluentui-more-horizontal-20-o', 'w-4 h-4')
                         </button>
                         <div id="dropdown-{{ $job->id }}" class="dropdown-content hidden">
                             <button data-atitle="¿Estás seguro de eliminar?"
