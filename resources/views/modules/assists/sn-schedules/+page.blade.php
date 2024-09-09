771dacdf0a064228d0275206fcdd5c37d53dcbfe
Daustinn <daustinn.music@gmail.com>
2024-09-09 08:43:23 -0500

    Refactor assist filter form in single-summary page

--- resources/views/modules/assists/sn-schedules/+page.blade.php
+++ resources/views/modules/assists/sn-schedules/+page.blade.php
@@ -12,30 +12,45 @@
 @endphp
 
 @section('layout.assists')
-    <p class="pb-2">
-        Asistencias sin horario.
+    <p class="py-2 text-sm">
+        Asistencias sin horario. En este modo solo se muestran las marcaciones en las bases de datos de los terminales.
     </p>
     <div class="space-y-2 flex flex-col h-full overflow-auto">
         <div class="flex-grow dinamic-form-to-params flex items-center flex-wrap gap-4">
-            <form class="dinamic-form-to-params flex p-1 px-2 items-center gap-2 flex-wrap">
-                <input type="text" value="{{ $query }}" name="query" placeholder="DNI, Nombres, apellidos...">
+            <form class="dinamic-form-to-params p-1 px-2 flex items-end flex-grow gap-2 flex-wrap">
                 <div id="date-range" class="flex items-center gap-1">
-                    <input class="w-[100px]" readonly {{ $start ? "data-default=$start" : '' }} type="text"
-                        name="start" placeholder="-">
-                    <span>a</span>
-                    <input class="w-[100px]" readonly {{ $end ? "data-default=$end" : '' }} type="text" name="end"
-                        placeholder="-">
+                    <label for="">
+                        <span class="block">Desde:</span>
+                        <input class="w-[100px] bg-white" readonly {{ $start ? "data-default=$start" : '' }} type="text"
+                            name="start" placeholder="-">
+                    </label>
+                    <label for="">
+                        <span class="block">Hasta:</span>
+                        <input class="w-[100px] bg-white" readonly {{ $end ? "data-default=$end" : '' }} type="text"
+                            name="end" placeholder="-">
+                    </label>
                 </div>
-                <select name="terminal">
-                    @foreach ($terminals as $terminal)
-                        <option value="{{ $terminal->database_name }}"
-                            {{ $currentTerminal === $terminal->database_name ? 'selected' : '' }}>
-                            {{ $terminal->name }}
-                        </option>
-                    @endforeach
-                </select>
-                <button type="submit"
-                    class="p-2 rounded-xl bg-green-600 px-2 text-sm text-white shadow-sm font-semibold">Filtrar</button>
+                <div class="label">
+                    <span>
+                        Terminal
+                    </span>
+                    <select name="terminal" class="bg-white">
+                        @foreach ($terminals as $terminal)
+                            <option value="{{ $terminal->database_name }}"
+                                {{ $currentTerminal === $terminal->database_name ? 'selected' : '' }}>
+                                {{ $terminal->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <label class="relative ml-auto w-[200px] max-w-full">
+                    <div class="absolute inset-y-0 z-10 text-neutral-400 grid place-content-center left-2">
+                        @svg('fluentui-search-28-o', 'w-5 h-5')
+                    </div>
+                    <input value="{{ request()->get('query') }}" name="query" placeholder="Filtrar usuarios..."
+                        type="search" class="pl-9 w-full bg-white">
+                </label>
+                <button type="submit" class="primary mt-2">Filtrar</button>
             </form>
 
             {{-- <button {{ count($schedules) === 0 ? 'disabled' : '' }} data-dropdown-toggle="dropdown"
@@ -67,25 +82,38 @@
                     </div>
                 @else
                     <table id="table-export-assists" class="w-full text-left relative">
-                        <thead class="border-b sticky bg-white top-0 z-[1]">
-                            <tr class="[&>th]:text-nowrap [&>th]:font-normal [&>th]:p-2">
-                                <th class="tracking-tight">ID Empleado</th>
-                                <th class="tracking-tight w-full">Usuario</th>
-                                <th class="tracking-tight">Terminal</th>
+                        <thead class="border-b top-0 z-[1]">
+                            <tr class="[&>th]:text-nowrap [&>th]:font-medium [&>th]:p-2">
+                                <th class=" tracking-tight">Usuario</th>
                                 <th>Fecha</th>
-                                <th class="text-center bg-yellow-100">Hora</th>
+                                <th>Dia</th>
+                                <th>Terminal</th>
+                                <th class="bg-yellow-100 text-center">Hora</th>
                             </tr>
                         </thead>
                         <tbody class="divide-y z-[0]">
                             @foreach ($assists as $assist)
                                 <tr data-dni="{{ $assist->emp_code }}"
-                                    class="[&>td]:py-2 even:bg-neutral-100 [&>td>p]:text-nowrap relative group [&>td]:px-3">
+                                    class="[&>td]:py-3 even:bg-neutral-100 [&>td>p]:text-nowrap relative group [&>td]:px-3">
                                     <td>
-                                        {{ $assist->emp_code }}
+                                        <div>
+                                            <p class="text-nowrap">
+                                                {{ $assist->employee->first_name }} {{ $assist->employee->last_name }}
+                                            </p>
+                                            <p class="text-xs">
+                                                {{ $assist->emp_code }}
+                                            </p>
+                                        </div>
                                     </td>
                                     <td>
-                                        <p class="text-nowrap">
-                                            {{ $assist->employee->first_name }} {{ $assist->employee->last_name }}
+                                        <p class="text-nowrap flex items-center gap-2">
+                                            @svg('fluentui-calendar-ltr-20-o', 'w-5 h-5')
+                                            {{ $assist->punch_time->format('Y-m-d') }}
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p class="text-nowrap capitalize">
+                                            {{ $assist->punch_time->isoFormat('dddd') }}
                                         </p>
                                     </td>
                                     <td>
@@ -93,13 +121,8 @@
                                             {{ $assist->terminal_alias }}
                                         </p>
                                     </td>
-                                    <td>
-                                        <p class="text-nowrap">
-                                            {{ $assist->punch_time->format('Y-m-d') }}
-                                        </p>
-                                    </td>
                                     <td class="bg-yellow-100">
-                                        <p class="text-nowrap">
+                                        <p class="text-nowrap text-center">
                                             {{ $assist->punch_time->format('H:i:s') }}
                                         </p>
                                     </td>
