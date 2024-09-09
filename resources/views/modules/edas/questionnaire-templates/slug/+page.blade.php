2d6606ad01d51d8a63e0915f810a6cee9a53f39a
Daustinn <daustinn.music@gmail.com>
2024-09-09 08:43:50 -0500

    Refactor layout of create questionnaire template page

--- resources/views/modules/edas/questionnaire-templates/slug/+page.blade.php
+++ resources/views/modules/edas/questionnaire-templates/slug/+page.blade.php
@@ -5,12 +5,14 @@
 @section('layout.edas')
 
     <div class="w-full max-w-2xl mx-auto">
-        <h2 class="py-5">
-            <a href="/edas/questionnaire-templates" class="flex items-center gap-2">
-                svg'bx-left-arrow-alt', 'w-5 h-5')
-                Editar plantilla de cuestionarios: {{ $template->name }}
-            </a>
-        </h2>
+        <div class="border-b pb-1">
+            <div class="flex items-center justify-between p-2">
+                <button onclick="window.history.back()" class="flex gap-2 items-center text-gray-900 ">
+                    @svg('fluentui-arrow-left-20', 'w-5 h-5')
+                    Editar plantilla de cuestionarios: {{ $template->name }}
+                </button>
+            </div>
+        </div>
         <div class="flex flex-col w-full bg-white border-neutral-300 shadow-[0_0_10px_rgba(0,0,0,.2)] border rounded-xl">
             <form id="template-form" class="flex flex-col px-1 h-full overflow-y-auto w-full" role="form">
                 @include('modules.edas.questionnaire-templates.form')
@@ -25,30 +27,4 @@
             </div>
         </div>
     </div>
-
-    {{-- <div class="h-full bg-white relative w-full shadow-md flex flex-col border rounded-xl">
-        <nav class="border-b p-1 flex gap-3">
-            <button onclick="window.history.back()"
-                class="text-blue-500 hover:bg-white font-semibold justify-center min-w-max flex items-center rounded-md p-2 gap-1 text-sm px-2">
-                svg'heroicon-o-chevron-left', [
-                    'class' => 'w-5 h-5',
-                ])
-                <span class="max-lg:hidden">Editar plantilla</span>
-            </button>
-        </nav>
-        @include('components.users.auditory-card')
-        <form id="template-form" class="flex flex-col px-1 h-full overflow-y-auto w-full" role="form">
-            @include('modules.edas.questionnaire-templates.form')
-        </form>
-        <div class="p-3 px-4 border-t border-neutral-300 flex gap-2">
-            <button type="submit" form="template-form"
-                class="bg-blue-700 w-fit shadow-md shadow-blue-500/30 font-semibold hover:bg-blue-600 min-w-max flex items-center rounded-full p-1.5 gap-1 text-white text-sm px-3">
-                Actualizar
-            </button>
-            <button onclick="window.history.back()" type="button"
-                class="bg-white w-fit font-semibold hover:text-blue-600 text-black min-w-max flex items-center rounded-full p-1.5 gap-1 text-sm px-3">
-                Cancelar
-            </button>
-        </div>
-    </div> --}}
 @endsection
