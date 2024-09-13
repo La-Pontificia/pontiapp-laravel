b2619dd7fd62dd2b16d34db15e6e54cb6d20ea0c
Daustinn <daustinn.music@gmail.com>
2024-09-13 16:13:29 -0500

    Add server status display to assists layout

--- resources/views/modules/assists/+layout.blade.php
+++ resources/views/modules/assists/+layout.blade.php
@@ -3,5 +3,15 @@
 @section('title', 'Gestión de asistencias')
 
 @section('content')
-    @yield('layout.assists')
+    <div class="flex flex-col flex-grow h-full w-full">
+        @yield('layout.assists')
+        <div class="p-2">
+            <p class="text-xs">
+                Estado del servidor de asistencias: <span id="check-server"></span>
+            </p>
+            <p id="error-server" class="text-xs text-red-500">
+
+            </p>
+        </div>
+    </div>
 @endsection
