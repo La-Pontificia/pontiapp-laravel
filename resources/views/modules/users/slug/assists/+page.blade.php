08f8a96c948db962d2507a2d8af7f618925c1f01
Daustinn <daustinn.music@gmail.com>
2024-09-16 23:03:23 -0500

    Refactor assist summary page to optimize terminal selection logic

--- resources/views/modules/users/slug/assists/+page.blade.php
+++ resources/views/modules/users/slug/assists/+page.blade.php
@@ -5,15 +5,9 @@
 @php
     $start = request()->query('start') ? request()->query('start') : null;
     $end = request()->query('end') ? request()->query('end') : null;
-    $currentTerminal = null;
 
-    if (request()->query('terminal')) {
-        $currentTerminal = request()->query('terminal');
-    } elseif ($user->defaultTerminal) {
-        $currentTerminal = $user->defaultTerminal->database_name;
-    } else {
-        $currentTerminal = $terminals[0]->database_name;
-    }
+    $queryTerminals = explode(',', request()->query('assist_terminals'));
+    $terminalsInQuery = $queryTerminals ? $terminals->whereIn('id', $queryTerminals) : [];
 
 @endphp
 
@@ -41,19 +35,38 @@
                             <span>
                                 Terminal
                             </span>
-                            <select name="terminal" class="bg-white">
-                                @foreach ($terminals as $terminal)
-                                    <option value="{{ $terminal->database_name }}"
-                                        {{ $currentTerminal == $terminal->database_name ? 'selected' : '' }}>
-                                        {{ $terminal->name }}
-                                    </option>
-                                @endforeach
-                            </select>
+                            <button aria-hidden="true" id="terminal_button" data-dropdown-toggle="terminals"
+                                class="form-control flex items-center gap-2" type="button">
+                                Terminales
+                                @svg('fluentui-chevron-down-20-o', 'w-5 h-5')
+                            </button>
+
+                            <div id="terminals" class="z-10 hidden w-48 bg-white rounded-lg shadow">
+                                <div class="flex flex-col p-1">
+                                    @foreach ($terminals as $terminal)
+                                        @php
+                                            $checked = in_array($terminal->id, $queryTerminals);
+                                        @endphp
+                                        <label class="flex p-2 rounded-lg hover:bg-stone-100 items-center gap-2">
+                                            <input {{ $checked ? 'checked' : '' }} type="checkbox" name="assist_terminals[]"
+                                                value="{{ $terminal->id }}">
+                                            <div>
+                                                <span class="block"> {{ $terminal->name }} </span>
+                                                <p class="flex items-center gap-2">
+                                                    @svg('fluentui-task-list-square-database-20-o', 'w-5 h-5 opacity-70')
+                                                    <span class="text-sm font-normal"> {{ $terminal->database_name }}
+                                                    </span>
+                                                </p>
+                                            </div>
+                                        </label>
+                                    @endforeach
+                                </div>
+                            </div>
                         </div>
                         <button class="primary mt-5">Filtrar</button>
                         @if ($cuser->has('assists:export') || $cuser->isDev())
                             <button data-id="{{ $user->id }}" id="button-export-assists-peer-user" type="button"
-                                {{ count($schedules) === 0 ? 'disabled' : '' }} class="secondary ml-auto mt-5">
+                                {{ count($assists) === 0 ? 'disabled' : '' }} class="secondary ml-auto mt-5">
                                 @svg('fluentui-document-table-arrow-right-20-o', 'w-5 h-5')
                                 <span>
                                     Exportar
@@ -64,7 +77,7 @@
                 </div>
             </div>
             <div class="h-full w-full">
-                @if (count($schedules) === 0)
+                @if (count($assists) === 0)
                     <div class="grid h-full w-full place-content-center">
                         <img src="/empty-meetingList.webp" class="mx-auto" alt="">
                         <p class="text-center text-xs max-w-[40ch] mx-auto">
@@ -76,10 +89,10 @@
                         @php
                             $currentWeek = null;
                         @endphp
-                        @foreach ($schedules as $schedule)
+                        @foreach ($assists as $assist)
                             @php
-                                $from = \Carbon\Carbon::parse($schedule['from']);
-                                $date = \Carbon\Carbon::parse($schedule['date']);
+                                $from = \Carbon\Carbon::parse($assist['from']);
+                                $date = \Carbon\Carbon::parse($assist['date']);
                                 $weekNumber = $date->weekOfYear;
 
                                 $TTorTM = $from->hour >= 12 ? 'TT' : 'TM';
@@ -102,29 +115,37 @@
                                     <div class="flex items-center gap-2">
                                         @svg('fluentui-calendar-rtl-20-o', 'w-5 h-5 opacity-70')
                                         <p class="text-nowrap flex items-center gap-2">
-                                            {{ \Carbon\Carbon::parse($schedule['date'])->isoFormat('DD/MM/YYYY') }}
+                                            {{ \Carbon\Carbon::parse($assist['date'])->isoFormat('DD/MM/YYYY') }}
                                             •
-                                            {{ date('h:i A', strtotime($schedule['from'])) }} -
-                                            {{ date('h:i A', strtotime($schedule['to'])) }}
+                                            {{ date('h:i A', strtotime($assist['from'])) }} -
+                                            {{ date('h:i A', strtotime($assist['to'])) }}
                                         </p>
                                     </div>
                                     <div class="flex items-center gap-2">
                                         @svg('fluentui-person-clock-20-o', 'w-5 h-5 opacity-70')
                                         <p class="text-nowrap text-blue-600 flex items-center gap-2">
-                                            {{ $schedule['marked_in'] ? date('h:i A', strtotime($schedule['marked_in'])) : '' }}
+                                            {{ $assist['marked_in'] ? date('h:i A', strtotime($assist['marked_in'])) : '' }}
                                             -
-                                            {{ $schedule['marked_out'] ? date('h:i A', strtotime($schedule['marked_out'])) : '' }}
+                                            {{ $assist['marked_out'] ? date('h:i A', strtotime($assist['marked_out'])) : '' }}
                                         </p>
                                     </div>
                                 </div>
+                                <div>
+                                    <span class="block"> {{ $assist['terminal']->name }} </span>
+                                    <p class="flex items-center gap-2">
+                                        @svg('fluentui-task-list-square-database-20-o', 'w-5 h-5 opacity-70')
+                                        <span class="text-sm font-normal"> {{ $assist['terminal']->database_name }}
+                                        </span>
+                                    </p>
+                                </div>
                                 <div class="flex items-center gap-2">
                                     <p
-                                        class="text-sm {{ $schedule['observations'] == 'Tardanza' ? 'text-yellow-600' : ($schedule['observations'] == 'Completo' ? 'text-green-500' : '') }}">
-                                        {{ !$isFuture ? $schedule['observations'] : '' }}
+                                        class="text-sm {{ $assist['observations'] == 'Tardanza' ? 'text-yellow-600' : ($assist['observations'] == 'Completo' ? 'text-green-500' : '') }}">
+                                        {{ !$isFuture ? $assist['observations'] : '' }}
                                     </p>
-                                    @if (!$schedule['observations'])
+                                    @if (!$assist['observations'])
                                         @svg('fluentui-checkmark-circle-16', 'w-5 h-5 text-green-600')
-                                    @elseif ($schedule['observations'] == 'Tardanza')
+                                    @elseif ($assist['observations'] == 'Tardanza')
                                         @svg('fluentui-warning-16-o', 'w-5 h-5 text-yellow-600')
                                     @else
                                         @svg('fluentui-important-12-o', 'w-5 h-5 text-red-600')
