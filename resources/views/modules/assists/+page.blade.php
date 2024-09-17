ea9ea1de9f033e40813be92ab90dd8a53c489f35
Daustinn <daustinn.music@gmail.com>
2024-09-17 12:41:55 -0500

    Refactor terminal label layout in assists page

--- resources/views/modules/assists/+page.blade.php
+++ resources/views/modules/assists/+page.blade.php
@@ -221,10 +221,11 @@
                                     </td>
                                     <td>
                                         <div>
-                                            <span class="block"> {{ $assist['terminal']->name }} </span>
+                                            <span class="block text-nowrap"> {{ $assist['terminal']->name }} </span>
                                             <p class="flex items-center gap-2">
                                                 @svg('fluentui-task-list-square-database-20-o', 'w-5 h-5 opacity-70')
-                                                <span class="text-sm font-normal"> {{ $assist['terminal']->database_name }}
+                                                <span class="text-sm text-nowrap font-normal">
+                                                    {{ $assist['terminal']->database_name }}
                                                 </span>
                                             </p>
                                         </div>
