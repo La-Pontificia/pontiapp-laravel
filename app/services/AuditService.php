92505da7874216b3ee0e95cdfd80ec68b62ef68e
Daustinn <daustinn.music@gmail.com>
2024-09-26 15:46:02 -0500

    refactor AuditService to use Audit model for creating audit records

---
+++ app/services/AuditService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\services;
+
+use App\Models\Audit;
+use Jenssegers\Agent\Agent;
+use Stevebauman\Location\Facades\Location;
+
+class AuditService
+{
+
+    public static function registerAudit($title, $description, $module, $action, $request)
+    {
+        $agent = new Agent();
+        $location = Location::get($request->ip());
+        Audit::create([
+            'user_id' => auth()->id(),
+            'module' => $module,
+            'title' => $title,
+            'description' => $description,
+            'path' => $request->path(),
+            'os' => $agent->platform(),
+            'ip' => $request->ip(),
+            'platform' => $agent->browser(),
+            'device' => $agent->device(),
+            'browser' => $agent->browser(),
+            'country' => $location ? $location->countryName : null,
+            'region' => $location ? $location->regionName : null,
+            'city' => $location ? $location->cityName : null,
+            'action' => $action
+        ]);
+    }
+}
