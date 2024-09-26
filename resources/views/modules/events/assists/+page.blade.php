124c8fc0f71e1b43624df503897632a06b9c2cac
Daustinn <daustinn.music@gmail.com>
2024-09-26 15:46:23 -0500

    refactor: Add assists page view template

---
+++ resources/views/modules/events/assists/+page.blade.php
@@ -0,0 +1,9 @@
+@extends('modules.events.+layout')
+
+@section('title', 'Gestión Eventos')
+
+@section('layout.events')
+    <div class="w-full mx-auto">
+        <h2 class="pt-5">Asistencias a eventos.</h2>
+    </div>
+@endsection
