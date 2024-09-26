9e4c248acaf3df1ff3a1bc010e181d818eaf8233
Daustinn <daustinn.music@gmail.com>
2024-09-26 15:45:08 -0500

    refactor AuditController.php to use AuditService for auditing

---
+++ app/Http/Controllers/AuditController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Audit;
+use App\services\AuditService;
+use Illuminate\Http\Request;
+
+class AuditController extends Controller
+{
+    protected $auditService;
+
+    public function __construct(AuditService $auditService)
+    {
+        $this->auditService = $auditService;
+    }
+
+    public function index(Request $request)
+    {
+
+        $match = Audit::orderBy('created_at', 'desc')->limit(20);
+
+        $query = $request->get('query');
+        $action = $request->get('action');
+        $module = $request->get('module');
+
+        if ($query) {
+            $match->whereHas('user', function ($q) use ($query) {
+                $q->where('first_name', 'like', '%' . $query . '%')
+                    ->orWhere('last_name', 'like', '%' . $query . '%')
+                    ->orWhere('dni', 'like', '%' . $query . '%');
+            })->orWhere('description', 'like', '%' . $query . '%')
+                ->orWhere('module', 'like', '%' . $query . '%')
+                ->orWhere('action', 'like', '%' . $query . '%')
+                ->orWhere('title', 'like', '%' . $query . '%');
+        }
+
+        if ($action) {
+            $match->where('action', $action);
+        }
+
+        if ($module) {
+            $match->where('module', $module);
+        }
+
+        $records = $match->paginate();
+
+        return view('modules.audit.+page', compact('records'))->with('i', (request()->input('page', 1) - 1) * $records->perPage());
+    }
+
+    public function delete($id)
+    {
+        $audit = Audit::find($id);
+        $audit->delete();
+
+        $this->auditService->registerAudit('Registro eliminado', 'Se ha eliminado un registro', 'audits', 'delete', request());
+
+        return response()->json('Registro eliminado correctamente');
+    }
+}
