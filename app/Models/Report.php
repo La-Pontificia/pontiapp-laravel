80427cfe2d03a448dc7aadafe1b841d385817cf8
Daustinn <daustinn.music@gmail.com>
2024-10-02 16:21:21 -0500

    refactor: Add Report model for managing reports in the application

---
+++ app/Models/Report.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'reports';
+
+
+    protected $perPage = 25;
+
+    protected $fillable = ['id', 'title', 'file_url', 'download_link', 'generated_by'];
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    public function generatedBy()
+    {
+        return $this->hasOne(User::class, 'id',  'generated_by');
+    }
+}
