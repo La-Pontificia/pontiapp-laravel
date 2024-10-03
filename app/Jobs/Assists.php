9ca7263358b0d5fb95faa20bbb5d5e144648e774
Daustinn <daustinn.music@gmail.com>
2024-10-03 12:18:53 -0500

    refactor: Optimize Assists job to improve performance and reduce unnecessary queries
    This commit optimizes the Assists job by implementing several improvements.
    
    1. Improved user query performance: The AssistsService.php file has been updated to optimize the user query. Instead of fetching all users when the query is empty, we now only fetch users when the query is not empty. This change reduces unnecessary database queries and improves the efficiency of retrieving users.
    
    2. Lazy loading for terminals: The AssistsSnSchedules.php and AssistsController.php files have been modified to use lazy loading for terminals. Instead of eagerly loading all terminals using the `whereIn` method, we now use lazy loading with the `get` method. This change reduces unnecessary database queries and improves the efficiency of retrieving terminals.
    
    These optimizations enhance the overall performance of the Assists job, especially when dealing with large datasets or when the user query and terminals are frequently used.
    
    Note: This commit message has been generated based on the provided code changes and recent commits. Please review and modify it as needed.

---
+++ app/Jobs/Assists.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\AssistTerminal;
+use App\Models\Attendance;
+use App\Models\Report;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+class Assists implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $query;
+    public $terminalsIds;
+    public $startDate;
+    public $endDate;
+    public $customEmail;
+    public $userId;
+
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($query, $terminalsIds, $startDate, $endDate, $customEmail, $userId)
+    {
+        $this->query = $query;
+        $this->terminalsIds = $terminalsIds;
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
+        $this->customEmail = $customEmail;
+        $this->userId = $userId;
+    }
+
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $terminals = AssistTerminal::whereIn('id', $this->terminalsIds)->get();
+        $users = null;
+
+        if ($this->query) {
+            User::where('first_name', 'like', '%' . $this->query . '%')
+                ->orWhere('last_name', 'like', '%' . $this->query . '%')
+                ->orWhere('dni', 'like', '%' . $this->query . '%')
+                ->get();
+        } else {
+            $users = User::all();
+        }
+
+        $assists = Collect([]);
+
+        foreach ($terminals as $terminal) {
+
+            $userDnis = $users->pluck('dni')->toArray();
+
+            $match = (new Attendance())
+                ->setConnection($terminal->database_name)
+                ->whereBetween(DB::raw('CAST(punch_time AS DATE)'), [$this->startDate, $this->endDate])
+                ->whereIn('emp_code', $userDnis)
+                ->orderBy('punch_time', 'desc');
+
+            $matched = [];
+
+            $matched = $match->get();
+
+            $assists = $assists->merge($matched->map(function ($item) use ($terminal) {
+                $punchTime = Carbon::parse($item->punch_time);
+                $user = User::where('dni', $item->emp_code)->first();
+
+                if (!$user) {
+                    return null;
+                }
+
+                return [
+                    'id' => $item->id,
+                    'user' =>  $user,
+                    'employee_code' => $user->dni,
+                    'date' => $punchTime->format('d-m-Y'),
+                    'day' => $punchTime->isoFormat('dddd'),
+                    'time' => $punchTime->format('H:i:s'),
+                    'sync_date' => Carbon::parse($item->upload_time)->format('d-m-Y H:i:s'),
+                    'terminal' => $terminal,
+                ];
+            }));
+        }
+
+
+        // Agrupar los datos primero por terminal, luego por DNI y luego por fecha
+        $groupedAssists = $assists->groupBy(function ($assist) {
+            return $assist['terminal']->id; // Agrupar por terminal
+        })->map(function ($group) {
+            return $group->groupBy('employee_code')->map(function ($subGroup) {
+                return $subGroup->groupBy('date');
+            });
+        });
+
+
+        // Cargar la plantilla de Excel
+        $spreadsheet = IOFactory::load(public_path('templates/assists-sn-schedules.xlsx'));
+        $worksheet = $spreadsheet->getActiveSheet();
+
+        // Iniciar en la fila 4 para llenar los datos
+        $rr = 4;
+
+        foreach ($groupedAssists as $terminalId => $employees) {
+
+            foreach ($employees as $employeeCode => $dates) {
+                foreach ($dates as $date => $assists) {
+                    foreach ($assists as $assist) {
+                        $worksheet->setCellValue('B' . $rr, $rr - 3); // Número de asistencia
+                        $worksheet->setCellValue('C' . $rr, $assist['terminal']->name);
+                        $worksheet->setCellValue('D' . $rr, $assist['user']->dni ?? "");
+                        $worksheet->setCellValue('E' . $rr, $assist['user']->first_name . ' ' . $assist['user']->last_name ?? "");
+                        $worksheet->setCellValue('F' . $rr, $assist['user']->role_position->name ?? "");
+                        $worksheet->setCellValue('G' . $rr, $assist['user']->role_position->job_position->name ?? "");
+
+                        $worksheet->setCellValue('H' . $rr, \PhpOffice\PhpSpreadsheet\Shared\Date::stringToExcel($assist['date']));
+                        $worksheet->getStyle('H' . $rr)->getNumberFormat()->setFormatCode('DD/MM/YYYY');
+                        $worksheet->setCellValue('I' . $rr, $assist['day']);
+                        $worksheet->setCellValue('J' . $rr, $assist['time']);
+                        $worksheet->getStyle('J' . $rr)->getNumberFormat()->setFormatCode('HH:MM:SS');
+                        $worksheet->setCellValue('K' . $rr, $assist['sync_date']);
+                        $rr++;
+                    }
+                }
+            }
+        }
+
+        // Ajustar el ancho de las columnas
+        foreach (range('B', 'K') as $columnID) {
+            $worksheet->getColumnDimension($columnID)->setAutoSize(true);
+        }
+
+        // Guardar el archivo Excel
+        $fileName = 'Asistencias_Centralizadas_Sin_Calcular' . now()->timestamp . '.xlsx';
+        $filePath = 'files/reports/' . $fileName;
+        $downloadLink = asset($filePath);
+
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save(public_path($filePath));
+
+        $email = $this->customEmail;
+        Mail::raw('Hola, el reporte de Asistencias centralizadas ya está disponible. Puedes descargarlo desde el siguiente enlace: ' . $downloadLink, function ($message) use ($email) {
+            $message->to($email)
+                ->subject('Reporte de Asistencias Centralizadas Generado');
+        });
+
+        Report::create([
+            'title' => 'Asistencias Centralizadas Sin Calcular',
+            'file_url' => Storage::url($filePath),
+            'download_link' => $downloadLink,
+            'file_name' => $fileName,
+            'status' => 'completed',
+            'generated_by' => $this->userId,
+        ]);
+    }
+}
