d3c20b69e228a36ce00a12de02db0fb3d29e716d
Daustinn <daustinn.music@gmail.com>
2024-10-15 00:56:04 -0500

    Refactor layout and functionality for search module

---
+++ resources/views/modules/search.blade.php
@@ -0,0 +1,11 @@
+<div class="max-md:hidden block">
+    <div class="max-md:absolute">
+        <label class="relative block">
+            <div class="absolute inset-y-0 z-[1] flex items-center px-2 pointer-events-none">
+                @svg('fluentui-search-28-o', 'w-5 h-5 text-blue-600')
+            </div>
+            <input type="search" placeholder="Buscar" style="border: 0"
+                class="rounded-full w-[400px] pl-9 max-w-full drop-shadow-[0_1px_1px_rgba(0,0,0,.20)] hover:shadow-[0_1px_5px_rgba(0,0,0,.10)] border">
+        </label>
+    </div>
+</div>
