11c5401ba3817098341fb6027bb0f8d4031527c3
Daustinn <daustinn.music@gmail.com>
2024-10-15 00:55:50 -0500

    Refactor layout and functionality for real-time ticket management page

---
+++ resources/views/modules/tickets/+page.blade.php
@@ -0,0 +1,13 @@
+@extends('modules.tickets.+layout')
+
+@section('title', 'Gestión Tickets')
+
+@section('layout.tickets')
+    <div class="w-full flex flex-col flex-grow">
+        <nav class="px-2">
+            <h1 class="font-semibold text-lg">
+                Gestión Tickets
+            </h1>
+        </nav>
+    </div>
+@endsection
