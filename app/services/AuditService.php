fd41cb0a896b702f8f7ef9ce97e01527b902a19c
Daustinn <daustinn.music@gmail.com>
2024-11-19 02:41:56 -0500

    Refactor AuditService to use a shorter variable name for the request parameter

--- app/services/AuditService.php
+++ app/services/AuditService.php
@@ -10,18 +10,18 @@ use Stevebauman\Location\Facades\Location;
 class AuditService
 {
 
-    public static function registerAudit($title, $description, $module, $action, $request)
+    public static function registerAudit($title, $description, $module, $action, $req)
     {
         $agent = new Agent();
-        $location = Location::get($request->ip());
+        $location = Location::get($req->ip());
         Audit::create([
             'user_id' => Auth::id(),
             'module' => $module,
             'title' => $title,
             'description' => $description,
-            'path' => $request->path(),
+            'path' => $req->path(),
             'os' => $agent->platform(),
-            'ip' => $request->ip(),
+            'ip' => $req->ip(),
             'platform' => $agent->browser(),
             'device' => $agent->device(),
             'browser' => $agent->browser(),
