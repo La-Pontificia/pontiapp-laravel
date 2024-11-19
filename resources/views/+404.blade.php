eabce057f508efc5f3246d258823d77af7b67e49
Daustinn <daustinn.music@gmail.com>
2024-11-19 02:44:29 -0500

    Refactor 404 page layout and content

--- resources/views/+404.blade.php
+++ resources/views/+404.blade.php
@@ -3,21 +3,18 @@
 @section('title', '404')
 
 @section('app')
-    <div class="flex flex-col h-svh bg-gradient-to-tr text-black from-yellow-400 via-rose-400 to-violet-400 ">
-        <div class="w-full grid h-full place-content-center">
-            <div class="text-center space-y-3">
-                <h2 class="text-2xl font-bold">
-                    404 | Pagina no encontrada
-                </h2>
-                <p class="text-sm mx-auto max-w-[35ch] text-black">
-                    La página no existe o ha sido eliminada, por favor intenta de nuevo
-                    o volver al <a href="/" class="font-bold hover:underline">inicio</a>.
-                </p>
-            </div>
+    <div class="flex flex-col h-svh bg-black text-white">
+        <div class="flex-grow text-center grid place-content-center">
+            <h2>
+                <span class="text-5xl font-semibold">404</span>
+                <br>
+                <span class="text-sm">The page doesn't exist.</span>
+            </h2>
+            <a href="/" class="mt-4 bg-white text-black p-2 rounded-md px-3 text-sm font-medium">Volver al inicio</a>
         </div>
-        <footer class="p-4 text-center text-xs text-black">
-            Desarrollado por <a href="https://daustinn.com" target="_blank" rel="noopener noreferrer"
-                class=" font-semibold hover:underline">Daustinn</a> &copy; 2024
+        <footer class="p-4 text-center text-xs text-white/80">
+            developed by <a href="https://daustinn.com" target="_blank" rel="noopener noreferrer"
+                class=" font-semibold hover:underline">Daustinn</a> &copy; {{ date('Y') }}
         </footer>
     </div>
 @endsection
