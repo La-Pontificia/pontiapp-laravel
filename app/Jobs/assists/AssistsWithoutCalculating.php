4c6f7040e666a8d0ab38e5eab1ecf8c3c88bb064
Daustinn <daustinn.music@gmail.com>
2024-11-19 02:40:53 -0500

    Refactor job classes and move them to appropriate directories

--- app/Jobs/assists/AssistsWithoutCalculating.php
+++ app/Jobs/assists/AssistsWithoutCalculating.php
@@ -3,9 +3,7 @@
 namespace App\Jobs;
 
 use App\Models\AssistTerminal;
-use App\Models\Attendance;
 use App\Models\Report;
-use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -16,7 +14,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-class AssistsSnSchedules implements ShouldQueue
+class AssistsWithoutCalculating implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
@@ -24,21 +22,20 @@ class AssistsSnSchedules implements ShouldQueue
     public $terminalsIds;
     public $startDate;
     public $endDate;
-    public $customEmail;
     public $userId;
-
+    public $customEmail;
 
     /**
      * Create a new job instance.
      */
-    public function __construct($query, $terminalsIds, $startDate, $endDate, $customEmail, $userId)
+    public function __construct($query, $terminalsIds, $startDate, $endDate, $userId, $customEmail)
     {
         $this->query = $query;
         $this->terminalsIds = $terminalsIds;
         $this->startDate = $startDate;
         $this->endDate = $endDate;
-        $this->customEmail = $customEmail;
         $this->userId = $userId;
+        $this->customEmail = $customEmail;
     }
 
 
@@ -48,52 +45,42 @@ class AssistsSnSchedules implements ShouldQueue
     public function handle(): void
     {
         $terminals = AssistTerminal::whereIn('id', $this->terminalsIds)->get();
-        $users = Collect([]);
 
-        if ($this->query) {
-            $users = User::where('first_name', 'like', '%' . $this->query . '%')
-                ->orWhere('last_name', 'like', '%' . $this->query . '%')
-                ->orWhere('dni', 'like', '%' . $this->query . '%')
-                ->get();
-        } else {
-            $users = User::all();
-        }
-
-        $assists = Collect([]);
+        $assists = collect();
 
         foreach ($terminals as $terminal) {
 
-            $userDnis = $users->pluck('dni')->toArray();
+            $attendanceQuery = DB::connection($terminal->database_name)
+                ->table('iclock_transaction as it')
+                ->join('personnel_employee as pe', 'it.emp_code', '=', 'pe.emp_code')
+                ->select('it.id', 'it.punch_time', 'it.upload_time', 'pe.emp_code', 'pe.first_name', 'pe.last_name')
+                ->whereBetween(DB::raw('CAST(it.punch_time AS DATE)'), [$this->startDate, $this->endDate])
+                ->orderBy('it.punch_time', 'desc');
 
-            $match = (new Attendance())
-                ->setConnection($terminal->database_name)
-                ->whereBetween(DB::raw('CAST(punch_time AS DATE)'), [$this->startDate, $this->endDate])
-                ->whereIn('emp_code', $userDnis)
-                ->orderBy('punch_time', 'desc');
+            if (!empty($this->query)) {
+                $attendanceQuery->where(function ($q) {
+                    $q->where('pe.first_name', 'like', '%' . $this->query . '%')
+                        ->orWhere('pe.last_name', 'like', '%' . $this->query . '%')
+                        ->orWhere('pe.emp_code', 'like', '%' . $this->query . '%');
+                });
+            }
 
-            $matched = $match->get();
+            $matched = $attendanceQuery->get();
 
             $assists = $assists->merge($matched->map(function ($item) use ($terminal) {
                 $punchTime = Carbon::parse($item->punch_time);
-                $user = User::where('dni', $item->emp_code)->first();
-
-                if (!$user) {
-                    return null;
-                }
-
                 return [
                     'id' => $item->id,
-                    'user' =>  $user,
-                    'employee_code' => $user->dni,
                     'date' => $punchTime->format('d-m-Y'),
                     'day' => $punchTime->isoFormat('dddd'),
+                    'employee_code' => $item->emp_code,
+                    'employee_name' => $item->first_name . ' ' . $item->last_name,
                     'time' => $punchTime->format('H:i:s'),
                     'sync_date' => Carbon::parse($item->upload_time)->format('d-m-Y H:i:s'),
                     'terminal' => $terminal,
                 ];
             }));
         }
-
 
         // Agrupar los datos primero por terminal, luego por DNI y luego por fecha
         $groupedAssists = $assists->groupBy(function ($assist) {
@@ -104,9 +91,8 @@ class AssistsSnSchedules implements ShouldQueue
             });
         });
 
-
         // Cargar la plantilla de Excel
-        $spreadsheet = IOFactory::load(public_path('templates/assists-sn-schedules.xlsx'));
+        $spreadsheet = IOFactory::load(public_path('templates/without-calculating.xlsx'));
         $worksheet = $spreadsheet->getActiveSheet();
 
         // Iniciar en la fila 4 para llenar los datos
@@ -119,17 +105,14 @@ class AssistsSnSchedules implements ShouldQueue
                     foreach ($assists as $assist) {
                         $worksheet->setCellValue('B' . $rr, $rr - 3); // Número de asistencia
                         $worksheet->setCellValue('C' . $rr, $assist['terminal']->name);
-                        $worksheet->setCellValue('D' . $rr, $assist['user']->dni ?? "");
-                        $worksheet->setCellValue('E' . $rr, $assist['user']->first_name . ' ' . $assist['user']->last_name ?? "");
-                        $worksheet->setCellValue('F' . $rr, $assist['user']->role_position->name ?? "");
-                        $worksheet->setCellValue('G' . $rr, $assist['user']->role_position->job_position->name ?? "");
-
-                        $worksheet->setCellValue('H' . $rr, \PhpOffice\PhpSpreadsheet\Shared\Date::stringToExcel($assist['date']));
-                        $worksheet->getStyle('H' . $rr)->getNumberFormat()->setFormatCode('DD/MM/YYYY');
-                        $worksheet->setCellValue('I' . $rr, $assist['day']);
-                        $worksheet->setCellValue('J' . $rr, $assist['time']);
-                        $worksheet->getStyle('J' . $rr)->getNumberFormat()->setFormatCode('HH:MM:SS');
-                        $worksheet->setCellValue('K' . $rr, $assist['sync_date']);
+                        $worksheet->setCellValue('D' . $rr, $assist['employee_code'] ?? "");
+                        $worksheet->setCellValue('E' . $rr, $assist['employee_name'] ?? "");
+                        $worksheet->setCellValue('F' . $rr, \PhpOffice\PhpSpreadsheet\Shared\Date::stringToExcel($assist['date']));
+                        $worksheet->getStyle('F' . $rr)->getNumberFormat()->setFormatCode('DD/MM/YYYY');
+                        $worksheet->setCellValue('G' . $rr, $assist['day']);
+                        $worksheet->setCellValue('H' . $rr, $assist['time']);
+                        $worksheet->getStyle('H' . $rr)->getNumberFormat()->setFormatCode('HH:MM:SS');
+                        $worksheet->setCellValue('I' . $rr, $assist['sync_date']);
                         $rr++;
                     }
                 }
@@ -137,7 +120,7 @@ class AssistsSnSchedules implements ShouldQueue
         }
 
         // Ajustar el ancho de las columnas
-        foreach (range('B', 'K') as $columnID) {
+        foreach (range('B', 'I') as $columnID) {
             $worksheet->getColumnDimension($columnID)->setAutoSize(true);
         }
 
@@ -150,13 +133,14 @@ class AssistsSnSchedules implements ShouldQueue
         $writer->save(public_path($filePath));
 
         $email = $this->customEmail;
-        Mail::raw('Hola, el reporte de Asistencias centralizadas ya está disponible. Puedes descargarlo desde el siguiente enlace: ' . $downloadLink, function ($message) use ($email) {
+        Mail::raw('Hola, el reporte de Asistencias ya está disponible. Puedes descargarlo desde el siguiente enlace: ' . $downloadLink, function ($message) use ($email) {
             $message->to($email)
-                ->subject('Reporte de Asistencias Centralizadas Generado');
+                ->subject('Reporte de Asistencias Generado');
         });
 
+
         Report::create([
-            'title' => 'Asistencias Centralizadas Sin Calcular',
+            'title' => 'Asistencias sin calcular',
             'file_url' => $filePath,
             'download_link' => $downloadLink,
             'ext' => 'xlsx',
