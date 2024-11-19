f512c4da1ad5e002347467868e78b1eff752b0e2
Daustinn <daustinn.music@gmail.com>
2024-11-19 13:54:48 -0500

    Add CheckBirthdays middleware to handle birthday modals and cookies

---
+++ app/Http/Middleware/CheckBirthdays.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Closure;
+use Symfony\Component\HttpFoundation\Cookie;
+
+class CheckBirthdays
+{
+    public function handle($request, Closure $next)
+    {
+
+        $dateKey = 'dateBirthdayModal';
+        $showedKey = 'showedBirthdayModal';
+
+        $showedBirthdayModal = $request->cookie($showedKey);
+        $currentDateCookie = $request->cookie($dateKey);
+
+        $today = Carbon::today()->format('Y-m-d');
+        $todayFormatted = Carbon::today()->format('m-d');
+        $showedToday = $showedBirthdayModal == 'true' && $currentDateCookie == $today;
+        $birthdays = User::whereRaw("DATE_FORMAT(date_of_birth, '%m-%d') = ?", [$todayFormatted])->get();
+        $request->attributes->set('birthdayUsers', $birthdays);
+
+        if ($showedToday) {
+            $request->attributes->set('showBirthdayModal', false);
+            return $next($request);
+        }
+
+        if (!$showedToday && $birthdays->isNotEmpty()) {
+            $request->attributes->set('showBirthdayModal', true);
+            $response = $next($request);
+            $response->headers->setCookie(new Cookie($showedKey, 'false', time() + 86400, '/', null, false, false));
+            $response->headers->setCookie(new Cookie($dateKey, $today, time() + 86400, '/', null, false, false));
+
+            return $response;
+        }
+
+        return $next($request);
+    }
+}
