5b1dc0caafc923933924f647cd45ced734287717
Daustinn <daustinn.music@gmail.com>
2024-12-17 15:14:01 -0500

    Refactor routes to remove unused code

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -6,12 +6,9 @@ use App\Http\Controllers\Api\Auth\SanctumController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-    Route::middleware('web')->group(function () {
-        Route::get('login/id', [AzureController::class, 'login']);
-        Route::get('azure/callback', [AzureController::class, 'callback']);
-
-        Route::post('login/credentials', [AuthController::class, 'credentials']);
-    });
+    Route::get('login/id', [AzureController::class, 'login']);
+    Route::get('azure/callback', [AzureController::class, 'callback']);
+    Route::post('login/credentials', [AuthController::class, 'credentials']);
     Route::get('current', [AuthController::class, 'current']);
     Route::post('changePassword', [AuthController::class, 'changePassword']);
     Route::post('signout', [AuthController::class, 'signOut']);
