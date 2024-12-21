72b36435d7425fa1e307696e1bb125466ab1c8f3
Daustinn <daustinn.music@gmail.com>
2024-12-21 13:27:03 -0500

    Add maintenance page view

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.headers')
+
+@section('title', 'Maintenance')
+
+@section('app')
+    <div class="h-screen w-full bg-stone-900 grid text-center place-content-center text-stone-100">
+        <div class="max-w-md mx-auto w-full">
+            <h1 class="text-xl tracking-tight">
+                Ponti App en mantenimiento
+            </h1>
+            <p class="text-xs opacity-70">
+                Estamos actualizando la aplicación, por favor vuelve más tarde.
+            </p>
+        </div>
+    </div>
+@endsection
