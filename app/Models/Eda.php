fed64a628c9fa9710f417e71cfc46cf6793fc0b6
Daustinn <daustinn.music@gmail.com>
2025-01-22 13:34:09 -0500

    Refactor Eda model: Update fillable properties and remove unused relationships

--- app/Models/Eda.php
+++ app/Models/Eda.php
@@ -14,88 +14,18 @@ class Eda extends Model
   protected $perPage = 20;
 
   protected $fillable = [
-    'id',
-    'id_year',
-    'id_user',
-    'closed',
-    'send',
-    'approved',
-    'created_at',
-    'updated_at',
-    'created_by',
-    'approved_by',
-    'closed_by',
-    'sent_by',
+    'userId',
+    'approvedId',
+    'closedId',
+    'sentId',
+    'status',
+    'approvedAt',
+    'closedAt',
+    'sentAt',
+    'year',
+    'creatorId'
   ];
-
-  static $rules = [
-    'id_year' => ['required', 'string', 'max:36'],
-    'id_user' => ['required', 'string', 'max:36'],
-  ];
-
 
   protected $keyType = 'string';
-
   public $incrementing = false;
-
-  public function collaboratorQuestionnaire()
-  {
-    return $this->hasOne(Questionnaire::class, 'eda_id', 'id')
-      ->whereHas('template', function ($q) {
-        $q->where('use_for', 'collaborators');
-      });
-  }
-
-  public function supervisorQuestionnaire()
-  {
-    return $this->hasOne(Questionnaire::class, 'eda_id', 'id')
-      ->whereHas('template', function ($q) {
-        $q->where('use_for', 'supervisors');
-      });
-  }
-
-  public function questionnaires()
-  {
-    return $this->hasMany(Questionnaire::class, 'eda_id', 'id');
-  }
-
-  public function user()
-  {
-    return $this->hasOne(User::class, 'id', 'id_user');
-  }
-
-  public function goals()
-  {
-    return $this->hasMany(Goal::class, 'id_eda', 'id');
-  }
-
-  public function createdBy()
-  {
-    return $this->hasOne(User::class, 'id', 'created_by');
-  }
-
-  public function approvedBy()
-  {
-    return $this->hasOne(User::class, 'id', 'approved_by');
-  }
-
-  public function closedBy()
-  {
-    return $this->hasOne(User::class, 'id', 'closed_by');
-  }
-
-  public function sentBy()
-  {
-    return $this->hasOne(User::class, 'id', 'sent_by');
-  }
-
-  public function year()
-  {
-    return $this->hasOne(Year::class, 'id', 'id_year');
-  }
-
-  public function evaluations()
-  {
-    return $this->hasMany(Evaluation::class, 'id_eda', 'id');
-  }
 }
