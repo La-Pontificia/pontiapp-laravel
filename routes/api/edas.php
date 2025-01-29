510181552133246ad12bef7f6e13dc5984a7996f
Daustinn <daustinn.music@gmail.com>
2025-01-29 05:09:02 -0500

    Refactor EDAS API routes: Add new controllers for collaborators, years, evaluations, and objectives

--- routes/api/edas.php
+++ routes/api/edas.php
@@ -1,10 +1,45 @@
 <?php
 
+use App\Http\Controllers\Api\Eda\CollaboratorController;
 use App\Http\Controllers\Api\Eda\EdaController;
+use App\Http\Controllers\Api\Eda\EvaluationController;
+use App\Http\Controllers\Api\Eda\ObjetiveController;
+use App\Http\Controllers\Api\Eda\YearController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('check.auth')->group(function () {
     Route::prefix('edas')->group(function () {
-        Route::get('collaborators', [EdaController::class, 'collaborators']);
+        Route::prefix('collaborators')->group(function () {
+            Route::get('', [CollaboratorController::class, 'index']);
+            Route::get('{slug}', [CollaboratorController::class, 'slug']);
+        });
+        Route::prefix('years')->group(function () {
+            Route::get('', [YearController::class, 'index']);
+        });
+
+        Route::prefix('{slug}')->group(function () {
+            Route::get('', [EdaController::class, 'slug']);
+            Route::post('approve', [EdaController::class, 'approve']);
+            Route::post('close', [EdaController::class, 'close']);
+
+            Route::prefix('objetives')->group(function () {
+                Route::get('', [ObjetiveController::class, 'byEda']);
+                Route::post('store', [ObjetiveController::class, 'storeByEda']);
+            });
+        });
+
+        Route::prefix('evaluations')->group(function () {
+            Route::get('', [EvaluationController::class, 'index']);
+
+            Route::prefix('{slug}')->group(function () {
+                Route::get('', [EvaluationController::class, 'slug']);
+                Route::post('qualify', [EvaluationController::class, 'qualify']);
+                Route::post('selftQualify', [EvaluationController::class, 'selftQualify']);
+                Route::post('close', [EvaluationController::class, 'close']);
+            });
+        });
+
+        Route::get('{slugCollaborator}/{slugYear}', [EdaController::class, 'edaByCollaboratorAndYear']);
+        Route::post('{slugCollaborator}/{slugYear}/create', [EdaController::class, 'createEdaByCollaboratorAndYear']);
     });
 });
