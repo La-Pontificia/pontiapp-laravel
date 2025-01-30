191893386bf18b71edffdf04d9fd8b6247e577b1
Daustinn <daustinn.music@gmail.com>
2025-01-30 11:00:16 -0500

    Refactor email template for sending invitations to EDAs

--- resources/views/emails/invitationToEda.blade.php
+++ resources/views/emails/invitationToEda.blade.php
@@ -14,23 +14,11 @@
 <body
     style='background-color:#ffffff;font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif'>
     <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation"
-        style="max-width:100%;margin:0 auto;padding:20px 0 48px;width:580px">
+        style="max-width:100%;margin:0 auto;padding:10px 10px;width:600px">
         <tbody>
             <tr style="width:100%">
                 <td>
-                    <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0"
-                        role="presentation">
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <img alt="La Pontificia" height="30"
-                                        src="http://res.cloudinary.com/dc0t90ahb/image/upload/v1738211374/pontiapp/qjrx3wnvimjuct523xb2.png"
-                                        style="display:block;outline:none;border:none;text-decoration:none"
-                                        width="96" />
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+
                     <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0"
                         role="presentation" style="padding-bottom:20px">
                         <tbody>
@@ -40,22 +28,17 @@
                                         role="presentation">
                                         <tbody style="width:100%">
                                             <tr style="width:100%">
-                                                <p
-                                                    style="font-size:32px;line-height:1.3;margin:16px 0;font-weight:700;color:#484848">
+                                                <p style="font-size:20px;line-height:1.3;font-weight:700;color:#484848">
                                                     Estimado {{ $user->firstNames }},
                                                 </p>
-                                                <p
-                                                    style="font-size:16px;line-height:1.4;margin:16px 0;color:#484848;padding:24px;background-color:#f2f3f3;border-radius:4px">
-                                                    “Le informamos que PontiApp, la aplicación de gestión de EDAs,
-                                                    asistencias y más, ya está disponible.
-                                                    <br>
-                                                    <br>
-                                                    Hemos enviado esta invitación porque ha comenzado el envío de
-                                                    objetivos al EDA 2025. 📑”
+                                                <p>
+                                                    Se le remite sus accesos a la Aplicación Institucional PontiApp,
+                                                    sistema integrado de gestión de EDA.
                                                 </p>
-                                                <p style="font-size:15px;line-height:1.4;margin:16px 0;color:#484848">
+                                                <p>
                                                     Para acceder a la aplicación, utilice las siguientes credenciales:
                                                 </p>
+
                                                 <ul>
                                                     <li>
                                                         Usuario: Su correo institucional <a
@@ -65,42 +48,32 @@
                                                         Contraseña: Su número de documento de identidad
                                                     </li>
                                                 </ul>
-                                                <p
-                                                    style="font-size:15px;line-height:1.4;margin:16px 0;color:#484848;padding-bottom:16px">
-                                                    También puede iniciar sesión con su cuenta institucional de
-                                                    Microsoft.
-                                                </p>
-                                                <p
-                                                    style="font-size:15px;line-height:1.4;margin:16px 0;color:#484848;padding-bottom:16px">
+                                                <p>
                                                     Si encuentra algún inconveniente al iniciar sesión, no dude en
-                                                    comunicarse con el equipo de soporte técnico. 👨‍💻
+                                                    comunicarse con el equipo de sistemas
+
                                                 </p>
                                                 <a href="https://app.lapontificia.edu.pe"
-                                                    style="line-height:100%;text-decoration:none;display:block;max-width:100%;mso-padding-alt:0px;background-color:#25a2dc;border-radius:5px;color:#fff;font-size:16px;padding-top:19px;padding-bottom:19px;text-align:center;width:100%;padding:12px 0px 12px 0px;font-weight:500;"
+                                                    style="line-height:100%;text-decoration:none;display:block;max-width:100%;mso-padding-alt:0px;background-color:#242649;border-radius:5px;color:#fff;font-size:16px;padding-top:19px;padding-bottom:19px;text-align:center;width:100%;padding:12px 0px 12px 0px;font-weight:500;"
                                                     target="_blank"><span
                                                         style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:14.25px">Ir
                                                         a la aplicación</span></a>
+                                                <p>
+                                                    Saludos cordiales.
+                                                </p>
                                             </tr>
                                         </tbody>
                                     </table>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <hr style="width:100%;border:none;border-top:1px solid #eaeaea;border-color:#cccccc" />
-                    <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0"
-                        role="presentation">
-                        <tbody>
-                            <tr>
-                                <td>
                                     <table align="center" width="100%" border="0" cellpadding="0" cellspacing="0"
                                         role="presentation">
-                                        <tbody style="width:100%">
-                                            <tr style="width:100%">
-                                                <p
-                                                    style="font-size:14px;line-height:24px;margin:16px 0;color:#9ca299;margin-bottom:10px">
-                                                    2025 © La Pontificia. Tambo de Oro 112-601, Santiago de Surco, Lima
-                                                </p>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <img alt="La Pontificia" height="20"
+                                                        src="http://res.cloudinary.com/dc0t90ahb/image/upload/v1738211374/pontiapp/qjrx3wnvimjuct523xb2.png"
+                                                        style="display:block;outline:none;border:none;text-decoration:none;margin:auto;"
+                                                        width="70" />
+                                                </td>
                                             </tr>
                                         </tbody>
                                     </table>
