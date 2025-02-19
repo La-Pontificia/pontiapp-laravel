9ad9e87a1e750784d5629de6f05edf672a56febf
Daustinn <daustinn.music@gmail.com>
2025-02-19 11:09:37 -0500

    Refactor API routes: standardize endpoint definitions and rename routes for improved clarity and consistency

--- routes/api/rm.php
+++ routes/api/rm.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\Rm\RmAcademicAreaController;
 use App\Http\Controllers\Api\Rm\RmAcademicProgramController;
-use App\Http\Controllers\Api\Rm\RmAreaController;
 use App\Http\Controllers\Api\Rm\RmBranchController;
+use App\Http\Controllers\Api\Rm\RmClassroomController;
 use App\Http\Controllers\Api\Rm\RmCycleController;
-use App\Http\Controllers\Api\Rm\RmRoomController;
 use App\Http\Controllers\Api\Rm\RmSectionController;
 use App\Http\Controllers\Api\Rm\RmPeriodController;
 use App\Http\Controllers\Api\Rm\RmTTrackingController;
@@ -14,51 +14,58 @@ Route::middleware('check.auth')->group(function () {
     Route::prefix('rm')->group(function () {
         Route::prefix('periods')->group(function () {
             Route::get('', [RmPeriodController::class, 'index']);
-            Route::post('store', [RmPeriodController::class, 'store']);
-            Route::post('delete/{id}', [RmPeriodController::class, 'delete']);
+            Route::post('', [RmPeriodController::class, 'store']);
+            Route::post('{id}', [RmPeriodController::class, 'update']);
+            Route::post('{id}/delete', [RmPeriodController::class, 'delete']);
         });
 
         Route::prefix('sections')->group(function () {
             Route::get('', [RmSectionController::class, 'index']);
-            Route::post('store', [RmSectionController::class, 'store']);
-            Route::post('delete/{id}', [RmSectionController::class, 'delete']);
+            Route::post('', [RmSectionController::class, 'store']);
+            Route::post('{id}', [RmSectionController::class, 'update']);
+            Route::post('{id}/delete', [RmSectionController::class, 'delete']);
         });
 
-        Route::prefix('rooms')->group(function () {
-            Route::get('', [RmRoomController::class, 'index']);
-            Route::post('store', [RmRoomController::class, 'store']);
-            Route::post('delete/{id}', [RmRoomController::class, 'delete']);
+        Route::prefix('classrooms')->group(function () {
+            Route::get('', [RmClassroomController::class, 'index']);
+            Route::post('', [RmClassroomController::class, 'store']);
+            Route::post('{id}', [RmClassroomController::class, 'update']);
+            Route::post('{id}/delete', [RmClassroomController::class, 'delete']);
         });
 
         Route::prefix('cycles')->group(function () {
             Route::get('', [RmCycleController::class, 'index']);
-            Route::post('store', [RmCycleController::class, 'store']);
-            Route::post('delete/{id}', [RmCycleController::class, 'delete']);
+            Route::post('', [RmCycleController::class, 'store']);
+            Route::post('{id}', [RmCycleController::class, 'update']);
+            Route::post('{id}/delete', [RmCycleController::class, 'delete']);
         });
 
-        Route::prefix('areas')->group(function () {
-            Route::get('', [RmAreaController::class, 'index']);
-            Route::post('store', [RmAreaController::class, 'store']);
-            Route::post('delete/{id}', [RmAreaController::class, 'delete']);
+        Route::prefix('academic-areas')->group(function () {
+            Route::get('', [RmAcademicAreaController::class, 'index']);
+            Route::post('', [RmAcademicAreaController::class, 'store']);
+            Route::post('{id}', [RmAcademicAreaController::class, 'update']);
+            Route::post('{id}/delete', [RmAcademicAreaController::class, 'delete']);
         });
 
         Route::prefix('academic-programs')->group(function () {
             Route::get('', [RmAcademicProgramController::class, 'index']);
-            Route::post('store', [RmAcademicProgramController::class, 'store']);
-            Route::post('delete/{id}', [RmAcademicProgramController::class, 'delete']);
+            Route::post('', [RmAcademicProgramController::class, 'store']);
+            Route::post('{id}', [RmAcademicProgramController::class, 'update']);
+            Route::post('{id}/delete', [RmAcademicProgramController::class, 'delete']);
         });
 
         Route::prefix('branches')->group(function () {
             Route::get('', [RmBranchController::class, 'index']);
-            Route::post('store', [RmBranchController::class, 'store']);
-            Route::post('delete/{id}', [RmBranchController::class, 'delete']);
+            Route::post('', [RmBranchController::class, 'store']);
+            Route::post('{id}', [RmBranchController::class, 'update']);
+            Route::post('{id}/delete', [RmBranchController::class, 'delete']);
         });
 
         Route::prefix('tt')->group(function () {
             Route::get('', [RmTTrackingController::class, 'index']);
             Route::post('store', [RmTTrackingController::class, 'store']);
             Route::post('report', [RmTTrackingController::class, 'report']);
-            Route::post('{id}/update', [RmTTrackingController::class, 'update']);
+            Route::post('{id}', [RmTTrackingController::class, 'update']);
             Route::post('{id}/delete', [RmTTrackingController::class, 'delete']);
             Route::get('{id}', [RmTTrackingController::class, 'one']);
         });
