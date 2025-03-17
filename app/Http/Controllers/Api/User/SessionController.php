c17d5453f8deea6c731e57739a97d4dea75d74fe
Daustinn <daustinn.music@gmail.com>
2025-03-17 11:58:07 -0500

    Add SessionController: implement index and delete methods for session management

---
+++ app/Http/Controllers/Api/User/SessionController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\user\Session;
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    public function index(Request $req)
+    {
+        $match = Session::orderBy('created_at', 'asc');
+        $q = $req->query('q');
+        $userId = $req->query('userId');
+        $paginate = $req->query('paginate') === 'true';
+
+        if ($q) $match->where('userId', 'like', "%$q%")->orWhere('ip', 'like', "%$q%")->orWhere('location', 'like', "%$q%");
+        if ($userId) $match->where('userId', $userId);
+
+        $data = $paginate ? $match->paginate(25) :  $match->get();
+
+        $graphed = $data->map(function ($item) {
+            return $item->only(['id', 'userId', 'ip', 'userAgent', 'location', 'isMobile', 'isTablet', 'isDesktop', 'browser', 'platform', 'created_at']) +
+                ['user' => $item->user ? $item->user->only(['id', 'firstNames', 'lastNames', 'displayName']) : null];
+        });
+
+        return response()->json(
+            $paginate ? [
+                ...$data->toArray(),
+                'data' => $graphed,
+            ] : $graphed
+        );
+    }
+
+    public function delete($id)
+    {
+        $data = Session::find($id);
+        if (!$data) return response()->json('not_found', 404);
+        $data->delete();
+        return response()->json('Data deleted');
+    }
+}
