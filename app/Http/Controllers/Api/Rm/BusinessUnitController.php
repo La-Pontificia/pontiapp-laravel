39e5a95d8711036dfdd08e4ef8b2a8b3f81c59bb
Daustinn <daustinn.music@gmail.com>
2025-05-13 00:35:06 -0500

    Refactor BusinessUnitController: Rename 'all' method to 'index', implement 'store', 'update', and 'delete' methods for CRUD operations, and enhance response structure with creator and updater details for improved data handling.

--- app/Http/Controllers/Api/Rm/BusinessUnitController.php
+++ app/Http/Controllers/Api/Rm/BusinessUnitController.php
@@ -5,26 +5,97 @@ namespace App\Http\Controllers\Api\Rm;
 use App\Http\Controllers\Controller;
 use App\Models\Rm\BusinessUnit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BusinessUnitController extends Controller
 {
-    public function all(Request $req)
+    public function index(Request $req)
     {
         $match = BusinessUnit::orderBy('created_at', 'desc');
-        $paginate = $req->query('paginate', 'false');
-
         $q = $req->query('q');
-        $relationship = explode(',', $req->query('relationship', ''));
+
+        $paginate = $req->query('paginate') === 'true';
 
         if ($q) $match->where('name', 'like', "%$q%")
-            ->orWhere('description', 'like', "%$q%");
+            ->orWhere("acronym", "like", "%$q%");
 
-        if (in_array('creator', $relationship)) $match->with('creator');
-        if (in_array('updater', $relationship)) $match->with('updater');
+        $data = $paginate ? $match->paginate(25) :  $match->get();
 
+        $graphed = $data->map(function ($item) {
+            return $item->only(['id', 'name', 'acronym', 'logoURL', 'logoURLSquare', 'domain', 'created_at']) +
+                ['creator' => $item->creator ? $item->creator->only(['id', 'firstNames', 'lastNames', 'displayName']) : null];
+        });
 
-        $items = $paginate === 'true' ? $match->paginate() : $match->get();
+        return response()->json(
+            $paginate ? [
+                ...$data->toArray(),
+                'data' => $graphed,
+            ] : $graphed
+        );
+    }
 
-        return response()->json($items);
+    public function store(Request $req)
+    {
+        $req->validate([
+            'name' => 'required|string',
+            'acronym' => 'required|string',
+            'domain' => 'required|string',
+
+        ]);
+
+        $already = BusinessUnit::where('name', $req->name)->first();
+        if ($already) return response()->json('already_exists', 400);
+
+        $data = BusinessUnit::create([
+            'name' => $req->name,
+            'acronym' => $req->acronym,
+            'domain' => $req->domain,
+            'creatorId' => Auth::id(),
+        ]);
+
+        return response()->json($data);
+    }
+
+    public function update(Request $req, $id)
+    {
+        $req->validate([
+            'name' => 'required|string',
+            'acronym' => 'required|string',
+            'domain' => 'required|string',
+        ]);
+
+        $item = BusinessUnit::find($id);
+
+        // validate if the period exists
+        if (!$item) return response()->json('not_found', 404);
+
+        $already = BusinessUnit::where('name', $req->name)->where('id', '!=', $id)->first();
+
+        if ($already) return response()->json('already_exists', 400);
+
+        $item->update([
+            'name' => $req->name,
+            'acronym' => $req->acronym,
+            'domain' => $req->domain,
+            'updaterId' => Auth::id(),
+        ]);
+        return response()->json('Updated');
+    }
+
+    public function delete($id)
+    {
+        return response()->json('not_found', 404);
+    }
+
+    public function one($id)
+    {
+        $data = BusinessUnit::find($id);
+        if (!$data) return response()->json('Data not found', 404);
+        return response()->json(
+            $data->only(['id', 'name', 'acronym', 'logoURL', 'logoURLSquare', 'domain', ' created_at']) +
+                ['creator' => $data->creator ? $data->creator->only(['id', 'firstNames', 'lastNames', 'displayName']) : null] +
+                ['updater' => $data->updater ? $data->updater->only(['id', 'firstNames', 'lastNames', 'displayName']) : null]
+
+        );
     }
 }
